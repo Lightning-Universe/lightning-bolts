f11f27aedccfa91f10dfaf1efeccd6325f2654e9
William Falcon <waf2107@columbia.edu>
2020-04-17 07:54:09 -0400

    change name

---
+++ pytorch_lightning_bolts/__init__.py
@@ -0,0 +1,9 @@
+"""Root package info."""
+
+__version__ = '0.1.0-dev'
+__author__ = 'PyTorchLightning et al.'
+__author_email__ = 'name@pytorchlightning.ai'
+__license__ = 'TBD'
+__copyright__ = 'Copyright (c) 2020-2020, %s.' % __author__
+__homepage__ = 'https://github.com/PyTorchLightning/lightning-bolts'
+__docs__ = "PyTorch Lightning Bolts is a community contribution for ML researchers."
---
+++ pytorch_lightning_bolts/models/__init__.py
@@ -0,0 +1,3 @@
+"""
+Collection of PyTorchLightning models
+"""
