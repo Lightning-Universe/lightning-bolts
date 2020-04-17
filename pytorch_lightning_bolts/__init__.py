c189c3a563a5055e7eb337274542351544f9e92e
J. Borovec <jirka.borovec@seznam.cz>
2020-04-17 16:02:07 +0200

    formatting

--- pytorch_lightning_bolts/__init__.py
+++ pytorch_lightning_bolts/__init__.py
@@ -2,10 +2,12 @@
 
 import os
 
-__version__ = '0.0.1dev4'
+__version__ = '0.1.0-dev4'
 __author__ = 'PyTorchLightning et al.'
 __author_email__ = 'name@pytorchlightning.ai'
 __license__ = 'TBD'
 __copyright__ = 'Copyright (c) 2020-2020, %s.' % __author__
 __homepage__ = 'https://github.com/PyTorchLightning/pytorch-lightning-bolts'
 __docs__ = "PyTorch Lightning Bolts is a community contribution for ML researchers."
+
+PACKAGE_ROOT = os.path.dirname(__file__)
