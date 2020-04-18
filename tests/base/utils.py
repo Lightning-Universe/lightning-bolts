80836ef22161290f4e733e021eb955d59042feff
Annika Brundyn <annika@Annikas-MacBook-Pro.local>
2020-04-17 21:57:05 -0400

    pep8

--- tests/base/utils.py
+++ tests/base/utils.py
@@ -1,7 +1,14 @@
 import numpy as np
+import os
 import torch
 
+# generate a list of random seeds for each test
+RANDOM_PORTS = list(np.random.randint(12000, 19000, 1000))
+ROOT_SEED = 1234
+torch.manual_seed(ROOT_SEED)
+np.random.seed(ROOT_SEED)
 RANDOM_SEEDS = list(np.random.randint(0, 10000, 1000))
+ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 
 def reset_seed():
