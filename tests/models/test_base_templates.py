559b3d19a9e600c3d32c605ba54e5f092d51ef59
William Falcon <waf2107@columbia.edu>
2020-04-18 10:01:33 -0400

    mnist tests

--- tests/models/test_base_templates.py
+++ tests/models/test_base_templates.py
@@ -9,7 +9,7 @@ def test_vae(tmpdir):
     """ Test that an error is thrown when no `training_step()` is defined """
     reset_seed()
 
-    params = {'hidden_dim': 128, 'batch_size':32, 'learning_rate': 0.001}
+    params = {'hidden_dim': 128, 'batch_size': 32, 'learning_rate': 0.001}
     model = LitMNISTModel(Namespace(**params))
     trainer = pl.Trainer(train_percent_check=0.01, val_percent_check=0.01, max_epochs=1)
     trainer.fit(model)
