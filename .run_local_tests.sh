7a472f9094893dbc3cb1041c4cdd0fef7ce2060f
William Falcon <waf2107@columbia.edu>
2020-04-20 09:21:17 -0400

    added new path

---
+++ .run_local_tests.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# use this to run tests
+rm -rf _ckpt_*
+rm -rf ./tests/save_dir*
+rm -rf ./tests/mlruns_*
+rm -rf ./tests/cometruns*
+rm -rf ./tests/wandb*
+rm -rf ./tests/tests/*
+rm -rf ./lightning_logs
+python -m coverage run --source pytorch_lightning_bolts -m py.test pytorch_lightning_bolts tests -v --doctest-modules --flake8
+python -m coverage report -m
