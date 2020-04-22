f5a123db6565536b092f20af05caea3672fc912b
William Falcon <waf2107@columbia.edu>
2020-04-21 22:16:04 -0400

    added basic AE

--- pl_bolts/models/autoencoders/__init__.py
+++ pl_bolts/models/autoencoders/__init__.py
@@ -1,3 +1,3 @@
-"""Variational Autoencoder"""
+"""Autoencoders"""
 from pl_bolts.models.autoencoders.basic_vae.basic_vae_pl_module import BasicVAE
 from pl_bolts.models.autoencoders.basic_ae.basic_ae_pl_module import BasicAE
