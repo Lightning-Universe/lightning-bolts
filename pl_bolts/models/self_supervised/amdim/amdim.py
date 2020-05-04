4656eeb50610ca5a7cf158fd92a221ec5822c066
William Falcon <waf2107@columbia.edu>
2020-05-04 08:51:14 -0400

    added amdim initial commit

--- pl_bolts/models/self_supervised/amdim/amdim.py
+++ pl_bolts/models/self_supervised/amdim/amdim.py
@@ -12,7 +12,7 @@ from fisherman.utils.debugging import ForkedPdb
 from fisherman.models.lda_extensions.lda_datasets import UnlabeledImagenet
 from sklearn.utils import shuffle
 
-from fisherman.models.submodels import amdim_nets as amdim_utils
+from pl_bolts.models.self_supervised.amdim.networks import Encoder
 from fisherman.utils.datasets import AMDIMPretraining
 
 
@@ -25,7 +25,7 @@ class AMDIMSelfSupervised(pl.LightningModule):
 
         dummy_batch = torch.zeros((2, 3, hparams.image_height, hparams.image_height))
 
-        self.encoder = amdim_utils.Encoder(
+        self.encoder = Encoder(
             dummy_batch,
             num_channels=3,
             ndf=hparams.ndf,
