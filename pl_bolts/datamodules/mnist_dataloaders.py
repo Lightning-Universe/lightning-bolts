0089984c26dea144c9cc1fd495b1a13c1682bc60
William Falcon <waf2107@columbia.edu>
2020-05-15 09:28:20 -0400

    updated cf10 loader

--- pl_bolts/datamodules/mnist_dataloaders.py
+++ pl_bolts/datamodules/mnist_dataloaders.py
@@ -6,10 +6,11 @@ from pl_bolts.datamodules.bolts_dataloaders_base import BoltDataLoaders
 
 class MNISTDataLoaders(BoltDataLoaders):
 
-    def __init__(self, save_path, val_split=5000):
+    def __init__(self, save_path, val_split=5000, num_workers=16):
         super().__init__()
         self.save_path = save_path
         self.val_split = val_split
+        self.num_workers = num_workers
 
     @property
     def train_length(self):
@@ -25,7 +26,7 @@ class MNISTDataLoaders(BoltDataLoaders):
 
         dataset = MNIST(self.save_path, train=True, download=False, transform=transforms)
         dataset_train, _ = random_split(dataset, [self.train_length - self.val_split, self.val_split])
-        loader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
+        loader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=self.num_workers)
         return loader
 
     def val_dataloader(self, batch_size,transforms=None, use_default_normalize=True):
@@ -34,7 +35,7 @@ class MNISTDataLoaders(BoltDataLoaders):
 
         dataset = MNIST(self.save_path, train=True, download=True, transform=transforms)
         _, dataset_val = random_split(dataset, [self.train_length - self.val_split, self.val_split])
-        loader = DataLoader(dataset_val, batch_size=batch_size, shuffle=False)
+        loader = DataLoader(dataset_val, batch_size=batch_size, shuffle=False, num_workers=self.num_workers)
         return loader
 
     def test_dataloader(self, batch_size, transforms=None, use_default_normalize=True):
@@ -42,7 +43,7 @@ class MNISTDataLoaders(BoltDataLoaders):
             transforms = self.get_transforms()
 
         dataset = MNIST(self.save_path, train=False, download=False, transform=transforms)
-        loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+        loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=self.num_workers)
         return loader
 
     def default_transforms(self):
