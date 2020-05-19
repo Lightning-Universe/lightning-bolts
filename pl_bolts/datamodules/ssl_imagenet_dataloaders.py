9bf08c7b24c3ee3acab546f554ea953f7b4c8df6
William Falcon <waf2107@columbia.edu>
2020-05-18 20:23:13 -0400

    added meta parser

--- pl_bolts/datamodules/ssl_imagenet_dataloaders.py
+++ pl_bolts/datamodules/ssl_imagenet_dataloaders.py
@@ -29,9 +29,25 @@ class SSLImagenetDataLoaders(BoltDataLoaders):
                                     f'folder contains a subfolder with {split} in the name')
 
     def prepare_data(self):
-
         # imagenet cannot be downloaded... must provide path to folder with the train/val splits
-        pass
+        train_dir = self._resolve_data_subfolder(self.data_dir, 'train')
+        files = os.listdir(train_dir)
+        if 'meta.bin' not in files:
+            m = """
+            no meta.bin present. Imagenet is no longer automatically downloaded by PyTorch.
+            To get imagenet:
+            1. download yourself from http://www.image-net.org/challenges/LSVRC/2012/downloads
+            2. download the devkit (ILSVRC2012_devkit_t12.tar.gz)
+            3. generate the meta.bin file using the devkit
+            4. copy the meta.bin file into both train and val split folders
+            
+            To generate the meta.bin do the following:
+            
+            from pl_bolts.models.self_supervised.amdim.ssl_datasets import UnlabeledImagenet
+            path = '/path/to/folder/with/ILSVRC2012_devkit_t12.tar.gz/'
+            UnlabeledImagenet.generate_meta_bins(path)    
+            """
+            raise FileNotFoundError(m)
 
     def train_dataloader(self, batch_size, num_images_per_class=-1, transforms=None, add_normalize=False):
         if transforms is None:
