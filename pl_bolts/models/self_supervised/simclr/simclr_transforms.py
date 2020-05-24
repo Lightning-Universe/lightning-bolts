6fb5042483e2fe337661171b528394944035e522
William Falcon <waf2107@columbia.edu>
2020-05-23 21:34:14 -0400

    added simclr base

---
+++ pl_bolts/models/self_supervised/simclr/simclr_transforms.py
@@ -0,0 +1,44 @@
+import torchvision.transforms as transforms
+import numpy as np
+import cv2
+
+
+class SimCLRDataTransform(object):
+    def __init__(self, input_width, s=1):
+        self.s = s
+        self.width = input_width
+        color_jitter = transforms.ColorJitter(0.8 * self.s, 0.8 * self.s, 0.8 * self.s, 0.2 * self.s)
+        data_transforms = transforms.Compose([transforms.RandomResizedCrop(size=self.width),
+                                              transforms.RandomHorizontalFlip(),
+                                              transforms.RandomApply([color_jitter], p=0.8),
+                                              transforms.RandomGrayscale(p=0.2),
+                                              GaussianBlur(kernel_size=int(0.1 * self.width)),
+                                              transforms.ToTensor()])
+
+        self.transform = data_transforms
+
+    def __call__(self, sample):
+        xi = self.transform(sample)
+        xj = self.transform(sample)
+        return xi, xj
+
+
+class GaussianBlur(object):
+    # Implements Gaussian blur as described in the SimCLR paper
+    def __init__(self, kernel_size, min=0.1, max=2.0):
+        self.min = min
+        self.max = max
+        # kernel size is set to be 10% of the image height/width
+        self.kernel_size = kernel_size
+
+    def __call__(self, sample):
+        sample = np.array(sample)
+
+        # blur the image with a 50% chance
+        prob = np.random.random_sample()
+
+        if prob < 0.5:
+            sigma = (self.max - self.min) * np.random.random_sample() + self.min
+            sample = cv2.GaussianBlur(sample, (self.kernel_size, self.kernel_size), sigma)
+
+        return sample
