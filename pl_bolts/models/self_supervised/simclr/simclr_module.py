fafe2a9229866db96a7dd06529d8201022bef976
William Falcon <waf2107@columbia.edu>
2020-05-23 21:20:38 -0400

    added LARS optimizer

---
+++ pl_bolts/models/self_supervised/simclr/simclr_module.py
@@ -0,0 +1,190 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import pytorch_lightning as pl
+import torch
+import torchnet as tnt
+from pytorch_lightning.callbacks import ModelCheckpoint
+from torch import nn
+from torch.nn import functional as F
+from torch.optim.lr_scheduler import StepLR
+from torch.utils.data.dataloader import DataLoader
+import densenet
+from data import Dataset, toPIL, create_transform
+from optim import LARS
+
+
+class EncoderModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # self.model = xrv.models.DenseNet(num_classes=1).cuda()
+        self.model = densenet.densenet121(densenet.ModelCfg(), num_classes=1)
+        del self.model.classifier
+    def forward(self, x):
+        features = self.model.features(x)
+        out = F.relu(features, inplace=True)
+        out = F.adaptive_avg_pool2d(out, (1, 1)).view(features.size(0), -1)
+        return out
+    
+    
+class Projection(nn.Module):
+    def __init__(self, input_dim=1024, output_dim=128):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, 512, bias=False),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, output_dim, bias=True))
+    def forward(self, x):
+        x = self.model(x)
+        return F.normalize(x, dim=1)
+    
+    
+def nt_xent(out_1, out_2, temperature):
+    out = torch.cat([out_1, out_2], dim=0)
+    n_samples = len(out)
+    # Full similarity matrix
+    cov = torch.mm(out, out.t().contiguous())
+    sim = torch.exp(cov / temperature)
+    # Negative similarity
+    mask = ~torch.eye(n_samples, device=sim.device).bool()
+    neg = sim.masked_select(mask).view(n_samples, -1).sum(dim=-1)
+    # Positive similarity :
+    pos = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
+    pos = torch.cat([pos, pos], dim=0)
+    loss = -torch.log(pos / neg).mean()
+    return loss
+
+
+class SimCLR(pl.LightningModule):
+    def __init__(self, hparams, encoder, projection, loss_func, temperature, transform_list):
+        super().__init__()
+        self.hparams = hparams
+        self.encoder = encoder
+        self.projection = projection
+        self.loss_func = loss_func
+        self.temp = temperature
+        self.transform_list = transform_list
+        self.lossmeter = tnt.meter.AverageValueMeter()
+        
+    def forward(self, x):
+        h = self.encoder(x)
+        z = self.projection(h)
+        return h, z
+    
+    def on_epoch_start(self):
+        self.lossmeter.reset()
+
+    def training_step(self, batch, batch_idx):
+        h1, z1 = self.forward(batch['PA'])
+        h2, z2 = self.forward(batch['PA2'])
+        # return h1, z1, h2, z2
+        loss = self.loss_func(z1, z2, self.temp)
+        logs = {'loss': loss.item()}
+        return dict(loss=loss, log=logs)
+
+    # def training_step_end(self, output_parts):
+    #     h1s, z1s, h2s, z2s = output_parts
+    #     rank = torch.distributed.get_rank()
+    #     print(f'Rank = {rank}', [h1.shape for h1 in h1s])
+    #     print(f'Rank = {rank}', [h2.shape for h2 in h2s])
+    #     print(f'Rank = {rank}', [z1.shape for z1 in z1s])
+    #     print(f'Rank = {rank}', [z2.shape for z2 in z2s])
+
+    def validation_step(self, batch, batch_idx):
+        h1, z1 = self.forward(batch['PA'])
+        h2, z2 = self.forward(batch['PA2'])
+        loss = self.loss_func(z1, z2, self.temp)
+        logs = {'val_loss': loss.item()}
+        self.lossmeter.add(loss.item(), h1.shape[0])
+        return dict(loss=loss, log=logs)
+
+    def validation_epoch_end(self, outputs: list):
+        logs = dict(
+            val_loss=self.lossmeter.mean,
+        )
+        print('\nLogs: ', logs)
+        return dict(val_loss=self.lossmeter.mean, log=logs)
+
+    def create_dataloader(self, metafile):
+        data = CheX_Dataset(
+            Path(self.hparams.root),
+            Path(self.hparams.root) / self.hparams.dataset / metafile,
+            transform=toPIL,
+        )
+        # sampler = None if self.hparams.gpus == 1 else DistributedSampler(data)
+        sampler = None
+        transform = create_transform(self.transform_list)
+        return DataLoader(
+            Dataset(data, transform, transform),
+            batch_size=self.hparams.bsz,
+            sampler=sampler,
+            num_workers=10,
+        )
+
+    def train_dataloader(self):
+        return self.create_dataloader(metafile='train.csv')
+
+    def val_dataloader(self):
+        return self.create_dataloader(metafile='valid.csv')
+
+    def configure_optimizers(self):
+        if self.hparams.optim == 'adam':
+            optimizer = torch.optim.Adam(
+                self.parameters(), self.hparams.lr, weight_decay=self.hparams.wd)
+        elif self.hparams.optim == 'lars':
+            optimizer = LARS(
+                self.parameters(), lr=self.hparams.lr, momentum=self.hparams.mom,
+                weight_decay=self.hparams.wd, eta=self.hparams.eta)
+        else:
+            raise ValueError(f'Invalid optimizer: {self.hparams.optim}')
+        scheduler = StepLR(
+            optimizer, step_size=self.hparams.step, gamma=self.hparams.gamma)
+        return [optimizer], [scheduler]
+
+def create_argparser():
+    parser = ArgumentParser()
+    # Data
+    parser.add_argument('--root', type=str, default='/datasets01/covid19/032820/CheXpert')
+    # Training
+    parser.add_argument('--expdir', type=str, default='simclrlogs')
+    parser.add_argument('--dataset', type=str, default='CheXpert-v1.0')
+    parser.add_argument('--optim', choices=['adam', 'lars'], default='adam')
+    parser.add_argument('--bsz', type=int, default=16)
+    parser.add_argument('--lr', type=float, default=0.0001)
+    parser.add_argument('--mom', type=float, default=0.9)
+    parser.add_argument('--eta', type=float, default=0.001)
+    parser.add_argument('--step', type=float, default=30)
+    parser.add_argument('--gamma', type=float, default=0.5)
+    parser.add_argument('--wd', type=float, default=0.0005)
+    parser.add_argument('--gpus', type=int, default=8)
+    # Model
+    parser.add_argument('--temp', type=float, default=0.5)
+    parser.add_argument('--trans', type=str, default='randcrop,flip')
+    return parser
+
+
+def main(args):
+    print('Args:', args)
+    model = SimCLR(
+        hparams=args,
+        encoder=EncoderModel(),
+        projection=Projection(),
+        loss_func=nt_xent,
+        temperature=args.temp,
+        transform_list=list(args.trans.split(','))
+    )
+    checkpoint = ModelCheckpoint(
+        filepath=Path(args.expdir) / 'checkpoints', save_top_k=100)
+    trainer = pl.Trainer(
+        distributed_backend='ddp',
+        gpus=args.gpus,
+        default_save_path=args.expdir,
+        checkpoint_callback=checkpoint
+    )
+    trainer.fit(model)
+
+
+if __name__ == '__main__':
+    parser = create_argparser()
+    args = parser.parse_args()
+    main(args)
---
+++ pl_bolts/optimizers/lars.py
@@ -0,0 +1,108 @@
+"""
+Layer-wise adaptive rate scaling for SGD in PyTorch!
+Adapted from: https://github.com/noahgolmant/pytorch-lars/blob/master/lars.py
+"""
+import torch
+from torch.optim.optimizer import Optimizer
+
+
+class _RequiredParameter(object):
+    """Singleton class representing a required parameter for an Optimizer."""
+    def __repr__(self):
+        return "<required parameter>"
+
+
+required = _RequiredParameter()
+
+
+class LARS(Optimizer):
+    r"""Implements layer-wise adaptive rate scaling for SGD.
+    Args:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float): base learning rate (\gamma_0)
+        momentum (float, optional): momentum factor (default: 0) ("m")
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+            ("\beta")
+        eta (float, optional): LARS coefficient
+        max_epoch: maximum training epoch to determine polynomial LR decay.
+    Based on Algorithm 1 of the following paper by You, Gitman, and Ginsburg.
+    Large Batch Training of Convolutional Networks:
+        https://arxiv.org/abs/1708.03888
+    Example:
+        >>> optimizer = LARS(model.parameters(), lr=0.1, eta=1e-3)
+        >>> optimizer.zero_grad()
+        >>> loss_fn(model(input), target).backward()
+        >>> optimizer.step()
+    """
+    def __init__(self, params, lr=required, momentum=.9,
+                 weight_decay=.0005, eta=0.001, max_epoch=200):
+        if lr is not required and lr < 0.0:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if momentum < 0.0:
+            raise ValueError("Invalid momentum value: {}".format(momentum))
+        if weight_decay < 0.0:
+            raise ValueError("Invalid weight_decay value: {}"
+                             .format(weight_decay))
+        if eta < 0.0:
+            raise ValueError("Invalid LARS coefficient value: {}".format(eta))
+
+        self.epoch = 0
+        defaults = dict(lr=lr, momentum=momentum,
+                        weight_decay=weight_decay,
+                        eta=eta, max_epoch=max_epoch)
+        super(LARS, self).__init__(params, defaults)
+
+    def step(self, epoch=None, closure=None):
+        """Performs a single optimization step.
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+            epoch: current epoch to calculate polynomial LR decay schedule.
+                   if None, uses self.epoch and increments it.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        if epoch is None:
+            epoch = self.epoch
+            self.epoch += 1
+
+        for group in self.param_groups:
+            weight_decay = group['weight_decay']
+            momentum = group['momentum']
+            eta = group['eta']
+            lr = group['lr']
+            max_epoch = group['max_epoch']
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+
+                param_state = self.state[p]
+                d_p = p.grad.data
+
+                weight_norm = torch.norm(p.data)
+                grad_norm = torch.norm(d_p)
+
+                # Global LR computed on polynomial decay schedule
+                decay = (1 - float(epoch) / max_epoch) ** 2
+                global_lr = lr * decay
+
+                # Compute local learning rate for this layer
+                local_lr = eta * weight_norm / \
+                    (grad_norm + weight_decay * weight_norm)
+
+                # Update the momentum term
+                actual_lr = local_lr * global_lr
+
+                if 'momentum_buffer' not in param_state:
+                    buf = param_state['momentum_buffer'] = \
+                            torch.zeros_like(p.data)
+                else:
+                    buf = param_state['momentum_buffer']
+                buf.mul_(momentum).add_(actual_lr, d_p + weight_decay * p.data)
+                p.data.add_(-buf)
+
+        return loss
