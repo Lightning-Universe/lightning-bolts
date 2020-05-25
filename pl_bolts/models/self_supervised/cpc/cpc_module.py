b9685c7a439f0e4973f38589ca02977ca0c2fa2d
William Falcon <waf2107@columbia.edu>
2020-05-25 15:40:52 -0400

    updated docs

--- pl_bolts/models/self_supervised/cpc/cpc_module.py
+++ pl_bolts/models/self_supervised/cpc/cpc_module.py
@@ -235,7 +235,7 @@ class CPCV2(pl.LightningModule):
             ]
         }
 
-        # v100@32GB batch_size = 200
+        # v100@32GB batch_size = 176
         stl10 = {
             'dataset': 'stl10',
             'depth': 8,
