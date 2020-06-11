0e2c4a82eca1c453d1bb0c98312c3484248930a4
Annika Brundyn <annikabrundyn1@gmail.com>
2020-06-11 12:15:22 -0400

    dataloaders

--- pl_bolts/datamodules/sklearn_datamodule.py
+++ pl_bolts/datamodules/sklearn_datamodule.py
@@ -1,5 +1,5 @@
 from pl_bolts.datamodules.bolts_dataloaders_base import BoltDataLoaders
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader, random_split
 from sklearn.model_selection import train_test_split
 
 class SklearnDataset(Dataset):
@@ -25,23 +25,58 @@ class SklearnDataset(Dataset):
         return x, y
 
 
-
+# assumes you have a big X and Y and does the train/val/test split for you
 class SklearnDataLoaders(BoltDataLoaders):
-    def __init__(self, X, y, val_split=0.15, test_split=0.15, X_transform=None, y_transform=None):
+    def __init__(self, X, y, val_split=0.15, test_split=0.15, X_transform=None, y_transform=None, num_workers=16,
+                 shuffle=True, random_seed=0):
         self.X = X
         self.Y = y
         self.val_split = val_split
         self.test_split = test_split
         self.X_transform = X_transform
         self.y_transform = y_transform
+        self.num_workers = num_workers
 
+        # Split X, y into train/validation/test
+        dataset_size = len(X)
+        indices = list(range(dataset_size))
+        val_size = int(np.floor(val_split * dataset_size))
+        test_size = int(np.floor(test_split * dataset_size))
 
+        self.dataset_train, self.dataset_val, self.dataset_test = random_split((X, y),
+                                                                               [dataset_size - val_size - test_size,
+                                                                                val_size,
+                                                                                test_size])
 
+    def train_dataloader(self, batch_size):
+        loader = DataLoader(
+            self.dataset_train,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+            drop_last=True,
+            pin_memory=True
+        )
+        return loader
 
-    def train_dataloader(self):
+    def val_dataloader(self, batch_size):
+        loader = DataLoader(
+            self.dataset_val,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            drop_last=True,
+            pin_memory=True
+        )
+        return loader
 
-    def val_dataloader(self):
-
-    def test_dataloader(self):
-
-
+    def test_dataloader(self, batch_size):
+        loader = DataLoader(
+            self.dataset_test,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            drop_last=True,
+            pin_memory=True
+        )
+        return loader
