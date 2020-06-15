9b8f518bbbad99a16ae03140bfe07043560b7d86
William Falcon <waf2107@columbia.edu>
2020-06-15 15:22:18 -0400

    fixes

--- pl_bolts/models/self_supervised/__init__.py
+++ pl_bolts/models/self_supervised/__init__.py
@@ -7,4 +7,7 @@ These models might also generate better representations than models trained usin
 """
 from pl_bolts.models.self_supervised.amdim.amdim_module import AMDIM
 from pl_bolts.models.self_supervised.cpc.cpc_module import CPCV2
+from pl_bolts.models.self_supervised.simclr.simclr_module import SimCLR
+from pl_bolts.models.self_supervised.moco.moco2_module import MocoV2
+
 from pl_bolts.models.self_supervised.evaluator import SSLEvaluator
---
+++ tests/models/self_supervised.py
@@ -0,0 +1,26 @@
+import pytorch_lightning as pl
+from pl_bolts.models.self_supervised import CPCV2, AMDIM, SimCLR, MocoV2
+from tests import reset_seed
+from argparse import Namespace, ArgumentParser
+
+
+def test_cpcv2(tmpdir):
+    reset_seed()
+
+    parser = ArgumentParser()
+    parser = CPCV2.add_model_specific_args(parser)
+    hparams = parser.parse_args(args=[])
+    hparams.batch_size = 2
+    hparams.data_dir = tmpdir
+    hparams.meta_root = tmpdir
+
+    model = CPCV2(hparams)
+    trainer = pl.Trainer(fast_dev_run=True)
+    trainer.fit(model)
+    trainer.test()
+    loss = trainer.callback_metrics['loss']
+
+    assert loss > 0
+
+
+test_cpcv2('')
