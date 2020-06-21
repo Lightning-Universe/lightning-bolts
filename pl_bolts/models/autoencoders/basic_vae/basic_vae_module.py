dc2ffb8e040219b9cedce84ac8146649afc5c3e9
William Falcon <waf2107@columbia.edu>
2020-06-21 09:54:17 -0400

    adding docs

--- pl_bolts/models/autoencoders/basic_vae/basic_vae_module.py
+++ pl_bolts/models/autoencoders/basic_vae/basic_vae_module.py
@@ -192,7 +192,7 @@ class VAE(LightningModule):
                             help='input image width - 28 for MNIST (must be even)')
         parser.add_argument('--input_height', type=int, default=28,
                             help='input image height - 28 for MNIST (must be even)')
-        parser.add_argument('--input_channels', type=int, default=1,
+        parser.add_argument('--input_channels', type=int, default=3,
                             help='number of input channels')
         parser.add_argument('--batch_size', type=int, default=32)
         parser.add_argument('--learning_rate', type=float, default=1e-3)
