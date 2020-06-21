fbf42d91300f7a4c313f291bcf95f73f487012be
William Falcon <waf2107@columbia.edu>
2020-06-21 09:32:10 -0400

    adding docs

--- pl_bolts/datamodules/bolts_dataloaders_base.py
+++ pl_bolts/datamodules/bolts_dataloaders_base.py
@@ -1,9 +1,10 @@
 from abc import abstractmethod
 from typing import List, Union
+import inspect
 
 from pytorch_lightning.utilities import rank_zero_warn
 from torch.utils.data import DataLoader
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 
 
 class BoltDataModule(object):
@@ -208,6 +209,42 @@ class BoltDataModule(object):
 
     @property
     def add_argparse_args(self, parent_parser):
+        """
+        Adds dataset arguments to an argparser
+
+        Example::
+
+            datamodule = DataModule()
+            datamodule = datamodule.add_argparse_args
+        """
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
         parser.add_argument('--data_dir', type=str, default='')
         return parser
+
+    @classmethod
+    def from_argparse_args(cls, args: Union[Namespace, ArgumentParser], **kwargs) -> `BoltDataModule`:
+        """
+        Create an instance from CLI arguments.
+
+        Args:
+            args: The parser or namespace to take arguments from. Only known arguments will be
+                parsed and passed to the :class:`Trainer`.
+            **kwargs: Additional keyword arguments that may override ones in the parser or namespace.
+                These must be valid Trainer arguments.
+
+        Example::
+
+            parser = ArgumentParser(add_help=False)
+            parser = BoltDataModule.add_argparse_args(parser)
+            module = BoltDataModule.from_argparse_args(args)
+        """
+        if isinstance(args, ArgumentParser):
+            args = cls.parse_argparser(args)
+        params = vars(args)
+
+        # we only want to pass in valid Trainer args, the rest may be user specific
+        valid_kwargs = inspect.signature(cls.__init__).parameters
+        trainer_kwargs = dict((name, params[name]) for name in valid_kwargs if name in params)
+        trainer_kwargs.update(**kwargs)
+
+        return cls(**trainer_kwargs)
