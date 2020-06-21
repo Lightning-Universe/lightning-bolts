b274317c67e198c0b4c9bc6cb4d14fd5881b2020
William Falcon <waf2107@columbia.edu>
2020-06-21 14:03:06 -0400

    added pretrained weights

---
+++ pl_bolts/utils/pretrained_weights.py
@@ -0,0 +1,14 @@
+
+
+urls = {
+    'VAE': 'https://pl-bolts-weights.s3.us-east-2.amazonaws.com/vae/version_0/checkpoints/epoch%3D2.ckpt',
+    'CPCV2-resnet18': 'https://pl-bolts-weights.s3.us-east-2.amazonaws.com/cpc/resnet18_version_6/checkpoints/epoch%3D85.ckpt'
+}
+
+
+def load_pretrained(model, class_name=None):
+    if class_name is None:
+        class_name = model.__class__.__name__
+    ckpt_url = urls[class_name]
+    weights_model = model.__class__.load_from_checkpoint(ckpt_url)
+    model.load_state_dict(weights_model.state_dict())
