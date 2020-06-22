8195faad1c3cfdeff066130d6ffa1952879b2f6b
William Falcon <waf2107@columbia.edu>
2020-06-22 12:07:44 -0400

    added name

--- pl_bolts/models/self_supervised/cpc/__init__.py
+++ pl_bolts/models/self_supervised/cpc/__init__.py
@@ -3,4 +3,5 @@ from pl_bolts.models.self_supervised.cpc.networks import CPCResNet101
 from pl_bolts.models.self_supervised.cpc.transforms import (
      CPCTransformsCIFAR10,
      CPCTransformsSTL10Patches,
-     CPCTransformsImageNet128Patches)
+     CPCTransformsImageNet128Patches
+)
