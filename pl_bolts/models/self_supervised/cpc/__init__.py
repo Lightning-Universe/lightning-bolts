f65531b3cd28241daba19b84809ae413f683c7fa
William Falcon <waf2107@columbia.edu>
2020-06-24 14:37:02 -0400

    updated losses docs

--- pl_bolts/models/self_supervised/cpc/__init__.py
+++ pl_bolts/models/self_supervised/cpc/__init__.py
@@ -3,6 +3,7 @@ from pl_bolts.models.self_supervised.cpc.networks import CPCResNet101
 from pl_bolts.models.self_supervised.cpc.transforms import (
     CPCTrainTransformsCIFAR10,
     CPCEvalTransformsCIFAR10,
-    CPCTransformsSTL10Patches,
+    CPCTrainTransformsSTL10,
+    CPCEvalTransformsSTL10,
     CPCTransformsImageNet128Patches
 )
