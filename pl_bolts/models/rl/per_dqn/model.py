0eba30d276db5f94303f8c7f63e2a18535951242
Donal <donal.byrne@xperi.com>
2020-06-25 07:58:11 +0100

    Fixed PER DQN in relations to integration/unit tests

--- pl_bolts/models/rl/per_dqn/model.py
+++ pl_bolts/models/rl/per_dqn/model.py
@@ -1,5 +1,5 @@
 """
-N Step Deep Q-network
+DQN with Prioritized Experience Replay
 This example is based on: https://github.com/PacktPublishing/Deep-Reinforcement-Learning-Hands-On-Second-Edition/
 blob/master/Chapter08/05_dqn_prio_replay.py
 """
@@ -16,54 +16,41 @@ from pl_bolts.models.rl.dqn.model import DQN
 
 
 class PERDQN(DQN):
-    """ PER DQN Model """
+    """
+    PyTorch Lightning implementation of `DQN With Prioritized Experience Replay <https://arxiv.org/abs/1511.05952>`_
 
-    def __init__(self, env: str, gpus: int = 0, eps_start: float = 1.0, eps_end: float = 0.02,
-                 eps_last_frame: int = 150000, sync_rate: int = 1000, gamma: float = 0.99, lr: float = 1e-4,
-                 batch_size: int = 32, replay_size: int = 100000, warm_start_size: int = 10000, sample_len: int = 500,
-                 ) -> None:
-        """
-        PyTorch Lightning implementation of `DQN With Prioritized Experience Replay <https://arxiv.org/abs/1511.05952>`_
+    Paper authors: Tom Schaul, John Quan, Ioannis Antonoglou, David Silver
 
-        Paper authors: Tom Schaul, John Quan, Ioannis Antonoglou, David Silver
+    Model implemented by:
 
-        Model implemented by:
+        - `Donal Byrne <https://github.com/djbyrne>`
 
-            - `Donal Byrne <https://github.com/djbyrne>`
+    Example:
 
-        Example:
+        >>> from pl_bolts.models.rl.per_dqn.model import PERDQN
+        ...
+        >>> model = PERDQN("PongNoFrameskip-v4")
 
-            >>> from pl_bolts.models.rl.per_dqn.model import PERDQN
-            ...
-            >>> model = PERDQN("PongNoFrameskip-v4")
+    Train::
 
-        Train::
+        trainer = Trainer()
+        trainer.fit(model)
 
-            trainer = Trainer()
-            trainer.fit(model)
-
-        Args:
-            env: gym environment tag
-            gpus: number of gpus being used
-            eps_start: starting value of epsilon for the epsilon-greedy exploration
-            eps_end: final value of epsilon for the epsilon-greedy exploration
-            eps_last_frame: the final frame in for the decrease of epsilon. At this frame espilon = eps_end
-            sync_rate: the number of iterations between syncing up the target network with the train network
-            gamma: discount factor
-            lr: learning rate
-            batch_size: size of minibatch pulled from the DataLoader
-            replay_size: total capacity of the replay buffer
-            warm_start_size: how many random steps through the environment to be carried out at the start of
-            training to fill the buffer with a starting point
-            sample_len: the number of samples to pull from the dataset iterator and feed to the DataLoader
-        """
-        super().__init__(env, gpus, eps_start, eps_end, eps_last_frame, sync_rate, gamma, lr, batch_size, replay_size,
-                         warm_start_size, sample_len)
-
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-        self.source = ExperienceSource(self.env, self.agent, device)
-        self.buffer = PERBuffer(self.replay_size)
+    Args:
+        env: gym environment tag
+        gpus: number of gpus being used
+        eps_start: starting value of epsilon for the epsilon-greedy exploration
+        eps_end: final value of epsilon for the epsilon-greedy exploration
+        eps_last_frame: the final frame in for the decrease of epsilon. At this frame espilon = eps_end
+        sync_rate: the number of iterations between syncing up the target network with the train network
+        gamma: discount factor
+        lr: learning rate
+        batch_size: size of minibatch pulled from the DataLoader
+        replay_size: total capacity of the replay buffer
+        warm_start_size: how many random steps through the environment to be carried out at the start of
+        training to fill the buffer with a starting point
+        sample_len: the number of samples to pull from the dataset iterator and feed to the DataLoader
+    """
 
     def training_step(self, batch, _) -> OrderedDict:
         """
@@ -157,11 +144,11 @@ class PERDQN(DQN):
         losses_v = batch_weights * loss
         return losses_v.mean(), (losses_v + 1e-5).data.cpu().numpy()
 
-    def _dataloader(self) -> DataLoader:
+    def prepare_data(self) -> None:
         """Initialize the Replay Buffer dataset used for retrieving experiences"""
+        device = torch.device(self.trainer.root_gpu) if self.trainer.gpus >= 1 else self.device
+        self.source = ExperienceSource(self.env, self.agent, device)
         self.buffer = PERBuffer(self.replay_size)
         self.populate(self.warm_start_size)
 
-        dataset = PrioRLDataset(self.buffer, self.batch_size)
-        dataloader = DataLoader(dataset=dataset, batch_size=self.batch_size,)
-        return dataloader
+        self.dataset = PrioRLDataset(self.buffer, self.batch_size)
