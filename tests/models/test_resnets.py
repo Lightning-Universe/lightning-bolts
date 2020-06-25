8e938e722d6f0ac23ddfb15f424868d3c3c6e096
William Falcon <waf2107@columbia.edu>
2020-06-25 00:14:15 -0400

    encoder

--- tests/models/test_resnets.py
+++ tests/models/test_resnets.py
@@ -3,10 +3,27 @@ import torch
 
 from pl_bolts.models.self_supervised.resnets import (
     resnet18,
+    resnet34,
+    resnet50,
+    resnet101,
+    resnet152,
+    resnext50_32x4d,
+    resnext101_32x8d,
+    wide_resnet50_2,
+    wide_resnet101_2
 )
 
+
 @pytest.mark.parametrize("model_class", [
-    resnet18
+    resnet18,
+    resnet34,
+    resnet50,
+    resnet101,
+    resnet152,
+    resnext50_32x4d,
+    resnext101_32x8d,
+    wide_resnet50_2,
+    wide_resnet101_2
 ])
 def test_resnets(tmpdir, model_class):
     x = torch.rand(3, 3, 64, 64)
