c92fc820ee62844342d48a394f9d8abcc5742af0
William Falcon <waf2107@columbia.edu>
2020-06-29 13:34:03 -0400

    fixed base transformer

--- tests/datamodules/test_datamodules.py
+++ tests/datamodules/test_datamodules.py
@@ -1,4 +1,5 @@
 from pl_bolts.datamodules.cifar10_dataset import CIFAR10
+from pl_bolts.datamodules import DACDataModule
 
 
 def test_dev_datasets(tmpdir):
@@ -6,3 +7,16 @@ def test_dev_datasets(tmpdir):
     ds = CIFAR10(tmpdir)
     for b in ds:
         pass
+
+
+def test_dac_dm(tmpdir):
+
+    dm = DACDataModule(data_dir=tmpdir, use_tiny_dac=True)
+    dm.prepare_data()
+
+    for b in dm.train_dataloader():
+        break
+    for b in dm.val_dataloader():
+        break
+    for b in dm.test_dataloader():
+        break
