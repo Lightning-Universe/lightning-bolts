8ffe90f74daf041b22a505ba6b8e542ad242d5cf
Donal Byrne <donaljbyrne@me.com>
2020-06-29 18:05:51 +0100

    Rl docs update (#93)
    * Updated RL docs with latest models
    
    * Updated rl docs with latest models
    
    * updated rl docs to include baseline images and general clean up
    
    * Fixed errors in rl docs
    
    * move
    
    * convert to JPG
    
    * moving
    
    * latex
    
    * formatting
    
    Co-authored-by: Donal <donal.byrne@xperi.com>
    Co-authored-by: Jirka <jirka@pytorchlightning.ai>

--- pl_bolts/models/rl/dqn_model.py
+++ pl_bolts/models/rl/dqn_model.py
@@ -1,50 +1,5 @@
 """
 Deep Q Network
-==============
-
-The DQN was introduced in [Playing Atari with Deep Reinforcement Learning](https://arxiv.org/abs/1312.5602) by
-researchers at DeepMind. This took the concept of tabular Q learning and scaled it to much larger problems by
-apporximating the Q function using a deep neural network.
-
-The goal behind DQN was to take the simple control method of Q learning and scale it up in order to solve complicated
- tasks. As well as this, the method needed to be stable. The DQN solves these issues with the following additions.
-
-1. Approximated Q Function
-^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-Storing Q values in a table works well in theory, but is completely unscalable. Instead, the authors apporximate the
-Q function using a deep neural network. This allows the DQN to be used for much more complicated tasks
-
-2. Replay Buffer
-^^^^^^^^^^^^^^^^
-
-Similar to supervised learning, the DQN learns on randomly sampled batches of previous data stored in an
-Experience Replay Buffer. The 'target' is calculated using the Bellman equation
-
-.. math::
-
-    Q(s,a)<-(r+{\\gamma}\\max_{a'{\\in}A}Q(s',a'))^2
-
-and then we optimize using SGD just like a standard supervised learning problem.
-
-.. math::
-
-    L=(Q(s,a)-(r+{\\gamma}\\max_{a'{\\in}A}Q(s',a'))^2
-
-Benefits
---------
-
-Implementation
---------------
-
-Results
--------
-
-![DQN Basline Results](../../docs/images/pong_dqn_baseline_results.png)
-
-References
-----------
-
 """
 
 import argparse
@@ -86,6 +41,7 @@ class DQN(pl.LightningModule):
     ):
         """
         PyTorch Lightning implementation of `DQN <https://arxiv.org/abs/1312.5602>`_
+
 
         Paper authors: Volodymyr Mnih, Koray Kavukcuoglu, David Silver, Alex Graves,
         Ioannis Antonoglou, Daan Wierstra, Martin Riedmiller.
