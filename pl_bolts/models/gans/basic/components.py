753a8d1b7c835867dcb01d43a53a9f91bd4696c2
William Falcon <waf2107@columbia.edu>
2020-06-30 08:31:48 -0400

    pretrains

--- pl_bolts/models/gans/basic/components.py
+++ pl_bolts/models/gans/basic/components.py
@@ -1,49 +1,45 @@
 import numpy as np
 from torch import nn
+import torch.nn.functional as F
+import torch
 
 
 class Generator(nn.Module):
-    def __init__(self, latent_dim, img_shape):
+    def __init__(self, latent_dim, img_shape, hidden_dim=256):
         super().__init__()
+        feats = int(np.prod(img_shape))
         self.img_shape = img_shape
+        self.fc1 = nn.Linear(latent_dim, hidden_dim)
+        self.fc2 = nn.Linear(self.fc1.out_features, self.fc1.out_features * 2)
+        self.fc3 = nn.Linear(self.fc2.out_features, self.fc2.out_features * 2)
+        self.fc4 = nn.Linear(self.fc3.out_features, feats)
 
-        def block(in_feat, out_feat, normalize=True):
-            layers = [nn.Linear(in_feat, out_feat)]
-            if normalize:
-                layers.append(nn.BatchNorm1d(out_feat, 0.8))
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
-            return layers
-
-        self.model = nn.Sequential(
-            *block(latent_dim, 128, normalize=False),
-            *block(128, 256),
-            *block(256, 512),
-            *block(512, 1024),
-            nn.Linear(1024, int(np.prod(img_shape))),
-            nn.Tanh()
-        )
-
+    # forward method
     def forward(self, z):
-        img = self.model(z)
+        z = F.leaky_relu(self.fc1(z), 0.2)
+        z = F.leaky_relu(self.fc2(z), 0.2)
+        z = F.leaky_relu(self.fc3(z), 0.2)
+        img = torch.tanh(self.fc4(z))
         img = img.view(img.size(0), *self.img_shape)
         return img
 
 
 class Discriminator(nn.Module):
-    def __init__(self, img_shape):
+    def __init__(self, img_shape, hidden_dim=1024):
         super().__init__()
+        in_dim = int(np.prod(img_shape))
+        self.fc1 = nn.Linear(in_dim, hidden_dim)
+        self.fc2 = nn.Linear(self.fc1.out_features, self.fc1.out_features // 2)
+        self.fc3 = nn.Linear(self.fc2.out_features, self.fc2.out_features // 2)
+        self.fc4 = nn.Linear(self.fc3.out_features, 1)
 
-        self.model = nn.Sequential(
-            nn.Linear(int(np.prod(img_shape)), 512),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 256),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(256, 1),
-            nn.Sigmoid(),
-        )
-
+    # forward method
     def forward(self, img):
-        img_flat = img.view(img.size(0), -1)
-        validity = self.model(img_flat)
-
-        return validity
+        x = img.view(img.size(0), -1)
+        x = F.leaky_relu(self.fc1(x), 0.2)
+        x = F.dropout(x, 0.3)
+        x = F.leaky_relu(self.fc2(x), 0.2)
+        x = F.dropout(x, 0.3)
+        x = F.leaky_relu(self.fc3(x), 0.2)
+        x = F.dropout(x, 0.3)
+        return torch.sigmoid(self.fc4(x))
