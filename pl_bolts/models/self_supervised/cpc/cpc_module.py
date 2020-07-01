8c640bdf6c9b73ed2c97c4256b5ba379a53aaf13
William Falcon <waf2107@columbia.edu>
2020-07-01 13:17:56 -0400

    0.8.4

--- pl_bolts/models/self_supervised/cpc/cpc_module.py
+++ pl_bolts/models/self_supervised/cpc/cpc_module.py
@@ -352,8 +352,6 @@ if __name__ == '__main__':
         datamodule = CIFAR10DataModule.from_argparse_args(args)
         datamodule.train_transforms = CPCTrainTransformsCIFAR10()
         datamodule.val_transforms = CPCEvalTransformsCIFAR10()
-        args.patch_size = datamodule.val_transforms.patch_size
-        args.overlap = datamodule.val_transforms.overlap
 
     elif args.dataset == 'stl10':
         datamodule = STL10DataModule.from_argparse_args(args)
@@ -361,8 +359,6 @@ if __name__ == '__main__':
         datamodule.val_dataloader = datamodule.val_dataloader_mixed
         datamodule.train_transforms = CPCTrainTransformsSTL10()
         datamodule.val_transforms = CPCEvalTransformsSTL10()
-        args.patch_size = datamodule.val_transforms.patch_size
-        args.overlap = datamodule.val_transforms.overlap
 
     elif args.dataset == 'imagenet2012':
         datamodule = SSLImagenetDataModule.from_argparse_args(args)
