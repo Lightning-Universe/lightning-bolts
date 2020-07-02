27c1e08aad29b4187e8595f0b090bfa93d9fef7b
Jirka Borovec <Borda@users.noreply.github.com>
2020-07-02 13:02:45 +0200

    move Trains logger from PL (#86)
    * add Trains logger
    
    * req
    
    * import
    
    * import
    
    * docs
    
    * docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -416,6 +416,8 @@ import importlib
 import os
 import torch
 
+import pytorch_lightning as pl
+from pytorch_lightning import Trainer, LightningModule
 from pytorch_lightning.utilities import NATIVE_AMP_AVALAIBLE
 APEX_AVAILABLE = importlib.util.find_spec("apex") is not None
 XLA_AVAILABLE = importlib.util.find_spec("torch_xla") is not None
