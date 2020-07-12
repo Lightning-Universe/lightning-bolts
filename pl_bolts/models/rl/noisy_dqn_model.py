ad544602866edae8f54d7252a35ed88868d35e85
Donal Byrne <donaljbyrne@me.com>
2020-07-12 09:10:24 +0100

    Refactored noisy dqn to use train_batch structure

--- pl_bolts/models/rl/noisy_dqn_model.py
+++ pl_bolts/models/rl/noisy_dqn_model.py
@@ -76,42 +76,26 @@ class NoisyDQN(DQN):
         Returns:
             Training loss and log metrics
         """
-        # step through environment with agent and add to buffer
-        exp, reward, done = self.source.step(self.device)
-        self.buffer.append(exp)
-
-        self.episode_reward += reward
-        self.episode_steps += 1
-
         # calculates training loss
         loss = dqn_loss(batch, self.net, self.target_net)
 
         if self.trainer.use_dp or self.trainer.use_ddp2:
             loss = loss.unsqueeze(0)
 
-        if done:
-            self.total_reward = self.episode_reward
-            self.reward_list.append(self.total_reward)
-            self.avg_reward = sum(self.reward_list[-100:]) / 100
-            self.episode_count += 1
-            self.episode_reward = 0
-            self.total_episode_steps = self.episode_steps
-            self.episode_steps = 0
-
         # Soft update of target network
         if self.global_step % self.sync_rate == 0:
             self.target_net.load_state_dict(self.net.state_dict())
 
         log = {
-            "total_reward": torch.tensor(self.total_reward).to(self.device),
-            "avg_reward": torch.tensor(self.avg_reward),
+            "total_reward": self.total_reward,
+            "avg_reward": self.avg_reward,
             "train_loss": loss,
-            "episode_steps": torch.tensor(self.total_episode_steps),
+            "episode_steps": self.total_episode_steps,
         }
         status = {
-            "steps": torch.tensor(self.global_step).to(self.device),
-            "avg_reward": torch.tensor(self.avg_reward),
-            "total_reward": torch.tensor(self.total_reward).to(self.device),
+            "steps": self.global_step,
+            "avg_reward": self.avg_reward,
+            "total_reward": self.total_reward,
             "episodes": self.episode_count,
             "episode_steps": self.episode_steps,
             "epsilon": self.agent.epsilon,
@@ -120,7 +104,7 @@ class NoisyDQN(DQN):
         return OrderedDict(
             {
                 "loss": loss,
-                "avg_reward": torch.tensor(self.avg_reward),
+                "avg_reward": self.avg_reward,
                 "log": log,
                 "progress_bar": status,
             }
