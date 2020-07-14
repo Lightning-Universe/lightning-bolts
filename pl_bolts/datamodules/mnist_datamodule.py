6f2448824d044aa3286ee6d8434f97ab3e1413e0
Ananya Harsh Jha <ahj265@nyu.edu>
2020-07-14 03:04:13 -0400

    fixed test_gan.py

--- pl_bolts/datamodules/mnist_datamodule.py
+++ pl_bolts/datamodules/mnist_datamodule.py
@@ -132,7 +132,7 @@ class MNISTDataModule(LightningDataModule):
         if self.normalize:
             mnist_transforms = transform_lib.Compose([
                 transform_lib.ToTensor(),
-                transform_lib.Normalize(mean=0.5, std=0.5),
+                transform_lib.Normalize(mean=(0.5,), std=(0.5,)),
             ])
         else:
             mnist_transforms = transform_lib.ToTensor()
