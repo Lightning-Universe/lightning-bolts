7a2ee0a69303af7c26b906273023cba0402ae9c6
Hengjian Jia <henryjia18@gmail.com>
2020-07-26 15:43:17 +0100

    Correct formatting in tests

--- tests/datamodules/test_dataloader.py
+++ tests/datamodules/test_dataloader.py
@@ -7,7 +7,7 @@ from pl_bolts.datamodules.async_dataloader import AsynchronousLoader
 def test_async_dataloader(tmpdir):
     ds = CIFAR10(tmpdir)
 
-    if torch.cuda.device_count() > 0: # Can only run this test with a GPU
+    if torch.cuda.device_count() > 0:  # Can only run this test with a GPU
         device = torch.device('cuda', 0)
         dataloader = AsynchronousLoader(ds, device=device)
 
