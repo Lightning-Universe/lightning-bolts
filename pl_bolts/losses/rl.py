0a07a3881013b562c74a1f60ceb7cd1ba61ff6cc
Donal Byrne <donaljbyrne@me.com>
2020-07-28 19:00:49 +0100

    Update pl_bolts/losses/rl.py
    Co-authored-by: Jirka Borovec <Borda@users.noreply.github.com>

--- pl_bolts/losses/rl.py
+++ pl_bolts/losses/rl.py
@@ -25,9 +25,7 @@ def dqn_loss(batch: Tuple[torch.Tensor, torch.Tensor], net: nn.Module,
     """
     states, actions, rewards, dones, next_states = batch
 
-    actions = actions.long()
-
-    actions = actions.squeeze(-1)
+    actions = actions.long().squeeze(-1)
 
     state_action_values = (
         net(states).gather(1, actions.unsqueeze(-1)).squeeze(-1)
