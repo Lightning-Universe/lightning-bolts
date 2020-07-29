5cff0331d1162411911b3d95219a1ad725a29b1d
nateraw <nxr9266@g.rit.edu>
2020-07-28 22:37:20 -0600

    :white_check_mark: add random test to make coverage pass

--- tests/models/test_autoencoders.py
+++ tests/models/test_autoencoders.py
@@ -1,6 +1,8 @@
 import pytorch_lightning as pl
+import torch
 
 from pl_bolts.models.autoencoders import VAE, AE
+from pl_bolts.models.autoencoders.basic_ae import AEEncoder
 from tests import reset_seed
 
 
@@ -23,3 +25,19 @@ def test_ae(tmpdir):
     trainer = pl.Trainer(fast_dev_run=True, default_root_dir=tmpdir)
     trainer.fit(model)
     trainer.test(model)
+
+
+def test_basic_ae_encoder(tmpdir):
+    reset_seed()
+
+    hidden_dim = 128
+    latent_dim = 2
+    width = height = 28
+    batch_size = 16
+    channels = 1
+
+    encoder = AEEncoder(hidden_dim, latent_dim, width, height)
+    x = torch.randn(batch_size, channels, width, height)
+    z = encoder(x)
+
+    assert z.shape == (batch_size, latent_dim)
