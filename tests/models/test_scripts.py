02e3eeaa37c7f2ccd40d908768192d1e2e05c2ad
Jirka Borovec <jirka.borovec@seznam.cz>
2020-08-05 00:53:10 +0200

    cli: GAN

--- tests/models/test_scripts.py
+++ tests/models/test_scripts.py
@@ -32,3 +32,12 @@ def test_cli_run_basic_vae(cli_args):
     with mock.patch("argparse._sys.argv", ["any.py"] + cli_args):
         run_cli()
 
+
+@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3'])
+def test_cli_run_gan(cli_args):
+    """Test running CLI for an example with default params."""
+    from pl_bolts.models.gans.basic.basic_gan_module import run_cli
+
+    cli_args = cli_args.split(' ') if cli_args else []
+    with mock.patch("argparse._sys.argv", ["any.py"] + cli_args):
+        run_cli()
