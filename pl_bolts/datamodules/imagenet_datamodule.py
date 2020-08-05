c53a15d7b43f03e9c96257ddfa8d539a68ca6ee4
William Falcon <waf2107@columbia.edu>
2020-08-05 17:59:00 -0400

    docs

--- pl_bolts/datamodules/imagenet_datamodule.py
+++ pl_bolts/datamodules/imagenet_datamodule.py
@@ -24,6 +24,15 @@ class ImagenetDataModule(LightningDataModule):
             **kwargs,
     ):
         """
+        .. figure:: https://3qeqpr26caki16dnhd19sv6by6v-wpengine.netdna-ssl.com/wp-content/uploads/2017/08/
+            Sample-of-Images-from-the-ImageNet-Dataset-used-in-the-ILSVRC-Challenge.png
+            :width: 400
+            :alt: Imagenet
+
+        Specs:
+            - 1000 classes
+            - Each image is (3 x varies x varies) (here we default to 3 x 224 x 224)
+
         Imagenet train, val and test dataloaders.
 
         The train set is the imagenet train.
