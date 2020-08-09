7f8dc245c8f9b5ee2ed3737c9480c69d1af25c01
William Falcon <waf2107@columbia.edu>
2020-08-09 07:28:03 -0400

    pl 0.9 update

---
+++ tests/models/test_executable_scripts.py
@@ -0,0 +1,20 @@
+from unittest import mock
+import pytest
+
+
+@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --limit_train_batches 3 --limit_val_batches 3'])
+def test_cli_run_rl_dqn(cli_args):
+    from pl_bolts.models.gans.basic.basic_gan_module import cli_main
+
+    cli_args = cli_args.split(' ') if cli_args else []
+    with mock.patch("argparse._sys.argv", ["any.py"] + cli_args):
+        cli_main()
+
+
+@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --limit_train_batches 3 --limit_val_batches 3'])
+def test_cli_run_rl_dqn(cli_args):
+    from pl_bolts.models.autoencoders.basic_vae.basic_vae_module import cli_main
+
+    cli_args = cli_args.split(' ') if cli_args else []
+    with mock.patch("argparse._sys.argv", ["any.py"] + cli_args):
+        cli_main()
