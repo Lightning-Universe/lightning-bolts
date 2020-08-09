6d9c174899b360ba6cd02a435cae3346413498f3
William Falcon <waf2107@columbia.edu>
2020-08-09 17:22:33 -0400

    update cpc

--- tests/models/test_executable_scripts.py
+++ tests/models/test_executable_scripts.py
@@ -2,7 +2,7 @@ from unittest import mock
 import pytest
 
 
-@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --limit_train_batches 3 --limit_val_batches 3'])
+@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --limit_train_batches 3 --limit_val_batches 3 --batch_size 3'])
 def test_cli_basic_gan(cli_args):
     from pl_bolts.models.gans.basic.basic_gan_module import cli_main
 
@@ -11,7 +11,7 @@ def test_cli_basic_gan(cli_args):
         cli_main()
 
 
-@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --limit_train_batches 3 --limit_val_batches 3'])
+@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --limit_train_batches 3 --limit_val_batches 3 --batch_size 3'])
 def test_cli_basic_vae(cli_args):
     from pl_bolts.models.autoencoders.basic_vae.basic_vae_module import cli_main
 
@@ -20,7 +20,8 @@ def test_cli_basic_vae(cli_args):
         cli_main()
 
 
-@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --limit_train_batches 3 --limit_val_batches 3'])
+@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --limit_train_batches 3 '
+                                      '--limit_val_batches 3 --batch_size 3 --encoder resnet18'])
 def test_cli_cpc(cli_args):
     from pl_bolts.models.self_supervised.cpc.cpc_module import cli_main
 
