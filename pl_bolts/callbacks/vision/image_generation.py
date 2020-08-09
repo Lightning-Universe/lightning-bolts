2bc042849484d6e9386685a418046d5ba53634af
William Falcon <waf2107@columbia.edu>
2020-08-09 07:21:54 -0400

    pl 0.9 update

--- pl_bolts/callbacks/vision/image_generation.py
+++ pl_bolts/callbacks/vision/image_generation.py
@@ -3,13 +3,17 @@ from pytorch_lightning import Callback
 
 
 class TensorboardGenerativeModelImageSampler(Callback):
-    def __init__(self):
+    def __init__(self, num_samples: int = 3):
         """
         Generates images and logs to tensorboard.
         Your model must implement the forward function for generation
 
         Requirements::
 
+            # model must have img_dim arg
+            model.img_dim = (1, 28, 28)
+
+            # model forward must work for sampling
             z = torch.rand(batch_size, latent_dim)
             img_samples = your_model(z)
 
@@ -20,15 +24,19 @@ class TensorboardGenerativeModelImageSampler(Callback):
             trainer = Trainer(callbacks=[TensorboardGenerativeModelImageSampler()])
         """
         super().__init__()
+        self.num_samples = num_samples
 
     def on_epoch_end(self, trainer, pl_module):
         import torchvision
 
-        num_samples = 3
-        z = torch.randn(num_samples, pl_module.hparams.latent_dim, device=pl_module.device)
+        z = torch.randn(self.num_samples, pl_module.hparams.latent_dim, device=pl_module.device)
 
         # generate images
         images = pl_module(z)
+
+        if len(images.size()) == 2:
+            img_dim = pl_module.img_dim
+            images = images.view(self.num_samples, *img_dim)
 
         grid = torchvision.utils.make_grid(images)
         trainer.logger.experiment.add_image('images', grid, global_step=trainer.global_step)
