dc2ec915e12fba609ddbe878f465b69c132146d9
William Falcon <waf2107@columbia.edu>
2020-08-09 17:10:07 -0400

    update cpc

--- pl_bolts/models/self_supervised/cpc/cpc_module.py
+++ pl_bolts/models/self_supervised/cpc/cpc_module.py
@@ -8,11 +8,8 @@ from typing import Union
 
 import pytorch_lightning as pl
 import torch
-import torch.nn.functional as F
 import torch.optim as optim
 from pytorch_lightning.utilities import rank_zero_warn
-
-from pl_bolts import metrics
 from pl_bolts.datamodules import CIFAR10DataModule, STL10DataModule
 from pl_bolts.datamodules.ssl_imagenet_datamodule import SSLImagenetDataModule
 from pl_bolts.losses.self_supervised_learning import CPCTask
