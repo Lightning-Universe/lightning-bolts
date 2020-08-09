2fe32b4eddca554f6fdb0d79a133bea4c4885a4c
William Falcon <waf2107@columbia.edu>
2020-08-09 09:51:45 -0400

    pl 0.9 update

---
+++ pl_bolts/utils/shaping.py
@@ -0,0 +1,12 @@
+import torch
+import numpy as np
+
+
+def tile(a, dim, n_tile):
+    init_dim = a.size(dim)
+    repeat_idx = [1] * a.dim()
+    repeat_idx[dim] = n_tile
+    a = a.repeat(*repeat_idx)
+    order_index = torch.LongTensor(np.concatenate([init_dim * np.arange(n_tile) + i for i in range(init_dim)]))
+    order_index = order_index.type_as(a).long()
+    return torch.index_select(a, dim, order_index)
