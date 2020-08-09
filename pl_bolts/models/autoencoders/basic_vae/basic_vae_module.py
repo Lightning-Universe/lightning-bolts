686eb2a32da3429dcade112b6c526b99b28b9e06
William Falcon <waf2107@columbia.edu>
2020-08-09 13:31:31 -0400

    pl 0.9 update

--- pl_bolts/models/autoencoders/basic_vae/basic_vae_module.py
+++ pl_bolts/models/autoencoders/basic_vae/basic_vae_module.py
@@ -145,7 +145,7 @@ class VAE(pl.LightningModule):
         kl_div = (log_qz - log_pz).sum(dim=2)
 
         # we used monte carlo sampling to estimate. average across samples
-        kl_div = kl_div.mean(-1)
+        # kl_div = kl_div.mean(-1)
 
         # ----------------------
         # Reconstruction loss
@@ -161,10 +161,10 @@ class VAE(pl.LightningModule):
 
         # sum across dimensions because sum of log probabilities of iid univariate gaussians is the same as
         # multivariate gaussian
-        recon_loss = recon_loss.sum(dim=-1)\
+        recon_loss = recon_loss.sum(dim=-1)
 
         # we used monte carlo sampling to estimate. average across samples
-        recon_loss = recon_loss.mean(-1)
+        # recon_loss = recon_loss.mean(-1)
 
         # ELBO = reconstruction + KL
         loss = recon_loss + kl_div
