887f8c16ef1f41a408b8b3aa767b9a178940bc60
William Falcon <waf2107@columbia.edu>
2020-08-09 07:49:27 -0400

    pl 0.9 update

--- tests/models/test_executable_scripts.py
+++ tests/models/test_executable_scripts.py
@@ -3,7 +3,7 @@ import pytest
 
 
 @pytest.mark.parametrize('cli_args', ['--max_epochs 1 --limit_train_batches 3 --limit_val_batches 3'])
-def test_cli_run_rl_dqn(cli_args):
+def test_cli_basic_gan(cli_args):
     from pl_bolts.models.gans.basic.basic_gan_module import cli_main
 
     cli_args = cli_args.split(' ') if cli_args else []
@@ -12,7 +12,7 @@ def test_cli_run_rl_dqn(cli_args):
 
 
 @pytest.mark.parametrize('cli_args', ['--max_epochs 1 --limit_train_batches 3 --limit_val_batches 3'])
-def test_cli_run_rl_dqn(cli_args):
+def test_cli_basic_vae(cli_args):
     from pl_bolts.models.autoencoders.basic_vae.basic_vae_module import cli_main
 
     cli_args = cli_args.split(' ') if cli_args else []
