962463f09ad4778fa2ca0f52e6a3d99f9f2ef6f0
William Falcon <waf2107@columbia.edu>
2020-08-09 12:39:08 -0400

    pl 0.9 update

---
+++ pl_bolts/datamodules/binary_mnist_datamodule.py
@@ -0,0 +1,188 @@
+import torch
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms as transform_lib
+from torchvision.datasets import MNIST
+from PIL import Image
+
+
+class BinaryMNISTDataModule(LightningDataModule):
+
+    name = 'mnist'
+
+    def __init__(
+            self,
+            data_dir: str,
+            val_split: int = 5000,
+            num_workers: int = 16,
+            normalize: bool = False,
+            seed: int = 42,
+            *args,
+            **kwargs,
+    ):
+        """
+        .. figure:: https://miro.medium.com/max/744/1*AO2rIhzRYzFVQlFLx9DM9A.png
+            :width: 400
+            :alt: MNIST
+
+        Specs:
+            - 10 classes (1 per digit)
+            - Each image is (1 x 28 x 28)
+
+        Binary MNIST, train, val, test splits and transforms
+
+        Transforms::
+
+            mnist_transforms = transform_lib.Compose([
+                transform_lib.ToTensor()
+            ])
+
+        Example::
+
+            from pl_bolts.datamodules import BinaryMNISTDataModule
+
+            dm = BinaryMNISTDataModule('.')
+            model = LitModel()
+
+            Trainer().fit(model, dm)
+
+        Args:
+            data_dir: where to save/load the data
+            val_split: how many of the training images to use for the validation split
+            num_workers: how many workers to use for loading data
+            normalize: If true applies image normalize
+        """
+        super().__init__(*args, **kwargs)
+        self.dims = (1, 28, 28)
+        self.data_dir = data_dir
+        self.val_split = val_split
+        self.num_workers = num_workers
+        self.normalize = normalize
+        self.seed = seed
+
+    @property
+    def num_classes(self):
+        """
+        Return:
+            10
+        """
+        return 10
+
+    def prepare_data(self):
+        """
+        Saves MNIST files to data_dir
+        """
+        MNIST(self.data_dir, train=True, download=True, transform=transform_lib.ToTensor())
+        MNIST(self.data_dir, train=False, download=True, transform=transform_lib.ToTensor())
+
+    def train_dataloader(self, batch_size=32, transforms=None):
+        """
+        MNIST train set removes a subset to use for validation
+
+        Args:
+            batch_size: size of batch
+            transforms: custom transforms
+        """
+        transforms = transforms or self.train_transforms or self._default_transforms()
+
+        dataset = BinaryMNIST(self.data_dir, train=True, download=False, transform=transforms)
+        train_length = len(dataset)
+        dataset_train, _ = random_split(
+            dataset,
+            [train_length - self.val_split, self.val_split],
+            generator=torch.Generator().manual_seed(self.seed)
+        )
+        loader = DataLoader(
+            dataset_train,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+            drop_last=True,
+            pin_memory=True
+        )
+        return loader
+
+    def val_dataloader(self, batch_size=32, transforms=None):
+        """
+        MNIST val set uses a subset of the training set for validation
+
+        Args:
+            batch_size: size of batch
+            transforms: custom transforms
+        """
+        transforms = transforms or self.val_transforms or self._default_transforms()
+        dataset = BinaryMNIST(self.data_dir, train=True, download=True, transform=transforms)
+        train_length = len(dataset)
+        _, dataset_val = random_split(
+            dataset,
+            [train_length - self.val_split, self.val_split],
+            generator=torch.Generator().manual_seed(self.seed)
+        )
+        loader = DataLoader(
+            dataset_val,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            drop_last=True,
+            pin_memory=True
+        )
+        return loader
+
+    def test_dataloader(self, batch_size=32, transforms=None):
+        """
+        MNIST test set uses the test split
+
+        Args:
+            batch_size: size of batch
+            transforms: custom transforms
+        """
+        transforms = transforms or self.val_transforms or self._default_transforms()
+
+        dataset = BinaryMNIST(self.data_dir, train=False, download=False, transform=transforms)
+        loader = DataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            drop_last=True,
+            pin_memory=True
+        )
+        return loader
+
+    def _default_transforms(self):
+        if self.normalize:
+            mnist_transforms = transform_lib.Compose([
+                transform_lib.ToTensor(),
+                transform_lib.Normalize(mean=(0.5,), std=(0.5,)),
+            ])
+        else:
+            mnist_transforms = transform_lib.ToTensor()
+
+        return mnist_transforms
+
+
+class BinaryMNIST(MNIST):
+    def __getitem__(self, idx):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[idx], int(self.targets[idx])
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img.numpy(), mode='L')
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        # binary
+        img[img < 0.5] = 0.0
+        img[img >= 0.5] = 1.0
+
+        return img, target
