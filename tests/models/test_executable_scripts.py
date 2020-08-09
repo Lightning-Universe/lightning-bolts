51cf0af2a2ddd68e61fc5a955965679b6a97dae4
William Falcon <waf2107@columbia.edu>
2020-08-09 17:09:44 -0400

    update cpc

--- tests/models/test_executable_scripts.py
+++ tests/models/test_executable_scripts.py
@@ -18,3 +18,12 @@ def test_cli_basic_vae(cli_args):
     cli_args = cli_args.split(' ') if cli_args else []
     with mock.patch("argparse._sys.argv", ["any.py"] + cli_args):
         cli_main()
+
+
+@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --limit_train_batches 3 --limit_val_batches 3'])
+def test_cli_cpc(cli_args):
+    from pl_bolts.models.self_supervised.cpc.cpc_module import cli_main
+
+    cli_args = cli_args.split(' ') if cli_args else []
+    with mock.patch("argparse._sys.argv", ["any.py"] + cli_args):
+        cli_main()
