ac3b17ed7e4b8ad7c1a5cf29dd1563e071c38978
Teddy Koker <teddy.koker@gmail.com>
2020-08-13 11:53:04 -0400

    fixed vocdetection dataset

--- pl_bolts/datamodules/vocdetection_datamodule.py
+++ pl_bolts/datamodules/vocdetection_datamodule.py
@@ -2,7 +2,85 @@ import torch
 from pytorch_lightning import LightningDataModule
 from torch.utils.data import DataLoader
 from torchvision.datasets import VOCDetection
-from torchvision import transforms as transform_lib
+import torchvision.transforms as T
+
+
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, image, target):
+        for t in self.transforms:
+            image, target = t(image, target)
+        return image, target
+
+
+CLASSES = (
+    "__background__ ",
+    "aeroplane",
+    "bicycle",
+    "bird",
+    "boat",
+    "bottle",
+    "bus",
+    "car",
+    "cat",
+    "chair",
+    "cow",
+    "diningtable",
+    "dog",
+    "horse",
+    "motorbike",
+    "person",
+    "pottedplant",
+    "sheep",
+    "sofa",
+    "train",
+    "tvmonitor",
+)
+
+
+def _prepare_voc_instance(image, target):
+    """
+    Prepares VOC dataset into appropriate target for fasterrcnn
+
+    https://github.com/pytorch/vision/issues/1097#issuecomment-508917489
+    """
+    anno = target["annotation"]
+    h, w = anno["size"]["height"], anno["size"]["width"]
+    boxes = []
+    classes = []
+    area = []
+    iscrowd = []
+    objects = anno["object"]
+    if not isinstance(objects, list):
+        objects = [objects]
+    for obj in objects:
+        bbox = obj["bndbox"]
+        bbox = [int(bbox[n]) - 1 for n in ["xmin", "ymin", "xmax", "ymax"]]
+        boxes.append(bbox)
+        classes.append(CLASSES.index(obj["name"]))
+        iscrowd.append(int(obj["difficult"]))
+        area.append((bbox[2] - bbox[0]) * (bbox[3] - bbox[1]))
+
+    boxes = torch.as_tensor(boxes, dtype=torch.float32)
+    classes = torch.as_tensor(classes)
+    area = torch.as_tensor(area)
+    iscrowd = torch.as_tensor(iscrowd)
+
+    image_id = anno["filename"][5:-4]
+    image_id = torch.as_tensor([int(image_id)])
+
+    target = {}
+    target["boxes"] = boxes
+    target["labels"] = classes
+    target["image_id"] = image_id
+
+    # for conversion to coco api
+    target["area"] = area
+    target["iscrowd"] = iscrowd
+
+    return image, target
 
 
 class VOCDetectionDataModule(LightningDataModule):
@@ -42,7 +120,7 @@ class VOCDetectionDataModule(LightningDataModule):
         VOCDetection(self.data_dir, year=self.year, image_set="train", download=True)
         VOCDetection(self.data_dir, year=self.year, image_set="val", download=True)
 
-    def train_dataloader(self, batch_size=8, transforms=None):
+    def train_dataloader(self, batch_size=1, transforms=None):
         """
         VOCDetection train set uses the `train` subset
 
@@ -50,9 +128,14 @@ class VOCDetectionDataModule(LightningDataModule):
             batch_size: size of batch
             transforms: custom transforms
         """
+        t = [_prepare_voc_instance]
         transforms = transforms or self.train_transforms or self._default_transforms()
+        if transforms is not None:
+            t.append(transforms)
+        transforms = Compose(t)
+
         dataset = VOCDetection(
-            self.data_dir, year=self.year, image_set="train", transform=transforms
+            self.data_dir, year=self.year, image_set="train", transforms=transforms
         )
         loader = DataLoader(
             dataset,
@@ -63,7 +146,7 @@ class VOCDetectionDataModule(LightningDataModule):
         )
         return loader
 
-    def val_dataloader(self, batch_size=8, transforms=None):
+    def val_dataloader(self, batch_size=1, transforms=None):
         """
         VOCDetection val set uses the `val` subset
 
@@ -71,9 +154,13 @@ class VOCDetectionDataModule(LightningDataModule):
             batch_size: size of batch
             transforms: custom transforms
         """
+        t = [_prepare_voc_instance]
         transforms = transforms or self.val_transforms or self._default_transforms()
+        if transforms is not None:
+            t.append(transforms)
+        transforms = Compose(t)
         dataset = VOCDetection(
-            self.data_dir, year=self.year, image_set="trainval", transform=transforms
+            self.data_dir, year=self.year, image_set="val", transforms=transforms
         )
         loader = DataLoader(
             dataset,
@@ -86,17 +173,20 @@ class VOCDetectionDataModule(LightningDataModule):
 
     def _default_transforms(self):
         if self.normalize:
-            transforms = transform_lib.Compose(
-                [
-                    transform_lib.ToTensor(),
-                    transform_lib.Normalize(
-                        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-                    ),
-                ]
+            return (
+                lambda image, target: (
+                    T.Compose(
+                        [
+                            T.ToTensor(),
+                            T.Normalize(
+                                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                            ),
+                        ]
+                    )(image),
+                    target,
+                ),
             )
-        else:
-            transforms = transform_lib.ToTensor()
-        return transforms
+        return lambda image, target: (T.ToTensor()(image), target)
 
 
 dm = VOCDetectionDataModule("tmp")
