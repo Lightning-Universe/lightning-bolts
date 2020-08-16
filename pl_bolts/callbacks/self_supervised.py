91dd5a916f330dcd99ebace9ecbaae3d43b2ebde
William Falcon <waf2107@columbia.edu>
2020-08-16 00:15:35 -0400

    updated docs

--- pl_bolts/callbacks/self_supervised.py
+++ pl_bolts/callbacks/self_supervised.py
@@ -58,15 +58,20 @@ class SSLOnlineEvaluator(pl.Callback):  # pragma: no-cover
         representations = representations.reshape(representations.size(0), -1)
         return representations
 
+    def __to_device(self, x, device):
+        if isinstance(x, list):
+            for i in range(len(x)):
+                x[i] = x[i].to(device)
+        else:
+            x = x.to(device)
+
+        return x
+
     def on_train_batch_end(self, trainer, pl_module, batch, batch_idx, dataloader_idx):
 
         x, y = batch
-        y = y.to(pl_module.device)
-        if isinstance(x, list):
-            for i in range(len(x)):
-                x[i] = x[i].to(pl_module.device)
-        else:
-            x = x.to(pl_module.device)
+        y = self.__to_device(y, pl_module.device)
+        x = self.__to_device(x, pl_module.device)
 
         with torch.no_grad():
             representations = self.get_representations(pl_module, x)
