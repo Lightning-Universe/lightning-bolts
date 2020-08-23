8da7ca0c051a90a11ae634bfc94e329cb4f29d8e
William Falcon <waf2107@columbia.edu>
2020-08-22 20:06:50 -0400

    added simclr finetuner

---
+++ pl_bolts/models/self_supervised/simclr/simclr_finetuner.py
@@ -0,0 +1,54 @@
+from pl_bolts.models.self_supervised import SimCLR, SSLFineTuner
+from pl_bolts.datamodules import CIFAR10DataModule, STL10DataModule, ImagenetDataModule
+from argparse import ArgumentParser
+import pytorch_lightning as pl
+import os
+from pl_bolts.models.self_supervised.simclr.simclr_transforms import SimCLREvalDataTransform, SimCLRTrainDataTransform
+
+
+def cli_main():
+    pl.seed_everything(1234)
+
+    parser = ArgumentParser()
+    parser = pl.Trainer.add_argparse_args(parser)
+    parser.add_argument('--dataset', type=str, help='stl10, cifar10', default='cifar10')
+    parser.add_argument('--ckpt_path', type=str, help='path to ckpt')
+    parser.add_argument('--data_dir', type=str, help='path to ckpt', default=os.getcwd())
+    args = parser.parse_args()
+
+    # load the backbone
+    backbone = SimCLR.load_from_checkpoint(args.ckpt_path, strict=False)
+
+    # init default datamodule
+    if args.dataset == 'cifar10':
+        dm = CIFAR10DataModule.from_argparse_args(args)
+        dm.train_transforms = SimCLRTrainDataTransform(32)
+        dm.val_transforms = SimCLREvalDataTransform(32)
+        args.num_samples = dm.num_samples
+
+    elif args.dataset == 'stl10':
+        dm = STL10DataModule.from_argparse_args(args)
+        dm.train_dataloader = dm.train_dataloader_labeled
+        dm.val_dataloader = dm.val_dataloader_labeled
+        args.num_samples = dm.num_labeled_samples
+
+        (c, h, w) = dm.size()
+        dm.train_transforms = SimCLRTrainDataTransform(h)
+        dm.val_transforms = SimCLREvalDataTransform(h)
+
+    elif args.dataset == 'imagenet2012':
+        dm = ImagenetDataModule.from_argparse_args(args, image_size=196)
+        (c, h, w) = dm.size()
+        dm.train_transforms = SimCLRTrainDataTransform(h)
+        dm.val_transforms = SimCLREvalDataTransform(h)
+
+    # finetune
+    tuner = SSLFineTuner(backbone, in_features=2048 * 2 * 2, num_classes=dm.num_classes)
+    trainer = pl.Trainer.from_argparse_args(args, early_stop_callback=True)
+    trainer.fit(tuner, dm)
+
+    trainer.test(datamodule=dm)
+
+
+if __name__ == '__main__':
+    cli_main()
