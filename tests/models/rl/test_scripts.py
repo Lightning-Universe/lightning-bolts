cbfe1d764dc45a531e91cbc687b8060b38db0237
Jirka Borovec <jirka.borovec@seznam.cz>
2020-09-01 11:59:52 +0200

    run

--- tests/models/rl/test_scripts.py
+++ tests/models/rl/test_scripts.py
@@ -3,8 +3,12 @@ from unittest import mock
 import pytest
 
 
-@pytest.mark.parametrize('cli_args', ['--env PongNoFrameskip-v4 --max_steps 10 --fast_dev_run'
-                                      ' --warm_start_steps 10 --n_steps 2 --batch_size 10'])
+@pytest.mark.parametrize('cli_args', ['--env PongNoFrameskip-v4'
+                                      ' --max_steps 10'
+                                      ' --fast_dev_run'
+                                      ' --warm_start_steps 10'
+                                      ' --n_steps 2'
+                                      ' --batch_size 10'])
 def test_cli_run_rl_dqn(cli_args):
     """Test running CLI for an example with default params."""
     from pl_bolts.models.rl.dqn_model import cli_main
@@ -14,8 +18,12 @@ def test_cli_run_rl_dqn(cli_args):
         cli_main()
 
 
-@pytest.mark.parametrize('cli_args', ['--env PongNoFrameskip-v4 --max_steps 10 --fast_dev_run'
-                                      ' --warm_start_steps 10 --n_steps 2 --batch_size 10'])
+@pytest.mark.parametrize('cli_args', ['--env PongNoFrameskip-v4'
+                                      ' --max_steps 10'
+                                      ' --fast_dev_run'
+                                      ' --warm_start_steps 10'
+                                      ' --n_steps 2'
+                                      ' --batch_size 10'])
 def test_cli_run_rl_double_dqn(cli_args):
     """Test running CLI for an example with default params."""
     from pl_bolts.models.rl.double_dqn_model import cli_main
@@ -25,8 +33,12 @@ def test_cli_run_rl_double_dqn(cli_args):
         cli_main()
 
 
-@pytest.mark.parametrize('cli_args', ['--env PongNoFrameskip-v4 --max_steps 10 --fast_dev_run'
-                                      ' --warm_start_steps 10 --n_steps 2 --batch_size 10'])
+@pytest.mark.parametrize('cli_args', ['--env PongNoFrameskip-v4'
+                                      ' --max_steps 10'
+                                      ' --fast_dev_run'
+                                      ' --warm_start_steps 10'
+                                      ' --n_steps 2'
+                                      ' --batch_size 10'])
 def test_cli_run_rl_dueling_dqn(cli_args):
     """Test running CLI for an example with default params."""
     from pl_bolts.models.rl.dueling_dqn_model import cli_main
@@ -36,8 +48,12 @@ def test_cli_run_rl_dueling_dqn(cli_args):
         cli_main()
 
 
-@pytest.mark.parametrize('cli_args', ['--env PongNoFrameskip-v4 --max_steps 10 --fast_dev_run'
-                                      ' --warm_start_steps 10 --n_steps 2 --batch_size 10'])
+@pytest.mark.parametrize('cli_args', ['--env PongNoFrameskip-v4'
+                                      ' --max_steps 10'
+                                      ' --fast_dev_run'
+                                      ' --warm_start_steps 10'
+                                      ' --n_steps 2'
+                                      ' --batch_size 10'])
 def test_cli_run_rl_noisy_dqn(cli_args):
     """Test running CLI for an example with default params."""
     from pl_bolts.models.rl.noisy_dqn_model import cli_main
@@ -47,8 +63,12 @@ def test_cli_run_rl_noisy_dqn(cli_args):
         cli_main()
 
 
-@pytest.mark.parametrize('cli_args', ['--env PongNoFrameskip-v4 --max_steps 10 --fast_dev_run'
-                                      ' --warm_start_steps 10 --n_steps 2 --batch_size 10'])
+@pytest.mark.parametrize('cli_args', ['--env PongNoFrameskip-v4'
+                                      ' --max_steps 10'
+                                      ' --fast_dev_run'
+                                      ' --warm_start_steps 10'
+                                      ' --n_steps 2'
+                                      ' --batch_size 10'])
 def test_cli_run_rl_per_dqn(cli_args):
     """Test running CLI for an example with default params."""
     from pl_bolts.models.rl.per_dqn_model import cli_main
@@ -58,8 +78,11 @@ def test_cli_run_rl_per_dqn(cli_args):
         cli_main()
 
 
-@pytest.mark.parametrize('cli_args', ['--env CartPole-v0 --max_steps 10 --fast_dev_run'
-                                      ' --n_steps 2 --batch_size 10'])
+@pytest.mark.parametrize('cli_args', ['--env CartPole-v0'
+                                      ' --max_steps 10'
+                                      ' --fast_dev_run'
+                                      ' --n_steps 2'
+                                      ' --batch_size 10'])
 def test_cli_run_rl_reinforce(cli_args):
     """Test running CLI for an example with default params."""
     from pl_bolts.models.rl.reinforce_model import cli_main
@@ -69,8 +92,11 @@ def test_cli_run_rl_reinforce(cli_args):
         cli_main()
 
 
-@pytest.mark.parametrize('cli_args', ['--env CartPole-v0 --max_steps 10 --fast_dev_run'
-                                      ' --n_steps 2 --batch_size 10'])
+@pytest.mark.parametrize('cli_args', ['--env CartPole-v0'
+                                      ' --max_steps 10'
+                                      ' --fast_dev_run'
+                                      ' --n_steps 2'
+                                      ' --batch_size 10'])
 def test_cli_run_rl_vanilla_policy_gradient(cli_args):
     """Test running CLI for an example with default params."""
     from pl_bolts.models.rl.vanilla_policy_gradient_model import cli_main
--- tests/models/self_supervised/test_scripts.py
+++ tests/models/self_supervised/test_scripts.py
@@ -4,7 +4,7 @@ import pytest
 
 
 @pytest.mark.skip(reason='seems to freeze CLI run...')  # TODO
-@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3'])
+@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3 --fast_dev_run'])
 def test_cli_run_self_supervised_amdim(cli_args):
     """Test running CLI for an example with default params."""
     from pl_bolts.models.self_supervised.amdim.amdim_module import cli_main
@@ -14,8 +14,7 @@ def test_cli_run_self_supervised_amdim(cli_args):
         cli_main()
 
 
-@pytest.mark.skip(reason='seems to freeze CLI run...')  # TODO
-@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3'])
+@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3 --fast_dev_run'])
 def test_cli_run_self_supervised_cpc(cli_args):
     """Test running CLI for an example with default params."""
     from pl_bolts.models.self_supervised.cpc.cpc_module import cli_main
@@ -25,8 +24,7 @@ def test_cli_run_self_supervised_cpc(cli_args):
         cli_main()
 
 
-@pytest.mark.skip(reason='seems to freeze CLI run...')  # TODO
-@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3'])
+@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3 --fast_dev_run'])
 def test_cli_run_self_supervised_moco(cli_args):
     """Test running CLI for an example with default params."""
     from pl_bolts.models.self_supervised.moco.moco2_module import cli_main
@@ -36,8 +34,7 @@ def test_cli_run_self_supervised_moco(cli_args):
         cli_main()
 
 
-@pytest.mark.skip(reason='seems to freeze CLI run...')  # TODO
-@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3'])
+@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3 --fast_dev_run'])
 def test_cli_run_self_supervised_simclr(cli_args):
     """Test running CLI for an example with default params."""
     from pl_bolts.models.self_supervised.simclr.simclr_module import cli_main
