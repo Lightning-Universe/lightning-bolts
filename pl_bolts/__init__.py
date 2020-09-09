710fbe6c369f783ce59016da37992a1b55ddd057
Jirka Borovec <jirka.borovec@seznam.cz>
2020-09-09 17:19:12 +0200

    update license

--- pl_bolts/__init__.py
+++ pl_bolts/__init__.py
@@ -5,7 +5,7 @@ import os
 __version__ = '0.2.0'
 __author__ = 'PyTorchLightning et al.'
 __author_email__ = 'name@pytorchlightning.ai'
-__license__ = 'TBD'
+__license__ = 'Apache-2.0'
 __copyright__ = 'Copyright (c) 2020-2020, %s.' % __author__
 __homepage__ = 'https://github.com/PyTorchLightning/pytorch-lightning-bolts'
 __docs__ = "PyTorch Lightning Bolts is a community contribution for ML researchers."
