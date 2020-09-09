2469f6a9106119b2ebac1303048fcceea8dc4e65
Ananya Harsh Jha <ananya@pytorchlightning.ai>
2020-09-09 01:10:07 +0000

    gpus flag for models test_scripts

--- tests/models/test_scripts.py
+++ tests/models/test_scripts.py
@@ -1,6 +1,7 @@
 from unittest import mock
 
 import pytest
+import torch
 
 
 @pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3'])
@@ -13,7 +14,8 @@ def test_cli_run_mnist(cli_args):
         cli_main()
 
 
-@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3'])
+@pytest.mark.skipif(torch.cuda.device_count() == 0, reason="test requires GPU machine")
+@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3 --gpus 1'])
 def test_cli_run_basic_ae(cli_args):
     """Test running CLI for an example with default params."""
     from pl_bolts.models.autoencoders.basic_ae.basic_ae_module import cli_main
@@ -23,7 +25,8 @@ def test_cli_run_basic_ae(cli_args):
         cli_main()
 
 
-@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3'])
+@pytest.mark.skipif(torch.cuda.device_count() == 0, reason="test requires GPU machine")
+@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3 --gpus 1'])
 def test_cli_run_basic_vae(cli_args):
     """Test running CLI for an example with default params."""
     from pl_bolts.models.autoencoders.basic_vae.basic_vae_module import cli_main
@@ -63,8 +66,9 @@ def test_cli_run_log_regression(cli_args):
         cli_main()
 
 
-@pytest.mark.skip(reason='seems to freeze CLI run...')  # TODO
-@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3'])
+#@pytest.mark.skip(reason='seems to freeze CLI run...')  # TODO
+@pytest.mark.skipif(torch.cuda.device_count() == 0, reason="test requires GPU machine")
+@pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3 --gpus 1'])
 def test_cli_run_vision_image_gpt(cli_args):
     """Test running CLI for an example with default params."""
     from pl_bolts.models.vision.image_gpt.igpt_module import cli_main
