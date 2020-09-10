33ce0330e4f8c8659111067aa25015f4530a9c47
nateraw <nxr9266@g.rit.edu>
2020-09-10 15:36:43 -0600

    :white_check_mark: pytest is cute

--- tests/models/test_autoencoders.py
+++ tests/models/test_autoencoders.py
@@ -1,16 +1,28 @@
+import pytest
 import pytorch_lightning as pl
 import torch
 from pytorch_lightning import seed_everything
 
-from pl_bolts.datamodules import MNISTDataModule
+from pl_bolts.datamodules import MNISTDataModule, CIFAR10DataModule
 from pl_bolts.models.autoencoders import VAE, AE
 from pl_bolts.models.autoencoders.basic_ae import AEEncoder
 from pl_bolts.models.autoencoders.basic_vae import Encoder, Decoder
 
 
-def test_vae(tmpdir):
+@pytest.mark.parametrize(
+    "dm_cls",
+    [
+        pytest.param(
+            MNISTDataModule, id='mnist'
+        ),
+        pytest.param(
+            CIFAR10DataModule, id='cifar10'
+        ),
+    ]
+)
+def test_vae(tmpdir, dm_cls):
     seed_everything()
-    dm = MNISTDataModule(batch_size=2, num_workers=0)
+    dm = dm_cls(batch_size=2, num_workers=0)
     model = VAE(*dm.size())
     trainer = pl.Trainer(fast_dev_run=True, default_root_dir=tmpdir, deterministic=True)
     trainer.fit(model, dm)
@@ -19,42 +31,70 @@ def test_vae(tmpdir):
 
     assert loss > 0, 'VAE failed'
 
-
-def test_ae(tmpdir):
+@pytest.mark.parametrize(
+    "dm_cls",
+    [
+        pytest.param(
+            MNISTDataModule, id='mnist'
+        ),
+        pytest.param(
+            CIFAR10DataModule, id='cifar10'
+        ),
+    ]
+)
+def test_ae(tmpdir, dm_cls):
     seed_everything()
-
-    dm = MNISTDataModule(batch_size=2, num_workers=0)
+    dm = dm_cls(batch_size=2, num_workers=0)
     model = VAE(*dm.size())
     trainer = pl.Trainer(fast_dev_run=True, default_root_dir=tmpdir)
     trainer.fit(model, dm)
     trainer.test(model, datamodule=dm)
 
 
-def test_basic_ae_encoder(tmpdir):
+@pytest.mark.parametrize(
+    "hidden_dim,latent_dim,batch_size,channels,height,width",
+    [
+        pytest.param(
+            128, 2, 16, 1, 28, 28, id='like-mnist-hidden-128-latent-2'
+        ),
+        pytest.param(
+            128, 4, 16, 1, 28, 28, id='like-mnist-hidden-128-latent-4'
+        ),
+        pytest.param(
+            64, 4, 16, 1, 28, 28, id='like-mnist-hidden-64-latent-4'
+        ),
+        pytest.param(
+            128, 2, 16, 3, 32, 32, id='like-cifar10-hidden-128-latent-2'
+        ),
+    ]
+)
+def test_basic_ae_encoder(tmpdir, hidden_dim, latent_dim, batch_size, channels, height, width):
     seed_everything()
-
-    hidden_dim = 128
-    latent_dim = 2
-    width = height = 28
-    batch_size = 16
-    channels = 1
-
     encoder = AEEncoder(hidden_dim, latent_dim, channels, width, height)
     x = torch.randn(batch_size, channels, width, height)
     z = encoder(x)
-
     assert z.shape == (batch_size, latent_dim)
 
 
-def test_basic_vae_components(tmpdir):
+@pytest.mark.parametrize(
+    "hidden_dim,latent_dim,batch_size,channels,height,width",
+    [
+        pytest.param(
+            128, 2, 16, 1, 28, 28, id='like-mnist-hidden-128-latent-2'
+        ),
+        pytest.param(
+            128, 4, 16, 1, 28, 28, id='like-mnist-hidden-128-latent-4'
+        ),
+        pytest.param(
+            64, 4, 16, 1, 28, 28, id='like-mnist-hidden-64-latent-4'
+        ),
+        pytest.param(
+            128, 2, 16, 3, 32, 32, id='like-cifar10-hidden-128-latent-2'
+        ),
+    ]
+)
+def test_basic_vae_components(tmpdir, hidden_dim, latent_dim, batch_size, channels, height, width):
     seed_everything()
-
-    hidden_dim = 128
-    latent_dim = 2
-    width = height = 28
-    batch_size = 16
-    channels = 1
-
     enc = Encoder(hidden_dim, latent_dim, channels, width, height)
     x = torch.randn(batch_size, channels, width, height)
     mu, sigma = enc(x)
