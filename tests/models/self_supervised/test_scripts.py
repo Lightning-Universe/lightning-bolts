f90a427282d30a9d436c69b496c4217e5c5b09b0
Ananya Harsh Jha <ananya@pytorchlightning.ai>
2020-09-10 15:03:11 +0000

    removed pep8 issue

--- tests/models/self_supervised/test_scripts.py
+++ tests/models/self_supervised/test_scripts.py
@@ -43,6 +43,7 @@ def test_cli_run_self_supervised_simclr(cli_args):
     with mock.patch("argparse._sys.argv", ["any.py"] + cli_args):
         cli_main()
 
+
 @pytest.mark.parametrize('cli_args', ['--max_epochs 1 --max_steps 3 --fast_dev_run --batch_size 2 --online_ft'])
 def test_cli_run_self_supervised_byol(cli_args):
     """Test running CLI for an example with default params."""
