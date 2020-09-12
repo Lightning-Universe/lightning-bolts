bc2826adb46af550af1544e1ede3cd58ff171b8d
Ananya Harsh Jha <ananya@pytorchlightning.ai>
2020-09-12 02:16:57 +0000

    removed extra func

--- pl_bolts/models/autoencoders/components.py
+++ pl_bolts/models/autoencoders/components.py
@@ -46,13 +46,6 @@ def resize_conv1x1(in_planes, out_planes, scale=1):
         )
 
 
-def conv3x3(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(
-        in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False
-    )
-
-
 class EncoderBlock(nn.Module):
     """
     ResNet block, copied from
