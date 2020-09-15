9653259950c0eb3906d3bb2fc07ad1484a823a27
Jirka Borovec <jirka.borovec@seznam.cz>
2020-09-15 09:33:26 +0200

    tests

--- tests/models/test_executable_scripts.py
+++ tests/models/test_executable_scripts.py
@@ -3,72 +3,40 @@ from unittest import mock
 import pytest
 
 
-@pytest.mark.parametrize(
-    "dataset_name", [
-        pytest.param('mnist', id="mnist"),
-        pytest.param('cifar10', id="cifar10")
-    ]
-)
-def test_cli_basic_gan(dataset_name):
+@pytest.mark.parametrize('cli_args', [
+    '--dataset mnist --max_epochs 1 --batch_size 2 --limit_train_batches 2 --limit_val_batches 2',
+    '--dataset cifar10 --max_epochs 1 --batch_size 2 --limit_train_batches 2 --limit_val_batches 2',
+])
+def test_cli_basic_gan(cli_args):
     from pl_bolts.models.gans.basic.basic_gan_module import cli_main
 
-    cli_args = f"""
-        --dataset {dataset_name}
-        --max_epochs 1
-        --limit_train_batches 3
-        --limit_val_batches 3
-        --batch_size 3
-    """.strip().split()
-
-    with mock.patch("argparse._sys.argv", ["any.py"] + cli_args):
+    with mock.patch("argparse._sys.argv", ["any.py"] + cli_args.strip().split()):
         cli_main()
 
 
-@pytest.mark.parametrize(
-    "dataset_name", [
-        pytest.param('cifar10', id="cifar10")
-    ]
-)
-def test_cli_basic_vae(dataset_name):
+@pytest.mark.parametrize('cli_args', [
+    '--dataset cifar10 --max_epochs 1 --batch_size 2 --encoder resnet18 --fast_dev_run',
+])
+def test_cli_basic_vae(cli_args):
     from pl_bolts.models.autoencoders.basic_vae.basic_vae_module import cli_main
 
-    cli_args = f"""
-        --dataset {dataset_name}
-        --max_epochs 1
-        --batch_size 3
-        --fast_dev_run
-        --gpus 0
-    """.strip().split()
-
-    with mock.patch("argparse._sys.argv", ["any.py"] + cli_args):
+    with mock.patch("argparse._sys.argv", ["any.py"] + cli_args.strip().split()):
         cli_main()
 
 
-@pytest.mark.parametrize(
-    "dataset_name", [
-        pytest.param('cifar10', id="cifar10")
-    ]
-)
-def test_cli_basic_ae(dataset_name):
+@pytest.mark.parametrize('cli_args', [
+    '--dataset cifar10 --max_epochs 1 --batch_size 2 --encoder resnet18 --fast_dev_run',
+])
+def test_cli_basic_ae(cli_args):
     from pl_bolts.models.autoencoders.basic_ae.basic_ae_module import cli_main
 
-    cli_args = f"""
-        --dataset {dataset_name}
-        --max_epochs 1
-        --batch_size 3
-        --fast_dev_run
-        --gpus 0
-    """.strip().split()
-
-    with mock.patch("argparse._sys.argv", ["any.py"] + cli_args):
+    with mock.patch("argparse._sys.argv", ["any.py"] + cli_args.strip().split()):
         cli_main()
 
 
-@pytest.mark.parametrize('cli_args', ['--max_epochs 1'
-                                      ' --limit_train_batches 3'
-                                      ' --limit_val_batches 3'
-                                      ' --batch_size 3'
-                                      ' --encoder resnet18'])
+@pytest.mark.parametrize('cli_args', [
+    '--max_epochs 1 --limit_train_batches 3 --limit_val_batches 3 --batch_size 2 --encoder resnet18',
+])
 def test_cli_cpc(cli_args):
     from pl_bolts.models.self_supervised.cpc.cpc_module import cli_main
 
