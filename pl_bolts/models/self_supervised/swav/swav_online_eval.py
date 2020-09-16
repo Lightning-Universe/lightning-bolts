f8ca002ab6feebc335d41e4d7257b99544382b3e
ananyahjha93 <ananya@pytorchlightning.ai>
2020-09-16 18:05:52 -0400

    swav

---
+++ pl_bolts/models/self_supervised/swav/swav_online_eval.py
@@ -0,0 +1,111 @@
+import torch
+import math
+import pytorch_lightning as pl
+from torch.nn import functional as F
+from pytorch_lightning.metrics.functional import accuracy
+from typing import Optional
+
+
+class SwavOnlineEvaluator(pl.Callback):
+    def __init__(
+        self,
+        drop_p: float = 0.2,
+        hidden_dim: Optional[int] = None,
+        z_dim: int = None,
+        num_classes: int = None,
+        dataset: str = 'stl10'
+    ):
+        super().__init__()
+
+        self.hidden_dim = hidden_dim
+        self.drop_p = drop_p
+        self.optimizer = None
+
+        self.z_dim = z_dim
+        self.num_classes = num_classes
+        self.dataset = dataset
+
+        self.loss = []
+        self.acc = []
+
+    def on_pretrain_routine_start(self, trainer, pl_module):
+        from pl_bolts.models.self_supervised.evaluator import SSLEvaluator
+
+        pl_module.non_linear_evaluator = SSLEvaluator(
+            n_input=self.z_dim,
+            n_classes=self.num_classes,
+            p=self.drop_p,
+            n_hidden=self.hidden_dim,
+        ).to(pl_module.device)
+
+        self.optimizer = torch.optim.Adam(
+            pl_module.non_linear_evaluator.parameters(), lr=1e-4
+        )
+
+    def get_representations(self, pl_module, x):
+        representations = pl_module(x)
+        representations = representations.reshape(representations.size(0), -1)
+        return representations
+
+    def to_device(self, batch, device):
+        # get the labeled batch
+        if self.dataset == 'stl10':
+            labeled_batch = batch[1]
+            batch = labeled_batch
+
+        inputs, y = batch
+
+        # last input is for online eval
+        x = inputs[-1]
+        x = x.to(device)
+        y = y.to(device)
+
+        return x, y
+
+    def on_train_batch_end(self, trainer, pl_module, batch, batch_idx, dataloader_idx):
+        x, y = self.to_device(batch, pl_module.device)
+
+        with torch.no_grad():
+            representations = self.get_representations(pl_module, x)
+
+        representations = representations.detach()
+
+        # forward pass
+        mlp_preds = pl_module.non_linear_evaluator(representations)
+        mlp_loss = F.cross_entropy(mlp_preds, y)
+
+        # update finetune weights
+        mlp_loss.backward()
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+
+        # log metrics
+        acc = accuracy(mlp_preds, y)
+        metrics = {"train_mlp_loss": mlp_loss, "train_mlp_acc": acc}
+        pl_module.logger.log_metrics(metrics, step=trainer.global_step)
+
+    def on_validation_batch_end(self, trainer, pl_module, batch, batch_idx, dataloader_idx):
+        x, y = self.to_device(batch, pl_module.device)
+
+        with torch.no_grad():
+            representations = self.get_representations(pl_module, x)
+
+        representations = representations.detach()
+
+        # forward pass
+        mlp_preds = pl_module.non_linear_evaluator(representations)
+        mlp_loss = F.cross_entropy(mlp_preds, y)
+
+        # log metrics
+        acc = accuracy(mlp_preds, y)
+
+        self.loss.append(mlp_loss.item())
+        self.acc.append(acc.item())
+
+    def on_validation_epoch_end(self, trainer, pl_module):
+        metrics = {"val_mlp_loss": sum(self.loss) / len(self.loss), "val_mlp_acc": sum(self.acc) / len(self.acc)}
+        pl_module.logger.log_metrics(metrics, step=trainer.current_epoch)
+
+        # reset
+        self.loss = []
+        self.acc = []
--- tests/models/self_supervised/test_models.py
+++ tests/models/self_supervised/test_models.py
@@ -1,12 +1,14 @@
 import pytorch_lightning as pl
 from pytorch_lightning import seed_everything
 
-from pl_bolts.datamodules import CIFAR10DataModule
-from pl_bolts.models.self_supervised import CPCV2, AMDIM, MocoV2, SimCLR, BYOL
+from pl_bolts.datamodules import CIFAR10DataModule, STL10DataModule
+from pl_bolts.transforms.dataset_normalizations import stl10_normalization
+from pl_bolts.models.self_supervised import CPCV2, AMDIM, MocoV2, SimCLR, BYOL, SwAV
 from pl_bolts.models.self_supervised.cpc import CPCTrainTransformsCIFAR10, CPCEvalTransformsCIFAR10
 from pl_bolts.models.self_supervised.moco.callbacks import MocoLRScheduler
 from pl_bolts.models.self_supervised.moco.transforms import (Moco2TrainCIFAR10Transforms, Moco2EvalCIFAR10Transforms)
 from pl_bolts.models.self_supervised.simclr.transforms import SimCLREvalDataTransform, SimCLRTrainDataTransform
+from pl_bolts.models.self_supervised.swav.transforms import SwAVTrainDataTransform, SwAVEvalDataTransform
 
 
 def test_cpcv2(tmpdir):
@@ -75,6 +77,35 @@ def test_simclr(tmpdir):
     model = SimCLR(batch_size=2, num_samples=datamodule.num_samples)
     trainer = pl.Trainer(fast_dev_run=True, max_epochs=1, default_root_dir=tmpdir)
     trainer.fit(model, datamodule)
+    loss = trainer.progress_bar_dict['loss']
+
+    assert float(loss) > 0
+
+
+def test_swav(tmpdir):
+    seed_everything()
+
+    batch_size = 2
+    datamodule = STL10DataModule(tmpdir, num_workers=0, batch_size=batch_size)
+    datamodule.train_dataloader = datamodule.train_dataloader_mixed
+    datamodule.val_dataloader = datamodule.val_dataloader_mixed
+
+    datamodule.train_transforms = SwAVTrainDataTransform(normalize=stl10_normalization())
+    datamodule.val_transforms = SwAVEvalDataTransform(normalize=stl10_normalization())
+
+    model = SwAV(
+        arch='resnet18',
+        hidden_mlp=512,
+        gpus=0,
+        datamodule=datamodule,
+        num_samples=datamodule.num_unlabeled_samples,
+        batch_size=batch_size
+    )
+
+    trainer = pl.Trainer(
+        gpus=0, fast_dev_run=True, max_epochs=1, default_root_dir=tmpdir, max_steps=3
+    )
+    trainer.fit(model)
     loss = trainer.progress_bar_dict['loss']
 
     assert float(loss) > 0
