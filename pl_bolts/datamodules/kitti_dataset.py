1b5ef8e0661c9ad4ae2521ea8d3e500a15b262e4
Annika Brundyn <annikabrundyn1@gmail.com>
2020-09-22 21:24:42 -0400

    kitti dataset

---
+++ pl_bolts/datamodules/kitti_dataset.py
@@ -0,0 +1,110 @@
+import os
+import random
+from argparse import ArgumentParser, Namespace
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset
+
+import pytorch_lightning as pl
+from pl_examples.models.unet import UNet
+from pytorch_lightning.loggers import WandbLogger
+
+DEFAULT_VOID_LABELS = (0, 1, 2, 3, 4, 5, 6, 9, 10, 14, 15, 16, 18, 29, 30, -1)
+DEFAULT_VALID_LABELS = (7, 8, 11, 12, 13, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33)
+
+
+class KITTI(Dataset):
+    """
+    Class for KITTI Semantic Segmentation Benchmark dataset
+    Dataset link - http://www.cvlibs.net/datasets/kitti/eval_semseg.php?benchmark=semantics2015
+    There are 34 classes in the given labels. However, not all of them are useful for training
+    (like railings on highways, road dividers, etc.).
+    So, these useless classes (the pixel values of these classes) are stored in the `void_labels`.
+    The useful classes are stored in the `valid_labels`.
+    The `encode_segmap` function sets all pixels with any of the `void_labels` to `ignore_index`
+    (250 by default). It also sets all of the valid pixels to the appropriate value between 0 and
+    `len(valid_labels)` (since that is the number of valid classes), so it can be used properly by
+    the loss function when comparing with the output.
+    The `get_filenames` function retrieves the filenames of all images in the given `path` and
+    saves the absolute path in a list.
+    In the `get_item` function, images and masks are resized to the given `img_size`, masks are
+    encoded using `encode_segmap`, and given `transform` (if any) are applied to the image only
+    (mask does not usually require transforms, but they can be implemented in a similar way).
+    """
+    IMAGE_PATH = os.path.join('training', 'image_2')
+    MASK_PATH = os.path.join('training', 'semantic')
+
+    def __init__(
+        self,
+        data_path: str,
+        split: str,
+        img_size: tuple = (1242, 376),
+        void_labels: list = DEFAULT_VOID_LABELS,
+        valid_labels: list = DEFAULT_VALID_LABELS,
+        transform=None
+    ):
+        self.img_size = img_size
+        self.void_labels = void_labels
+        self.valid_labels = valid_labels
+        self.ignore_index = 250
+        self.class_map = dict(zip(self.valid_labels, range(len(self.valid_labels))))
+        self.transform = transform
+
+        self.split = split
+        self.data_path = data_path
+        self.img_path = os.path.join(self.data_path, self.IMAGE_PATH)
+        self.mask_path = os.path.join(self.data_path, self.MASK_PATH)
+        self.img_list = self.get_filenames(self.img_path)
+        self.mask_list = self.get_filenames(self.mask_path)
+
+        # Split between train and valid set (80/20)
+        random_inst = random.Random(12345)  # for repeatability
+        n_items = len(self.img_list)
+        idxs = random_inst.sample(range(n_items), n_items // 5)
+        if self.split == 'train':
+            idxs = [idx for idx in range(n_items) if idx not in idxs]
+        self.img_list = [self.img_list[i] for i in idxs]
+        self.mask_list = [self.mask_list[i] for i in idxs]
+
+    def __len__(self):
+        return len(self.img_list)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.img_list[idx])
+        img = img.resize(self.img_size)
+        img = np.array(img)
+
+        mask = Image.open(self.mask_list[idx]).convert('L')
+        mask = mask.resize(self.img_size)
+        mask = np.array(mask)
+        mask = self.encode_segmap(mask)
+
+        if self.transform:
+            img = self.transform(img)
+
+        return img, mask
+
+    def encode_segmap(self, mask):
+        """
+        Sets void classes to zero so they won't be considered for training
+        """
+        for voidc in self.void_labels:
+            mask[mask == voidc] = self.ignore_index
+        for validc in self.valid_labels:
+            mask[mask == validc] = self.class_map[validc]
+        # remove extra idxs from updated dataset
+        mask[mask > 18] = self.ignore_index
+        return mask
+
+    def get_filenames(self, path):
+        """
+        Returns a list of absolute paths to images inside given `path`
+        """
+        files_list = list()
+        for filename in os.listdir(path):
+            files_list.append(os.path.join(path, filename))
+        return files_list
