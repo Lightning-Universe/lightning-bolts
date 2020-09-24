16ee713c003b5365042eec9ddf115c64214b1d42
Annika Brundyn <annikabrundyn1@gmail.com>
2020-09-24 19:55:24 -0400

    update init

--- pl_bolts/models/__init__.py
+++ pl_bolts/models/__init__.py
@@ -8,3 +8,4 @@ from pl_bolts.models.mnist_module import LitMNIST
 from pl_bolts.models.regression import LinearRegression, LogisticRegression
 from pl_bolts.models.vision import PixelCNN
 from pl_bolts.models.vision.image_gpt.igpt_module import GPT2, ImageGPT
+from pl_bolts.models.vision import UNet
--- pl_bolts/models/vision/__init__.py
+++ pl_bolts/models/vision/__init__.py
@@ -1,1 +1,2 @@
 from pl_bolts.models.vision.pixel_cnn import PixelCNN
+from pl_bolts.models.vision.unet import UNet
