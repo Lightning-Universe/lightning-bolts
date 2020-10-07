39589f30316bdab2fd9b1c1d957ef1426292c5fa
Annika Brundyn <annikabrundyn1@gmail.com>
2020-10-07 17:38:29 -0400

    fix import

--- pl_bolts/models/vision/segmentation.py
+++ pl_bolts/models/vision/segmentation.py
@@ -4,7 +4,7 @@ import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
 
-from pl_examples.models.unet import UNet
+from pl_bolts.models import UNet
 
 
 class SemSegment(pl.LightningModule):
