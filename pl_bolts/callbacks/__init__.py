da626a983a81fd2d182c808fb43ff54d349ae9d4
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-10-18 03:25:28 +0200

    added top import

--- pl_bolts/callbacks/__init__.py
+++ pl_bolts/callbacks/__init__.py
@@ -1,6 +1,7 @@
 """
 Collection of PyTorchLightning callbacks
 """
+from pl_bolts.callbacks.data_monitor import ModuleDataMonitor, TrainingDataMonitor
 from pl_bolts.callbacks.printing import PrintTableMetricsCallback
 from pl_bolts.callbacks.variational import LatentDimInterpolator
 from pl_bolts.callbacks.vision import TensorboardGenerativeModelImageSampler
