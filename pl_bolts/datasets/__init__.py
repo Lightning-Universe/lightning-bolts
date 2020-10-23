f0939f62a5559a9a68cbb60ab8cbddac8c42cb05
Jirka Borovec <Borda@users.noreply.github.com>
2020-10-23 17:32:20 +0200

    Fix imports in datamodules (#303)
    * fix datamodule imports
    
    * flake8

--- pl_bolts/datasets/__init__.py
+++ pl_bolts/datasets/__init__.py
@@ -5,3 +5,11 @@ from pl_bolts.datasets.dummy_dataset import (
     DummyDataset,
     DummyDetectionDataset
 )
+
+__all__ = [
+    "RandomDictStringDataset",
+    "RandomDictDataset",
+    "RandomDataset",
+    "DummyDataset",
+    "DummyDetectionDataset",
+]
