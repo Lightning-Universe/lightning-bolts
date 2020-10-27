4cbe44445ceb12e39de8e3e85f48a74428402bae
ananyahjha93 <ananya@pytorchlightning.ai>
2020-10-27 11:49:39 -0400

    import fix

--- pl_bolts/callbacks/ssl_online.py
+++ pl_bolts/callbacks/ssl_online.py
@@ -5,7 +5,6 @@ from pytorch_lightning.metrics.functional import accuracy
 from torch.nn import functional as F
 
 from pytorch_lightning import Callback
-from pl_bolts.models.self_supervised.evaluator import SSLEvaluator
 
 
 class SSLOnlineEvaluator(Callback):  # pragma: no-cover
