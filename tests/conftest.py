326a54bcc0d389b4996b49c7ebec286cc2618ed2
Akihiro Nitta <nitta@akihironitta.com>
2020-10-30 06:25:46 +0900

    Add TODO

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
 import pytest
 
 
+# GitHub Actions use this path to cache datasets.
 @pytest.fixture(scope="session")
 def data_dir():
-    # GitHub Actions use this path to cache datasets.
+    # TODO(akihironitta): Use something like pytest-lazy-fixture
+    # to use this fixture in `pytest.mark.parametrize`.
+    # See https://github.com/pytest-dev/pytest/issues/349.
     return "./datasets"
