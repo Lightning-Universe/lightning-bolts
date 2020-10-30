c89d711c090fe890524dcfb1c5987036116737a2
Akihiro Nitta <nitta@akihironitta.com>
2020-10-31 01:35:01 +0900

    Add notes

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,9 @@ import pytest
 
 
 # GitHub Actions use this path to cache datasets.
+# Use `data_dir` fixture where possible and use `DATA_DIR` in
+# `pytest.mark.parametrize()` where you cannot use `data_dir`.
+# https://github.com/pytest-dev/pytest/issues/349
 DATA_DIR = "./datasets"
 
 
