197f25a4d97e58268adf6e62bf2c9125272fc7b1
Zymrael <mic.poli@hotmail.it>
2020-10-30 21:47:05 +0900

    add vanilla neural ODE variant

---
+++ pl_bolts/models/diffeq/__init__.py
@@ -0,0 +1,1 @@
+from pl_bolts.models.diffeq.neuralode import DepthInvariantNeuralODE
---
+++ pl_bolts/models/diffeq/neuralode.py
@@ -0,0 +1,116 @@
+from argparse import ArgumentParser
+
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+from torchdyn.models import NeuralODE, DataControl
+
+
+class DepthInvariantNeuralODE(pl.LightningModule):
+    """
+    Vanilla Neural ODE with constant-in-depth parameters.
+    Can be equipped with data control as described in https://arxiv.org/abs/2002.08071
+
+    Example::
+        TO DO
+
+    Example CLI::
+        TO DO
+    """
+
+    def __init__(
+            self,
+            input_dim: int,
+            hidden_dim: int,
+            absolute_tol: float = 1e-4,
+            relative_tol: float = 1e-4,
+            integration_span: Tensor = torch.linspace(0, 1),
+            data_control: str = 'True',
+            learning_rate: float = 0.01,
+            **kwargs
+    ):
+        """
+        Args:
+            input_dim: size of input data
+            hidden_dim: size of hidden state 
+            absolute_tol: absolute tolerance of the numerical solver
+            relative_tol: relative tolerance of the numerical solver
+            integration_span: mesh grid of evaluation points for the ODE
+            data_control: data control mechanism as described in https://arxiv.org/abs/2002.08071
+            learning_rate: learning rate for Adam
+
+        """
+        super().__init__()
+
+        if data_control:
+            vector_field = nn.Sequential(DataControl(),
+                                         nn.Linear(2 * input_dim, hidden_dim),
+                                         nn.Softplus(),
+                                         nn.Linear(hidden_dim, input_dim)
+                                         )
+
+        else:
+            vector_field = nn.Sequential(nn.Linear(input_dim, hidden_dim),
+                                         nn.Softplus(),
+                                         nn.Linear(hidden_dim, input_dim)
+                                         )
+
+        self.nde = NeuralODE(vector_field,
+                             order=1,
+                             sensitivity='adjoint',
+                             solver='dopri5',
+                             atol=absolute_tol,
+                             rtol=relative_tol,
+                             s_span=integration_span
+                             )
+
+    def forward(self, z):
+        """
+        Solve the NeuralODE in its integration interval with `z0` as initial condition, 
+        obtaining data features `zS` at final depth `S`
+
+        Example::
+            z0 = torch.rand(batch_size, input_dim)
+            nde = DepthInvariantNeuralODE(batch_size, input_dim)
+            zS = nde(z0)
+        """
+        return self.nde(z)
+
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        x, y = batch
+        y_hat = self.model(x)
+        loss = nn.CrossEntropyLoss()(y_hat, y)
+
+        nfe = self.nde.nfe
+        self.nde.nfe = 0
+        return {'loss': loss, 'nfe': nfe}
+
+    def configure_optimizers(self):
+        lr = self.hparams.learning_rate
+
+        opt_g = torch.optim.Adam(self.generator.parameters(), lr=lr)
+        return [opt_g], []
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument('--learning_rate', type=float, default=0.0002, help="adam: learning rate")
+        return parser
+
+
+def cli_main(args=None):
+    from torchdyn.datasets import ToyDataset, generate_concentric_spheres, generate_moons
+
+    pl.seed_everything(1234)
+
+    parser = ArgumentParser()
+    parser.add_argument("--dataset", default="concentric_circles", type=str, help="concentric_circles, moons")
+    script_args, _ = parser.parse_known_args(args)
+
+    return None, None, None
+
+
+if __name__ == '__main__':
+    _, _, _ = cli_main()
