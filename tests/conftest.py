6134448b073988216aafe32144af8ecf9bb29d1c
Jirka Borovec <jirka.borovec@seznam.cz>
2020-11-06 14:03:49 +0100

    data_dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,9 @@ import pytest
 # Use `data_dir` fixture where possible and use `DATA_DIR` in
 # `pytest.mark.parametrize()` where you cannot use `data_dir`.
 # https://github.com/pytest-dev/pytest/issues/349
-DATA_DIR = "./datasets"
+from tests import DATASETS_PATH
 
 
 @pytest.fixture(scope="session")
 def data_dir():
-    return DATA_DIR
+    return DATASETS_PATH
