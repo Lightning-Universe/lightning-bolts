1fe59bada2d734984401cc2ae7b9de75abcfcc9e
ananyahjha93 <ananya@pytorchlightning.ai>
2020-11-12 18:02:54 -0500

    updated transform

--- pl_bolts/models/self_supervised/simclr/transforms.py
+++ pl_bolts/models/self_supervised/simclr/transforms.py
@@ -37,27 +37,54 @@ class SimCLRTrainDataTransform(object):
         x = sample()
         (xi, xj) = transform(x)
     """
-    def __init__(self, input_height: int, s: int = 1):
+    def __init__(
+        self,
+        input_height: int = 224,
+        gaussian_blur: bool = True,
+        jitter_strength: float = 1.,
+        normalize: Optional[transforms.Normalize] = None
+    ) -> None:
+
         if not _TORCHVISION_AVAILABLE:
             raise ModuleNotFoundError(  # pragma: no-cover
                 'You want to use `transforms` from `torchvision` which is not installed yet.'
             )
 
-        self.s = s
+        self.jitter_strength = jitter_strength
         self.input_height = input_height
-        color_jitter = transforms.ColorJitter(0.8 * self.s, 0.8 * self.s, 0.8 * self.s, 0.2 * self.s)
-        data_transforms = transforms.Compose([transforms.RandomResizedCrop(size=self.input_height),
-                                              transforms.RandomHorizontalFlip(),
-                                              transforms.RandomApply([color_jitter], p=0.5),
-                                              transforms.RandomGrayscale(p=0.2),
-                                              GaussianBlur(kernel_size=int(0.1 * self.input_height)),
-                                              transforms.ToTensor()])
-        self.train_transform = data_transforms
+        self.gaussian_blur = gaussian_blur
+        self.normalize = normalize
+
+        self.color_jitter = transforms.ColorJitter(
+            0.8 * self.jitter_strength,
+            0.8 * self.jitter_strength,
+            0.8 * self.jitter_strength,
+            0.2 * self.jitter_strength
+        )
+
+        data_transforms = [
+            transforms.RandomResizedCrop(size=self.input_height),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomApply([self.color_jitter], p=0.8),
+            transforms.RandomGrayscale(p=0.2)
+        ]
+
+        if self.gaussian_blur:
+            data_transforms.append(GaussianBlur(kernel_size=int(0.1 * self.input_height, p=0.5)))
+
+        data_transforms.append(transforms.ToTensor())
+
+        if self.normalize:
+            data_transforms.append(normalize)
+
+        self.train_transform = transforms.Compose(data_transforms)
 
     def __call__(self, sample):
         transform = self.train_transform
+
         xi = transform(sample)
         xj = transform(sample)
+
         return xi, xj
 
 
@@ -79,42 +106,43 @@ class SimCLREvalDataTransform(object):
         x = sample()
         (xi, xj) = transform(x)
     """
-    def __init__(self, input_height: int, s: int = 1):
-        if not _TORCHVISION_AVAILABLE:
-            raise ModuleNotFoundError(  # pragma: no-cover
-                'You want to use `transforms` from `torchvision` which is not installed yet.'
-            )
-
-        self.s = s
+    def __init__(
+        self,
+        input_height: int = 224,
+        normalize: Optional[transforms.Normalize] = None
+    ):
         self.input_height = input_height
-        self.test_transform = transforms.Compose([
-            transforms.Resize(input_height + 10, interpolation=3),
-            transforms.CenterCrop(input_height),
-            transforms.ToTensor(),
-        ])
+        self.normalize = normalize
+
+        # TODO: simple resize of random resized crop
+        data_transforms = [
+            transforms.Resize(self.input_height),
+            transforms.ToTensor()
+        ]
+
+        if self.normalize:
+            data_transforms.append(normalize)
+
+        self.test_transform = transforms.Compose(data_transforms)
 
     def __call__(self, sample):
         transform = self.test_transform
+
         xi = transform(sample)
         xj = transform(sample)
+
         return xi, xj
 
 
 class GaussianBlur(object):
-    """
-    Implements Gaussian blur as described in the SimCLR paper
-    """
-
-    def __init__(self, kernel_size: int, min: float = 0.1, max: float = 2.0):
-        if not _TORCHVISION_AVAILABLE:
-            raise ModuleNotFoundError(  # pragma: no-cover
-                'You want to use `transforms` from `torchvision` which is not installed yet.'
-            )
-
+    # Implements Gaussian blur as described in the SimCLR paper
+    def __init__(self, kernel_size, p=0.5, min=0.1, max=2.0):
         self.min = min
         self.max = max
+
         # kernel size is set to be 10% of the image height/width
         self.kernel_size = kernel_size
+        self.p = p
 
     def __call__(self, sample):
         sample = np.array(sample)
@@ -122,7 +150,7 @@ class GaussianBlur(object):
         # blur the image with a 50% chance
         prob = np.random.random_sample()
 
-        if prob < 0.5:
+        if prob < self.p:
             sigma = (self.max - self.min) * np.random.random_sample() + self.min
             sample = cv2.GaussianBlur(sample, (self.kernel_size, self.kernel_size), sigma)
 
