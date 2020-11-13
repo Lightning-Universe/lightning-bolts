0f178de7ba3ad90dacb213d8254292c3cb7f437f
ananyahjha93 <ananya@pytorchlightning.ai>
2020-11-13 12:25:51 -0500

    updates simclr

--- pl_bolts/models/self_supervised/simclr/simclr_module.py
+++ pl_bolts/models/self_supervised/simclr/simclr_module.py
@@ -14,7 +14,7 @@ except ModuleNotFoundError:
 
 from pl_bolts.losses.self_supervised_learning import nt_xent_loss
 from pl_bolts.models.self_supervised.evaluator import Flatten
-from pl_bolts.models.self_supervised.resnets import resnet50_bn
+from pl_bolts.models.self_supervised.resnets import resnet18, resnet50
 from pl_bolts.optimizers.lars_scheduling import LARSWrapper
 from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
 
@@ -36,17 +36,38 @@ class Projection(nn.Module):
         x = self.model(x)
         return F.normalize(x, dim=1)
 
-
+"""
+TODO:
+- update transform for onlnie_ft and finetuning later?
+1. update resent
+2. update loss func
+4. update finetuner
+"""
 class SimCLR(pl.LightningModule):
-    # TODO:
-    def __init__(self,
-                 batch_size: int,
-                 num_samples: int,
-                 warmup_epochs: int = 10,
-                 lr: float = 1e-4,
-                 opt_weight_decay: float = 1e-6,
-                 loss_temperature: float = 0.5,
-                 **kwargs):
+    def __init__(
+        self,
+        gpus: int,
+        nodes: int,
+        num_samples: int,
+        batch_size: int,
+        dataset: str,
+        arch: str = 'resnet50',
+        hidden_mlp: int = 2048,
+        feat_dim: int = 128,
+        warmup_epochs: int = 10,
+        max_epochs: int = 100,
+        temperature: float = 0.1,
+        first_conv: bool = True,
+        maxpool1: bool = True,
+        optimizer: str = 'adam',
+        lars_wrapper: bool = True,
+        exclude_bn_bias: bool = False,
+        start_lr: float = 0.,
+        learning_rate: float = 1e-3,
+        final_lr: float = 0.,
+        weight_decay: float = 1e-6,
+        **kwargs
+    ):
         """
         Args:
             batch_size: the batch size
@@ -59,63 +80,82 @@ class SimCLR(pl.LightningModule):
         super().__init__()
         self.save_hyperparameters()
 
-        self.nt_xent_loss = nt_xent_loss
-        self.encoder = self.init_encoder()
+        self.gpus = gpus
+        self.nodes = nodes
+        self.arch = arch
+        self.dataset = dataset
+        self.num_samples = num_samples
+        self.batch_size = batch_size
 
-        # h -> || -> z
-        self.projection = Projection()
+        self.hidden_mlp = hidden_mlp
+        self.feat_dim = feat_dim
+        self.first_conv = first_conv
+        self.maxpool1 = maxpool1
 
-    # TODO:
-    def init_encoder(self):
-        encoder = resnet50_bn(return_all_feature_maps=False)
+        self.optim = optimizer
+        self.lars_wrapper = lars_wrapper
+        self.exclude_bn_bias = exclude_bn_bias
+        self.weight_decay = weight_decay
+        self.temperature = temperature
 
-        # when using cifar10, replace the first conv so image doesn't shrink away
-        encoder.conv1 = nn.Conv2d(
-            3, 64,
-            kernel_size=3,
-            stride=1,
-            padding=1,
-            bias=False
+        self.start_lr = start_lr
+        self.final_lr = final_lr
+        self.learning_rate = learning_rate
+        self.warmup_epochs = warmup_epochs
+        self.max_epochs = max_epochs
+
+        self.encoder = self.init_model()
+
+        self.projection = Projection(
+            input_dim=args.hidden_mlp,
+            hidden_dim=args.hidden_mlp,
+            output_dim=args.feat_dim
         )
-        return encoder
 
-    # TODO:
-    def setup(self, stage):
-        global_batch_size = self.trainer.world_size * self.hparams.batch_size
-        self.train_iters_per_epoch = self.hparams.num_samples // global_batch_size
+        # compute iters per epoch
+        global_batch_size = self.nodes * self.gpus * self.batch_size if self.gpus > 0 else self.batch_size
+        self.train_iters_per_epoch = self.num_samples // global_batch_size
 
-    # TODO:
+        # define LR schedule
+        warmup_lr_schedule = np.linspace(
+            self.start_lr, self.learning_rate, self.train_iters_per_epoch * self.warmup_epochs
+        )
+        iters = np.arange(self.train_iters_per_epoch * (self.max_epochs - self.warmup_epochs))
+        cosine_lr_schedule = np.array([self.final_lr + 0.5 * (self.learning_rate - self.final_lr) * (
+            1 + math.cos(math.pi * t / (self.train_iters_per_epoch * (self.max_epochs - self.warmup_epochs)))
+        ) for t in iters])
+
+        self.lr_schedule = np.concatenate((warmup_lr_schedule, cosine_lr_schedule))
+
+    def init_model(self):
+        if self.arch == 'resnet18':
+            backbone = resnet18
+        elif self.arch == 'resnet50':
+            backbone = resnet50
+
+        return backbone(
+            first_conv=self.first_conv, maxpool1=self.maxpool1, return_all_feature_maps=False
+        )
+
     def forward(self, x):
-        if isinstance(x, list):
-            x = x[0]
+        return self.encoder(x)
 
-        result = self.encoder(x)
-        if isinstance(result, list):
-            result = result[-1]
-        return result
-
-    # TODO:
     def shared_step(self, batch, batch_idx):
+        if self.dataset == 'stl10':
+            unlabeled_batch = batch[0]
+            batch = unlabeled_batch
+
         (img1, img2), y = batch
 
-        # ENCODE
-        # encode -> representations
-        # (b, 3, 32, 32) -> (b, 2048, 2, 2)
+        # get h representations
         h1 = self.encoder(img1)
         h2 = self.encoder(img2)
 
-        # the bolts resnets return a list of feature maps
-        if isinstance(h1, list):
-            h1 = h1[-1]
-            h2 = h2[-1]
-
-        # PROJECT
-        # img -> E -> h -> || -> z
-        # (b, 2048, 2, 2) -> (b, 128)
+        # get z representations
         z1 = self.projection(h1)
         z2 = self.projection(h2)
 
-        loss = self.nt_xent_loss(z1, z2, self.hparams.loss_temperature)
+        loss = self.nt_xent_loss(z1, z2, self.temperature)
 
         return loss
 
@@ -213,29 +253,66 @@ class SimCLR(pl.LightningModule):
         else:
             optimizer.step(closure=optimizer_closure)
 
+    def nt_xent_loss(self, out_1, out_2, temperature):
+        """
+        Loss used in SimCLR
+        """
+        out = torch.cat([out_1, out_2], dim=0)
+        n_samples = len(out)
+
+        # Full similarity matrix
+        cov = torch.mm(out, out.t().contiguous())
+        sim = torch.exp(cov / temperature)
+
+        # Negative similarity
+        mask = ~torch.eye(n_samples, device=sim.device).bool()
+        neg = sim.masked_select(mask).view(n_samples, -1).sum(dim=-1)
+
+        # Positive similarity :
+        pos = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
+        pos = torch.cat([pos, pos], dim=0)
+        loss = -torch.log(pos / neg).mean()
+
+        return loss
+
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
-        parser.add_argument('--online_ft', action='store_true', help='run online finetuner')
-        parser.add_argument('--dataset', type=str, default='cifar10', help='cifar10, imagenet2012, stl10')
 
-        (args, _) = parser.parse_known_args()
-        # Data
-        parser.add_argument('--data_dir', type=str, default='.')
+        # model params
+        parser.add_argument("--arch", default="resnet50", type=str, help="convnet architecture")
+        # specify flags to store false
+        parser.add_argument("--first_conv", action='store_false')
+        parser.add_argument("--maxpool1", action='store_false')
+        parser.add_argument("--hidden_mlp", default=2048, type=int, help="hidden layer dimension in projection head")
+        parser.add_argument("--feat_dim", default=128, type=int, help="feature dimension")
+        parser.add_argument("--online_ft", action='store_true')
+        parser.add_argument("--fp32", action='store_true')
 
-        # Training
-        parser.add_argument('--optimizer', choices=['adam', 'lars'], default='lars')
-        parser.add_argument('--batch_size', type=int, default=512)
-        parser.add_argument('--learning_rate', type=float, default=1.0)
-        parser.add_argument('--lars_momentum', type=float, default=0.9)
-        parser.add_argument('--lars_eta', type=float, default=0.001)
-        parser.add_argument('--lr_sched_step', type=float, default=30, help='lr scheduler step')
-        parser.add_argument('--lr_sched_gamma', type=float, default=0.5, help='lr scheduler step')
-        parser.add_argument('--weight_decay', type=float, default=1e-4)
-        # Model
-        parser.add_argument('--loss_temperature', type=float, default=0.5)
-        parser.add_argument('--num_workers', default=0, type=int)
-        parser.add_argument('--meta_dir', default='.', type=str, help='path to meta.bin for imagenet')
+        # transform params
+        parser.add_argument("--gaussian_blur", action="store_true", help="add gaussian blur")
+        parser.add_argument("--jitter_strength", type=float, default=1.0, help="jitter strength")
+        parser.add_argument("--dataset", type=str, default="stl10", help="stl10, cifar10")
+        parser.add_argument("--data_path", type=str, default=".", help="path to download data")
+
+        # training params
+        parser.add_argument("--fast_dev_run", action='store_true')
+        parser.add_argument("--nodes", default=1, type=int, help="number of nodes for training")
+        parser.add_argument("--gpus", default=1, type=int, help="number of gpus to train on")
+        parser.add_argument("--num_workers", default=8, type=int, help="num of workers per GPU")
+        parser.add_argument("--optimizer", default="adam", type=str, help="choose between adam/sgd")
+        parser.add_argument("--lars_wrapper", action='store_true', help="apple lars wrapper over optimizer used")
+        parser.add_argument('--exclude_bn_bias', action='store_true', help="exclude bn/bias from weight decay")
+        parser.add_argument("--max_epochs", default=100, type=int, help="number of total epochs to run")
+        parser.add_argument("--max_steps", default=-1, type=int, help="max steps")
+        parser.add_argument("--warmup_epochs", default=10, type=int, help="number of warmup epochs")
+        parser.add_argument("--batch_size", default=128, type=int, help="batch size per gpu")
+
+        parser.add_argument("--temperature", default=0.1, type=float, help="temperature parameter in training loss")
+        parser.add_argument("--weight_decay", default=1e-6, type=float, help="weight decay")
+        parser.add_argument("--learning_rate", default=1e-3, type=float, help="base learning rate")
+        parser.add_argument("--start_lr", default=0, type=float, help="initial warmup learning rate")
+        parser.add_argument("--final_lr", type=float, default=1e-6, help="final learning rate")
 
         return parser
 
@@ -251,8 +328,6 @@ def cli_main():
     parser = SimCLR.add_model_specific_args(parser)
     args = parser.parse_args()
 
-########################
-# TODO: chagen prams and transforms for simclr
     if args.dataset == 'stl10':
         dm = STL10DataModule(
             data_dir=args.data_path,
@@ -265,12 +340,14 @@ def cli_main():
         args.num_samples = dm.num_unlabeled_samples
 
         args.maxpool1 = False
+        args.first_conv = True
+        args.input_height = dm.size()[-1]
 
         normalization = stl10_normalization()
-    elif args.dataset == 'cifar10':
-        args.batch_size = 2
-        args.num_workers = 0
 
+        args.gaussian_blur = True
+        args.jitter_strength = 1.
+    elif args.dataset == 'cifar10':
         dm = CIFAR10DataModule(
             data_dir=args.data_path,
             batch_size=args.batch_size,
@@ -281,21 +358,17 @@ def cli_main():
 
         args.maxpool1 = False
         args.first_conv = False
+        args.input_height = dm.size()[-1]
 
         normalization = cifar10_normalization()
 
-        # cifar10 specific params
-        args.size_crops = [32, 16]
-        args.nmb_crops = [2, 1]
         args.gaussian_blur = False
+        args.jitter_strength = 0.5
     elif args.dataset == 'imagenet':
         args.maxpool1 = True
         args.first_conv = True
         normalization = imagenet_normalization()
 
-        args.size_crops = [224, 96]
-        args.min_scale_crops = [0.14, 0.05]
-        args.max_scale_crops = [1., 0.14]
         args.gaussian_blur = True
         args.jitter_strength = 1.
 
@@ -309,8 +382,6 @@ def cli_main():
         args.learning_rate = 4.8
         args.final_lr = 0.0048
         args.start_lr = 0.3
-
-        args.nmb_prototypes = 3000
         args.online_ft = True
 
         dm = ImagenetDataModule(
@@ -324,26 +395,17 @@ def cli_main():
     else:
         raise NotImplementedError("other datasets have not been implemented till now")
 
-    dm.train_transforms = SwAVTrainDataTransform(
-        normalize=normalization,
-        size_crops=args.size_crops,
-        nmb_crops=args.nmb_crops,
-        min_scale_crops=args.min_scale_crops,
-        max_scale_crops=args.max_scale_crops,
+    dm.train_transforms = SimCLRTrainDataTransform(
+        input_height=args.input_height,
         gaussian_blur=args.gaussian_blur,
-        jitter_strength=args.jitter_strength
+        jitter_strength=args.jitter_strength,
+        normalize=normalization,
     )
 
-    dm.val_transforms = SwAVEvalDataTransform(
+    dm.val_transforms = SimCLREvalDataTransform(
+        input_height=args.input_height,
         normalize=normalization,
-        size_crops=args.size_crops,
-        nmb_crops=args.nmb_crops,
-        min_scale_crops=args.min_scale_crops,
-        max_scale_crops=args.max_scale_crops,
-        gaussian_blur=args.gaussian_blur,
-        jitter_strength=args.jitter_strength
     )
-########################
 
     model = SimCLR(**args.__dict__)
 
--- pl_bolts/models/self_supervised/swav/swav_module.py
+++ pl_bolts/models/self_supervised/swav/swav_module.py
@@ -444,7 +444,7 @@ class SwAV(pl.LightningModule):
         parser.add_argument("--fast_dev_run", action='store_true')
         parser.add_argument("--nodes", default=1, type=int, help="number of nodes for training")
         parser.add_argument("--gpus", default=1, type=int, help="number of gpus to train on")
-        parser.add_argument("--num_workers", default=16, type=int, help="num of workers per GPU")
+        parser.add_argument("--num_workers", default=8, type=int, help="num of workers per GPU")
         parser.add_argument("--optimizer", default="adam", type=str, help="choose between adam/sgd")
         parser.add_argument("--lars_wrapper", action='store_true', help="apple lars wrapper over optimizer used")
         parser.add_argument('--exclude_bn_bias', action='store_true', help="exclude bn/bias from weight decay")
