c3bc62a9b52f06b6969d63d7d35d628c3df22e06
ananyahjha93 <ananya@pytorchlightning.ai>
2020-11-15 04:15:30 -0500

    sync

--- pl_bolts/models/self_supervised/simclr/simclr_module.py
+++ pl_bolts/models/self_supervised/simclr/simclr_module.py
@@ -27,11 +27,12 @@ def concat_all_gather(tensor):
     Performs all_gather operation on the provided tensors.
     *** Warning ***: torch.distributed.all_gather has no gradient.
     """
-    tensors_gather = [torch.ones_like(tensor)
-                      for _ in range(torch.distributed.get_world_size())]
-    torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
+    with torch.no_grad():
+        tensors_gather = [torch.ones_like(tensor)
+                        for _ in range(torch.distributed.get_world_size())]
+        torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
 
-    output = torch.cat(tensors_gather, dim=0)
+        output = torch.cat(tensors_gather, dim=0)
     return output
 
 
