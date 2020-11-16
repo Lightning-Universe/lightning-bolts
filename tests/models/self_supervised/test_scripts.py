e72ce70b926838b2827acd3e29036b2bc8f7d812
ananyahjha93 <ananya@pytorchlightning.ai>
2020-11-15 22:25:38 -0500

    fix tests

--- tests/models/self_supervised/test_scripts.py
+++ tests/models/self_supervised/test_scripts.py
@@ -45,7 +45,7 @@ def test_cli_run_self_supervised_moco(cli_args):
 
 
 @pytest.mark.parametrize('cli_args', [
-    f'--data_dir {DATASETS_PATH} --max_epochs 1 --max_steps 3 --fast_dev_run --batch_size 2 --online_ft'
+    f'--data_path {DATASETS_PATH} --max_epochs 1 --max_steps 3 --fast_dev_run --batch_size 2 --online_ft'
 ])
 def test_cli_run_self_supervised_simclr(cli_args):
     """Test running CLI for an example with default params."""
@@ -57,7 +57,7 @@ def test_cli_run_self_supervised_simclr(cli_args):
 
 
 @pytest.mark.parametrize('cli_args', [
-    f'--data_path {DATASETS_PATH} --max_epochs 1 --max_steps 3 --fast_dev_run --batch_size 2 --online_ft'
+    f'--data_dir {DATASETS_PATH} --max_epochs 1 --max_steps 3 --fast_dev_run --batch_size 2 --online_ft'
 ])
 def test_cli_run_self_supervised_byol(cli_args):
     """Test running CLI for an example with default params."""
