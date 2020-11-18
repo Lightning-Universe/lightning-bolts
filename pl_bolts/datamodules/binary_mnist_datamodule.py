107703a407272e1cc2967ef92b47f345b6511576
Annika Brundyn <annikabrundyn1@gmail.com>
2020-11-18 11:57:20 -0500

    download mnist

--- pl_bolts/datamodules/binary_mnist_datamodule.py
+++ pl_bolts/datamodules/binary_mnist_datamodule.py
@@ -10,7 +10,7 @@ try:
 
     from pl_bolts.datasets.mnist_dataset import BinaryMNIST
 except ModuleNotFoundError:
-    warn_missing_pkg('torchvision')  # pragma: no-cover
+    warn_missing_pkg('torchvision')  # pragma: no-cover12
     _TORCHVISION_AVAILABLE = False
 else:
     _TORCHVISION_AVAILABLE = True
@@ -44,7 +44,7 @@ class BinaryMNISTDataModule(LightningDataModule):
         Trainer().fit(model, dm)
     """
 
-    name = 'mnist'
+    name = "binary_mnist"
 
     def __init__(
             self,
@@ -89,8 +89,8 @@ class BinaryMNISTDataModule(LightningDataModule):
         """
         Saves MNIST files to data_dir
         """
-        MNIST(self.data_dir, train=True, download=True, transform=transform_lib.ToTensor())
-        MNIST(self.data_dir, train=False, download=True, transform=transform_lib.ToTensor())
+        BinaryMNIST(self.data_dir, train=True, download=True, transform=transform_lib.ToTensor())
+        BinaryMNIST(self.data_dir, train=False, download=True, transform=transform_lib.ToTensor())
 
     def train_dataloader(self, batch_size=32, transforms=None):
         """
