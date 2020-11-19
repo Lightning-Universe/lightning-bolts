496ad8d470658ee620805895ff168d170c8bdd6f
Aditya Oke <okeaditya315@gmail.com>
2020-11-20 01:08:04 +0530

    adds smoke test

---
+++ tests/test_smoke.py
@@ -0,0 +1,7 @@
+from pl_bolts.callbacks import *
+from pl_bolts.datamodules import *
+from pl_bolts.datasets import *
+from pl_bolts.losses import *
+from pl_bolts.models import *
+from pl_bolts.optimizers import *
+from pl_bolts.utils import *
