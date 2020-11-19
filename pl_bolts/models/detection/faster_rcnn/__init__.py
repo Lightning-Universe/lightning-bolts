bf649b0bb04181eb30c243e939499a552b3f275e
Aditya Oke <okeaditya315@gmail.com>
2020-11-20 00:09:47 +0530

    fixes bugs

--- pl_bolts/models/detection/faster_rcnn/__init__.py
+++ pl_bolts/models/detection/faster_rcnn/__init__.py
@@ -1,2 +1,2 @@
-from pl_bolts.models.detection.faster_rcnn.faster_rcnn import FRCNN
+from pl_bolts.models.detection.faster_rcnn.faster_rcnn_module import FRCNN
 from pl_bolts.models.detection.faster_rcnn.backbones import create_fasterrcnn_backbone
