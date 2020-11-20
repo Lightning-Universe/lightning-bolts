ff8cd589328a32557c9f1f68356f94a296f07ab4
Akihiro Nitta <nitta@akihironitta.com>
2020-11-20 18:32:07 +0900

    Add more to the tests

--- tests/datamodules/test_imports.py
+++ tests/datamodules/test_imports.py
@@ -5,7 +5,25 @@ import pytest
 
 
 @pytest.mark.parametrize("dm_cls,deps", [
+    ("AsynchronousLoader", []),
+    ("BinaryMNISTDataModule", ["torchvision"]),
+    ("CIFAR10DataModule", ["torchvision"]),
+    ("TinyCIFAR10DataModule", ["torchvision"]),
+    ("DiscountedExperienceSource", ["gym"]),
+    ("ExperienceSource", ["gym"]),
+    ("ExperienceSourceDataset", ["gym"]),
+    ("FashionMNISTDataModule", ["torchvision"]),
+    ("ImagenetDataModule", ["torchvision"]),
+    ("MNISTDataModule", ["torchvision"]),
     ("SklearnDataModule", ["sklearn"]),
+    ("SklearnDataset", []),
+    ("TensorDataset", []),
+    ("SSLImagenetDataModule", ["torchvision"]),
+    ("STL10DataModule", ["torchvision"]),
+    ("VOCDetectionDataModule", ["torchvision"]),
+    ("CityscapesDataModule", ["torchvision"]),
+    ("KittiDataset", ["PIL"]),
+    ("KittiDataModule", ["PIL"]),
 ])
 def test_import(dm_cls, deps):
     with mock.patch.dict("sys.modules", {pkg: None for pkg in deps}):
