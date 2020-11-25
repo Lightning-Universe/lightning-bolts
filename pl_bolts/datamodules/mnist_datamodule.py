69dfd5af552a6e026221b29b02940c9ef7421ce3
Akihiro Nitta <nitta@akihironitta.com>
2020-11-26 05:02:39 +0900

    mnist

--- pl_bolts/datamodules/mnist_datamodule.py
+++ pl_bolts/datamodules/mnist_datamodule.py
@@ -2,16 +2,14 @@ import torch
 from pytorch_lightning import LightningDataModule
 from torch.utils.data import DataLoader, random_split
 
+from pl_bolts import _TORCHVISION_AVAILABLE
 from pl_bolts.utils.warnings import warn_missing_pkg
 
-try:
+if _TORCHVISION_AVAILABLE:
     from torchvision import transforms as transform_lib
     from torchvision.datasets import MNIST
-except ModuleNotFoundError:
-    warn_missing_pkg('torchvision')  # pragma: no-cover
-    _TORCHVISION_AVAILABLE = False
-else:
-    _TORCHVISION_AVAILABLE = True
+else:  # pragma: no-cover
+    warn_missing_pkg('torchvision')
 
 
 class MNISTDataModule(LightningDataModule):
