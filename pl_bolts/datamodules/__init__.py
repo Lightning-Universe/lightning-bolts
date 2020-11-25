7113645abe435b3e2e2834856a54c0330d6edaab
Akihiro Nitta <nitta@akihironitta.com>
2020-11-26 04:26:30 +0900

    Apply isort

--- pl_bolts/datamodules/__init__.py
+++ pl_bolts/datamodules/__init__.py
@@ -1,29 +1,17 @@
 from pl_bolts.datamodules.async_dataloader import AsynchronousLoader
 from pl_bolts.datamodules.binary_mnist_datamodule import BinaryMNISTDataModule
-from pl_bolts.datamodules.cifar10_datamodule import (
-    CIFAR10DataModule,
-    TinyCIFAR10DataModule,
-)
-from pl_bolts.datamodules.experience_source import (
-    ExperienceSourceDataset,
-    ExperienceSource,
-    DiscountedExperienceSource,
-)
+from pl_bolts.datamodules.cifar10_datamodule import CIFAR10DataModule, TinyCIFAR10DataModule
+from pl_bolts.datamodules.cityscapes_datamodule import CityscapesDataModule
+from pl_bolts.datamodules.experience_source import DiscountedExperienceSource, ExperienceSource, ExperienceSourceDataset
 from pl_bolts.datamodules.fashion_mnist_datamodule import FashionMNISTDataModule
 from pl_bolts.datamodules.imagenet_datamodule import ImagenetDataModule
+from pl_bolts.datamodules.kitti_datamodule import KittiDataModule
 from pl_bolts.datamodules.mnist_datamodule import MNISTDataModule
-from pl_bolts.datamodules.sklearn_datamodule import (
-    SklearnDataset,
-    SklearnDataModule,
-    TensorDataset,
-)
+from pl_bolts.datamodules.sklearn_datamodule import SklearnDataModule, SklearnDataset, TensorDataset
 from pl_bolts.datamodules.ssl_imagenet_datamodule import SSLImagenetDataModule
 from pl_bolts.datamodules.stl10_datamodule import STL10DataModule
 from pl_bolts.datamodules.vocdetection_datamodule import VOCDetectionDataModule
-from pl_bolts.datamodules.cityscapes_datamodule import CityscapesDataModule
 from pl_bolts.datasets.kitti_dataset import KittiDataset
-from pl_bolts.datamodules.kitti_datamodule import KittiDataModule
-
 
 __all__ = [
     'BinaryMNISTDataModule',
