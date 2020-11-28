36344b48e9f9703ac0c5c9b2077071fdfe8aaf59
Christoph Clement <christoph.clement@tum.de>
2020-11-28 18:08:13 +0100

    Add stl10_sr dataset and datamodule

---
+++ pl_bolts/datamodules/stl10_sr_datamodule.py
@@ -0,0 +1,63 @@
+import os
+from typing import Optional
+
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import DataLoader
+
+from pl_bolts.datasets.concat_dataset import ConcatDataset
+from pl_bolts.datasets.stl10_sr_dataset import STL10_SR
+
+
+class STL10_SR_DataModule(LightningDataModule):
+
+    name = 'stl10_sr'
+
+    def __init__(
+            self,
+            data_dir: Optional[str] = None,
+            num_workers: int = 12,
+            batch_size: int = 32,
+            *args,
+            **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+
+        self.dataset_cls = STL10_SR
+        self.dims = (3, 96, 96)
+        self.data_dir = data_dir if data_dir is not None else os.getcwd()
+        self.num_workers = num_workers
+        self.batch_size = batch_size
+
+    @property
+    def num_classes(self):
+        return 10
+
+    def prepare_data(self):
+        self.dataset_cls(self.data_dir, split='unlabeled', download=True)
+        self.dataset_cls(self.data_dir, split='train', download=True)
+        self.dataset_cls(self.data_dir, split='test', download=True)
+
+    def setup(self, stage=None):
+        if stage == "fit" or stage is None:
+            dataset_labeled = self.dataset_cls(self.data_dir, split='train')
+            dataset_unlabeled = self.dataset_cls(self.data_dir, split='unlabeled')
+            self.dataset_train = ConcatDataset(dataset_labeled, dataset_unlabeled)
+
+        if stage == "test" or stage is None:
+            self.dataset_test = self.dataset_cls(self.data_dir, split='test')
+
+    def train_dataloader(self):
+        return self._dataloader(self.dataset_train)
+
+    def test_dataloader(self):
+        return self._dataloader(self.dataset_test, shuffle=False)
+
+    def _dataloader(self, dataset, shuffle=True):
+        return DataLoader(
+            dataset,
+            batch_size=self.batch_size,
+            shuffle=shuffle,
+            num_workers=self.num_workers,
+            drop_last=True,
+            pin_memory=True
+        )
---
+++ pl_bolts/datasets/stl10_sr_dataset.py
@@ -0,0 +1,42 @@
+import numpy as np
+from PIL import Image
+
+from pl_bolts.utils.warnings import warn_missing_pkg
+
+try:
+    from torchvision import transforms as transform_lib
+    from torchvision.datasets import STL10
+except ModuleNotFoundError:
+    warn_missing_pkg('torchvision')  # pragma: no-cover
+    _TORCHVISION_AVAILABLE = False
+else:
+    _TORCHVISION_AVAILABLE = True
+    
+
+class STL10_SR(STL10):
+    def __init__(self, hr_size, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        mean = np.array([0.485, 0.456, 0.406])
+        std = np.array([0.229, 0.224, 0.225])
+
+        self.hr_transforms = transform_lib.Compose([
+            transform_lib.Resize(hr_size, Image.BICUBIC),
+            transform_lib.ToTensor(),
+            transform_lib.Normalize(mean, std),
+        ])
+
+        self.lr_transforms = transform_lib.Compose([
+            transform_lib.Resize(hr_size // 4, Image.BICUBIC),
+            transform_lib.ToTensor(),
+            transform_lib.Normalize(mean, std),
+        ])
+
+    def __getitem__(self, index):
+        data = self.data[index]
+        image = Image.fromarray(np.transpose(data, (1, 2, 0)))
+
+        hr_image = self.hr_transforms(image)
+        lr_image = self.lr_transforms(image)
+
+        return hr_image, lr_image
