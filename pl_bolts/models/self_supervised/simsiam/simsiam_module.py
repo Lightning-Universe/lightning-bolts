58ffec24e8c9990badc67d3b276ce5a4716416e1
Zvi Lapp <lappzvi@gmail.com>
2020-11-28 18:26:28 +0200

    black reformatted

--- pl_bolts/models/self_supervised/simsiam/simsiam_module.py
+++ pl_bolts/models/self_supervised/simsiam/simsiam_module.py
@@ -59,16 +59,19 @@ class SimSiam(pl.LightningModule):
             --meta_dir /path/to/folder/with/meta.bin/
             --batch_size 32
     """
-    def __init__(self,
-                 num_classes,
-                 learning_rate: float = 0.2,
-                 weight_decay: float = 1.5e-6,
-                 input_height: int = 32,
-                 batch_size: int = 32,
-                 num_workers: int = 0,
-                 warmup_epochs: int = 10,
-                 max_epochs: int = 1000,
-                 **kwargs):
+
+    def __init__(
+        self,
+        num_classes,
+        learning_rate: float = 0.2,
+        weight_decay: float = 1.5e-6,
+        input_height: int = 32,
+        batch_size: int = 32,
+        num_workers: int = 0,
+        warmup_epochs: int = 10,
+        max_epochs: int = 1000,
+        **kwargs
+    ):
         """
         Args:
             datamodule: The datamodule
@@ -89,7 +92,9 @@ class SimSiam(pl.LightningModule):
     def _init_target_network(self):
         self.target_network = deepcopy(self.online_network)
 
-    def on_train_batch_end(self, outputs, batch: Any, batch_idx: int, dataloader_idx: int) -> None:
+    def on_train_batch_end(
+        self, outputs, batch: Any, batch_idx: int, dataloader_idx: int
+    ) -> None:
         self._init_target_network()
 
     def forward(self, x):
@@ -127,7 +132,9 @@ class SimSiam(pl.LightningModule):
         loss_a, loss_b, total_loss = self.shared_step(batch, batch_idx)
 
         # log results
-        self.log_dict({'1_2_loss': loss_a, '2_1_loss': loss_b, 'train_loss': total_loss})
+        self.log_dict(
+            {"1_2_loss": loss_a, "2_1_loss": loss_b, "train_loss": total_loss}
+        )
 
         return total_loss
 
@@ -135,48 +142,70 @@ class SimSiam(pl.LightningModule):
         loss_a, loss_b, total_loss = self.shared_step(batch, batch_idx)
 
         # log results
-        self.log_dict({'1_2_loss': loss_a, '2_1_loss': loss_b, 'train_loss': total_loss})
+        self.log_dict(
+            {"1_2_loss": loss_a, "2_1_loss": loss_b, "train_loss": total_loss}
+        )
 
         return total_loss
 
     def configure_optimizers(self):
-        optimizer = Adam(self.online_network.parameters(), lr=self.hparams.learning_rate, weight_decay=self.hparams.weight_decay)
+        optimizer = Adam(
+            self.online_network.parameters(),
+            lr=self.hparams.learning_rate,
+            weight_decay=self.hparams.weight_decay,
+        )
         optimizer = LARSWrapper(optimizer)
         scheduler = LinearWarmupCosineAnnealingLR(
             optimizer,
             warmup_epochs=self.hparams.warmup_epochs,
-            max_epochs=self.hparams.max_epochs
+            max_epochs=self.hparams.max_epochs,
         )
         return [optimizer], [scheduler]
 
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
-        parser.add_argument('--online_ft', action='store_true', help='run online finetuner')
-        parser.add_argument('--dataset', type=str, default='cifar10', help='cifar10, imagenet2012, stl10')
+        parser.add_argument(
+            "--online_ft", action="store_true", help="run online finetuner"
+        )
+        parser.add_argument(
+            "--dataset",
+            type=str,
+            default="cifar10",
+            help="cifar10, imagenet2012, stl10",
+        )
 
         (args, _) = parser.parse_known_args()
 
         # Data
-        parser.add_argument('--data_dir', type=str, default='.')
-        parser.add_argument('--num_workers', default=0, type=int)
+        parser.add_argument("--data_dir", type=str, default=".")
+        parser.add_argument("--num_workers", default=0, type=int)
 
         # optim
-        parser.add_argument('--batch_size', type=int, default=256)
-        parser.add_argument('--learning_rate', type=float, default=1e-3)
-        parser.add_argument('--weight_decay', type=float, default=1.5e-6)
-        parser.add_argument('--warmup_epochs', type=float, default=10)
+        parser.add_argument("--batch_size", type=int, default=256)
+        parser.add_argument("--learning_rate", type=float, default=1e-3)
+        parser.add_argument("--weight_decay", type=float, default=1.5e-6)
+        parser.add_argument("--warmup_epochs", type=float, default=10)
 
         # Model
-        parser.add_argument('--meta_dir', default='.', type=str, help='path to meta.bin for imagenet')
+        parser.add_argument(
+            "--meta_dir", default=".", type=str, help="path to meta.bin for imagenet"
+        )
 
         return parser
 
 
 def cli_main():
     from pl_bolts.callbacks.ssl_online import SSLOnlineEvaluator
-    from pl_bolts.datamodules import CIFAR10DataModule, ImagenetDataModule, STL10DataModule
-    from pl_bolts.models.self_supervised.simclr import SimCLREvalDataTransform, SimCLRTrainDataTransform
+    from pl_bolts.datamodules import (
+        CIFAR10DataModule,
+        ImagenetDataModule,
+        STL10DataModule,
+    )
+    from pl_bolts.models.self_supervised.simclr import (
+        SimCLREvalDataTransform,
+        SimCLRTrainDataTransform,
+    )
 
     seed_everything(1234)
 
@@ -193,13 +222,13 @@ def cli_main():
     dm = None
 
     # init default datamodule
-    if args.dataset == 'cifar10':
+    if args.dataset == "cifar10":
         dm = CIFAR10DataModule.from_argparse_args(args)
         dm.train_transforms = SimCLRTrainDataTransform(32)
         dm.val_transforms = SimCLREvalDataTransform(32)
         args.num_classes = dm.num_classes
 
-    elif args.dataset == 'stl10':
+    elif args.dataset == "stl10":
         dm = STL10DataModule.from_argparse_args(args)
         dm.train_dataloader = dm.train_dataloader_mixed
         dm.val_dataloader = dm.val_dataloader_mixed
@@ -209,7 +238,7 @@ def cli_main():
         dm.val_transforms = SimCLREvalDataTransform(h)
         args.num_classes = dm.num_classes
 
-    elif args.dataset == 'imagenet2012':
+    elif args.dataset == "imagenet2012":
         dm = ImagenetDataModule.from_argparse_args(args, image_size=196)
         (c, h, w) = dm.size()
         dm.train_transforms = SimCLRTrainDataTransform(h)
@@ -219,12 +248,16 @@ def cli_main():
     model = SimSiam(**args.__dict__)
 
     # finetune in real-time
-    online_eval = SSLOnlineEvaluator(dataset=args.dataset, z_dim=2048, num_classes=dm.num_classes)
+    online_eval = SSLOnlineEvaluator(
+        dataset=args.dataset, z_dim=2048, num_classes=dm.num_classes
+    )
 
-    trainer = pl.Trainer.from_argparse_args(args, max_steps=300000, callbacks=[online_eval])
+    trainer = pl.Trainer.from_argparse_args(
+        args, max_steps=300000, callbacks=[online_eval]
+    )
 
     trainer.fit(model, dm)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli_main()
