e25dc6447750109567cea8431b48f7378c3a5d1b
Brian Ko <briankosw@gmail.com>
2020-12-01 14:51:08 +0900

    Fixing syntax error

--- pl_bolts/datamodules/ssl_imagenet_datamodule.py
+++ pl_bolts/datamodules/ssl_imagenet_datamodule.py
@@ -111,7 +111,7 @@ class SSLImagenetDataModule(LightningDataModule):  # pragma: no cover
             batch_size=batch_size,
             shuffle=self.shuffle,
             num_workers=self.num_workers,
-            drop_last=self.drop_last
+            drop_last=self.drop_last,
             pin_memory=self.pin_memory
         )
         return loader
