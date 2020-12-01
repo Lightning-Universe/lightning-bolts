0f50cb59b33854bdb331f6362aea66efb72542af
Brian Ko <briankosw@gmail.com>
2020-12-01 14:53:30 +0900

    More syntax errors

--- pl_bolts/datamodules/stl10_datamodule.py
+++ pl_bolts/datamodules/stl10_datamodule.py
@@ -275,7 +275,7 @@ class STL10DataModule(LightningDataModule):  # pragma: no cover
             batch_size=self.batch_size,
             shuffle=self.shuffle,
             num_workers=self.num_workers,
-            drop_last=self.drop_last
+            drop_last=self.drop_last, 
             pin_memory=self.pin_memory
         )
         return loader
--- pl_bolts/datamodules/vocdetection_datamodule.py
+++ pl_bolts/datamodules/vocdetection_datamodule.py
@@ -171,7 +171,7 @@ class VOCDetectionDataModule(LightningDataModule):
             batch_size=batch_size,
             shuffle=self.shuffle,
             num_workers=self.num_workers,
-            drop_last=self.drop_last
+            drop_last=self.drop_last,
             pin_memory=self.pin_memory,
             collate_fn=_collate_fn,
         )
@@ -198,7 +198,7 @@ class VOCDetectionDataModule(LightningDataModule):
             batch_size=batch_size,
             shuffle=self.shuffle,
             num_workers=self.num_workers,
-            drop_last=self.drop_last
+            drop_last=self.drop_last,
             pin_memory=self.pin_memory,
             collate_fn=_collate_fn,
         )
