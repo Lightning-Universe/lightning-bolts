61712e9faa7db80c18189dadc604679b3ffa81af
Akihiro Nitta <nitta@akihironitta.com>
2020-12-07 13:20:38 +0900

    Apply isort

--- pl_bolts/__init__.py
+++ pl_bolts/__init__.py
@@ -43,17 +43,7 @@ if __LIGHTNING_BOLT_SETUP__:
     sys.stdout.write(f'Partial import of `{__name__}` during the build process.\n')  # pragma: no-cover
     # We are not importing the rest of the lightning during the build process, as it may not be compiled yet
 else:
-    from pl_bolts import (
-        callbacks,
-        datamodules,
-        datasets,
-        losses,
-        metrics,
-        models,
-        optimizers,
-        transforms,
-        utils,
-    )
+    from pl_bolts import callbacks, datamodules, datasets, losses, metrics, models, optimizers, transforms, utils
 
     __all__ = [
         'callbacks',
