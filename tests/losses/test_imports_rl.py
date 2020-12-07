862d09200de41ae834f81dc4c7e531804537086f
Akihiro Nitta <nitta@akihironitta.com>
2020-12-07 11:20:03 +0900

    Add test_imports for losses

---
+++ tests/losses/test_imports_rl.py
@@ -0,0 +1,22 @@
+import importlib
+from unittest import mock
+
+import pytest
+
+from tests import optional_pkg_names
+
+
+@pytest.mark.parametrize("name", [
+    "dqn_loss",
+    "per_dqn_loss",
+])
+def test_import(name):
+    """Tests importing when dependencies are not met.
+
+    Set the following in @pytest.mark.parametrize:
+        name: class, function or variable name to test importing
+    """
+    with mock.patch.dict("sys.modules", {pkg: None for pkg in optional_pkg_names}):
+        module_name = "pl_bolts.losses.rl"
+        module = importlib.import_module(module_name)
+        assert hasattr(module, name), f"`from {module_name} import {name}` failed."
---
+++ tests/losses/test_imports_self_supervised_learning.py
@@ -0,0 +1,25 @@
+import importlib
+from unittest import mock
+
+import pytest
+
+from tests import optional_pkg_names
+
+
+@pytest.mark.parametrize("name", [
+    "nt_xent_loss",
+    "CPCTask",
+    "AmdimNCELoss",
+    "FeatureMapContrastiveTask",
+    "tanh_clip",
+])
+def test_import(name):
+    """Tests importing when dependencies are not met.
+
+    Set the following in @pytest.mark.parametrize:
+        name: class, function or variable name to test importing
+    """
+    with mock.patch.dict("sys.modules", {pkg: None for pkg in optional_pkg_names}):
+        module_name = "pl_bolts.losses.self_supervised_learning"
+        module = importlib.import_module(module_name)
+        assert hasattr(module, name), f"`from {module_name} import {name}` failed."
