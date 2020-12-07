61e5809ec0f237d4b9496ee3ca896e428853e462
Akihiro Nitta <nitta@akihironitta.com>
2020-12-07 13:43:38 +0900

    Update test_imports for losses

--- tests/losses/test_imports.py
+++ tests/losses/test_imports.py
@@ -7,6 +7,22 @@ from tests import optional_pkg_names
 
 
 @pytest.mark.parametrize("name", [
+    "dqn_loss",
+    "per_dqn_loss",
+])
+def test_import_rl(name):
+    """Tests importing when dependencies are not met.
+
+    Set the following in @pytest.mark.parametrize:
+        name: class, function or variable name to test importing
+    """
+    with mock.patch.dict("sys.modules", {pkg: None for pkg in optional_pkg_names}):
+        module_name = "pl_bolts.losses.rl"
+        module = importlib.import_module(module_name)
+        assert hasattr(module, name), f"`from {module_name} import {name}` failed."
+
+
+@pytest.mark.parametrize("name", [
     "nt_xent_loss",
     "CPCTask",
     "AmdimNCELoss",
