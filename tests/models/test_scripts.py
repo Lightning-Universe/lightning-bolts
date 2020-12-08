e8040a918162f0a2c7c1a5b872dd5ea2e47e88ac
Jirka Borovec <Borda@users.noreply.github.com>
2020-12-08 10:37:07 +0100

    skip hanging (#437)

--- tests/models/test_scripts.py
+++ tests/models/test_scripts.py
@@ -98,6 +98,8 @@ def test_cli_run_log_regression(cli_args):
         cli_main()
 
 
+# TODO: this test is hanging (runs for more then 10min) so we need to use GPU or optimize it...
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="test requires GPU machine")
 @pytest.mark.parametrize('cli_args', [f'--data_dir {DATASETS_PATH} --max_epochs 1 --max_steps 2'])
 def test_cli_run_vision_image_gpt(cli_args):
     """Test running CLI for an example with default params."""
