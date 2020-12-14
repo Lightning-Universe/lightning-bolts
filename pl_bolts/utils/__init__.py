9ce2f71c4413ef24fecd1404a7283c0aee7eac4f
ananyahjha93 <ananya@pytorchlightning.ai>
2020-12-14 12:52:46 -0500

    removed _module_avail

--- pl_bolts/utils/__init__.py
+++ pl_bolts/utils/__init__.py
@@ -1,9 +1,6 @@
 from importlib.util import find_spec
 
 import torch
-from pytorch_lightning.utilities import _module_available
-
-_NATIVE_AMP_AVAILABLE = _module_available("torch.cuda.amp") and hasattr(torch.cuda.amp, "autocast")
 
 _TORCHVISION_AVAILABLE = find_spec("torchvision") is not None
 _GYM_AVAILABLE = find_spec("gym") is not None
