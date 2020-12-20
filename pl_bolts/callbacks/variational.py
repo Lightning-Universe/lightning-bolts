00f8c2c31c3a97407e1a4838c4bb2e36fe550cda
Jirka Borovec <jirka.borovec@seznam.cz>
2020-12-21 00:13:32 +0100

    flake8

--- pl_bolts/callbacks/variational.py
+++ pl_bolts/callbacks/variational.py
@@ -38,15 +38,6 @@ class LatentDimInterpolator(Callback):
         num_samples: int = 2,
         normalize: bool = True,
     ):
-        """	
-        Args:	
-            interpolate_epoch_interval: default 20	
-            range_start: default -5	
-            range_end: default 5	
-            steps: number of step between start and end	
-            num_samples: default 2	
-            normalize: default True (change image to (0, 1) range)	
-        """
         super().__init__()
         self.interpolate_epoch_interval = interpolate_epoch_interval
         self.range_start = range_start
