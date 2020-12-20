1e0b2d141c0317f4c437fba3de940b75c738088f
Jirka Borovec <jirka.borovec@seznam.cz>
2020-12-21 00:19:33 +0100

    format

--- tests/metrics/test_object_detection.py
+++ tests/metrics/test_object_detection.py
@@ -8,34 +8,16 @@ import torch
 from pl_bolts.metrics.object_detection import giou
 
 
-@pytest.mark.parametrize(
-    "preds, target, expected_giou",
-    [
-        (
-            torch.tensor([[100, 100, 200, 200]]),
-            torch.tensor([[100, 100, 200, 200]]),
-            torch.tensor([1.0]),
-        )
-    ],
-)
+@pytest.mark.parametrize("preds, target, expected_giou", [
+    (torch.tensor([[100, 100, 200, 200]]), torch.tensor([[100, 100, 200, 200]]), torch.tensor([1.0]))
+])
 def test_complete_overlap(preds, target, expected_giou):
     torch.testing.assert_allclose(giou(preds, target), expected_giou)
 
 
-@pytest.mark.parametrize(
-    "preds, target, expected_giou",
-    [
-        (
-            torch.tensor([[100, 100, 200, 200]]),
-            torch.tensor([[100, 200, 200, 300]]),
-            torch.tensor([0.0]),
-        ),
-        (
-            torch.tensor([[100, 100, 200, 200]]),
-            torch.tensor([[200, 200, 300, 300]]),
-            torch.tensor([-0.5]),
-        ),
-    ],
-)
+@pytest.mark.parametrize("preds, target, expected_giou", [
+    (torch.tensor([[100, 100, 200, 200]]), torch.tensor([[100, 200, 200, 300]]), torch.tensor([0.0])),
+    (torch.tensor([[100, 100, 200, 200]]), torch.tensor([[200, 200, 300, 300]]), torch.tensor([-0.5])),
+])
 def test_no_overlap(preds, target, expected_giou):
     torch.testing.assert_allclose(giou(preds, target), expected_giou)
