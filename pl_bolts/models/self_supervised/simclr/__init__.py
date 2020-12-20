2192729bb5de1e0cf1928050da424b2a1b481120
Jirka Borovec <jirka.borovec@seznam.cz>
2020-12-20 23:36:58 +0100

    fix

--- pl_bolts/models/self_supervised/simclr/__init__.py
+++ pl_bolts/models/self_supervised/simclr/__init__.py
@@ -1,2 +1,4 @@
 from pl_bolts.models.self_supervised.simclr.transforms import (  # noqa: F401
-    SimCLREvalDataTransform, SimCLRTrainDataTransform)
+    SimCLREvalDataTransform,
+    SimCLRTrainDataTransform,
+)
