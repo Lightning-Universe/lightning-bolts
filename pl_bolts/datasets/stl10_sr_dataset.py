cf75ac64acce4c97a0298a15fd2e5dc716de9971
Christoph Clement <christoph.clement@tum.de>
2020-12-20 14:15:13 +0100

    Fix make docs

--- pl_bolts/datasets/stl10_sr_dataset.py
+++ pl_bolts/datasets/stl10_sr_dataset.py
@@ -13,7 +13,7 @@ if _TORCHVISION_AVAILABLE:
     from torchvision.datasets import STL10
 else:
     warn_missing_pkg("torchvision")  # pragma: no-cover
-
+    STL10 = None
 
 class STL10_SR(STL10):
     def __init__(self, root: str, *args, **kwargs) -> None:
