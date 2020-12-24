3b98e9da106b1aae786e1445c29e74bd525be1ee
Akihiro Nitta <nitta@akihironitta.com>
2020-12-24 19:20:54 +0900

    Remove from pl_bolts.callbacks.vision import *

--- pl_bolts/callbacks/__init__.py
+++ pl_bolts/callbacks/__init__.py
@@ -4,4 +4,5 @@ Collection of PyTorchLightning callbacks
 from pl_bolts.callbacks.data_monitor import ModuleDataMonitor, TrainingDataMonitor  # noqa: F401
 from pl_bolts.callbacks.printing import PrintTableMetricsCallback  # noqa: F401
 from pl_bolts.callbacks.variational import LatentDimInterpolator  # noqa: F401
-from pl_bolts.callbacks.vision import TensorboardGenerativeModelImageSampler  # noqa: F401
+from pl_bolts.callbacks.vision.confused_logit import ConfusedLogitCallback  # noqa: F401
+from pl_bolts.callbacks.vision.image_generation import TensorboardGenerativeModelImageSampler  # noqa: F401
