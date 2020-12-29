fbad4cf37bf0f2024cc0ce58ef1a3e7e5c54a423
Christoph Clement <christoph.clement@artorg.unibe.ch>
2020-12-29 17:19:11 +0100

    Change order of images

--- pl_bolts/callbacks/vision/sr_image_logger.py
+++ pl_bolts/callbacks/vision/sr_image_logger.py
@@ -8,7 +8,7 @@ from pl_bolts.utils.warnings import warn_missing_pkg
 if _TORCHVISION_AVAILABLE:
     from torchvision.utils import make_grid
 else:
-    warn_missing_pkg('torchvision')  # pragma: no-cover
+    warn_missing_pkg("torchvision")  # pragma: no-cover
 
 
 class SRImageLoggerCallback(Callback):
@@ -22,13 +22,13 @@ class SRImageLoggerCallback(Callback):
         if global_step % self.log_interval == 0:
             hr_image, lr_image = batch
             hr_image, lr_image = hr_image.to(pl_module.device), lr_image.to(pl_module.device)
-            lr_image_scaled = F.interpolate(lr_image, scale_factor=4)
             hr_fake = pl_module(lr_image)
+            lr_image = F.interpolate(lr_image, scale_factor=4)
 
-            hr_image_grid = make_grid(hr_image[: self.num_samples], nrow=1, normalize=True)
-            lr_image_grid = make_grid(lr_image_scaled[: self.num_samples], nrow=1, normalize=True)
+            lr_image_grid = make_grid(lr_image[: self.num_samples], nrow=1, normalize=True)
             hr_fake_grid = make_grid(hr_fake[: self.num_samples], nrow=1, normalize=True)
+            hr_image_grid = make_grid(hr_image[: self.num_samples], nrow=1, normalize=True)
 
-            grid = torch.cat((lr_image_grid, hr_image_grid, hr_fake_grid), -1)
+            grid = torch.cat((lr_image_grid, hr_fake_grid, hr_image_grid), -1)
             title = "sr_images"
             trainer.logger.experiment.add_image(title, grid, global_step=global_step)
