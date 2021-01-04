1a29d6b4b7331e70b50e3118ef53b3a40da4ed45
Christoph Clement <christoph.clement@tum.de>
2021-01-04 13:55:39 +0100

    Update args

--- pl_bolts/callbacks/vision/image_generation.py
+++ pl_bolts/callbacks/vision/image_generation.py
@@ -1,3 +1,5 @@
+from typing import Optional, Tuple
+
 import torch
 from pytorch_lightning import Callback, LightningModule, Trainer
 
@@ -6,7 +8,7 @@ from pl_bolts.utils.warnings import warn_missing_pkg
 try:
     import torchvision
 except ModuleNotFoundError:
-    warn_missing_pkg('torchvision')  # pragma: no-cover
+    warn_missing_pkg("torchvision")  # pragma: no-cover
 
 
 class TensorboardGenerativeModelImageSampler(Callback):
@@ -30,9 +32,24 @@ class TensorboardGenerativeModelImageSampler(Callback):
         trainer = Trainer(callbacks=[TensorboardGenerativeModelImageSampler()])
     """
 
-    def __init__(self, num_samples: int = 3) -> None:
+    def __init__(
+        self,
+        num_samples: int = 3,
+        nrow: int = 8,
+        padding: int = 2,
+        normalize: bool = False,
+        range: Optional[Tuple[int, int]] = None,
+        scale_each: bool = False,
+        pad_value: int = 0,
+    ) -> None:
         super().__init__()
-        self.num_samples: int = num_samples
+        self.num_samples = num_samples
+        self.nrow = nrow
+        self.padding = padding
+        self.normalize = normalize
+        self.range = range
+        self.scale_each = scale_each
+        self.pad_value = pad_value
 
     def on_epoch_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
         dim = (self.num_samples, pl_module.hparams.latent_dim)  # type: ignore[union-attr]
@@ -48,6 +65,16 @@ class TensorboardGenerativeModelImageSampler(Callback):
             img_dim = pl_module.img_dim
             images = images.view(self.num_samples, *img_dim)
 
-        grid = torchvision.utils.make_grid(images)
-        str_title = f'{pl_module.__class__.__name__}_images'
-        trainer.logger.experiment.add_image(str_title, grid, global_step=trainer.global_step)
+        grid = torchvision.utils.make_grid(
+            tensor=images,
+            nrow=self.nrow,
+            padding=self.padding,
+            normalize=self.normalize,
+            range=self.range,
+            scale_each=self.scale_each,
+            pad_value=self.pad_value,
+        )
+        str_title = f"{pl_module.__class__.__name__}_images"
+        trainer.logger.experiment.add_image(
+            str_title, grid, global_step=trainer.global_step
+        )
