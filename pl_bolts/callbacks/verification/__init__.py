4ff963032c9725c22656b7f4e02e89f0ddc5bd26
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-01-09 23:21:56 +0100

    stop complain

--- pl_bolts/callbacks/verification/__init__.py
+++ pl_bolts/callbacks/verification/__init__.py
@@ -1,1 +1,2 @@
-from pl_bolts.callbacks.verification.batch_gradient import BatchGradientVerification, BatchGradientVerificationCallback
+from pl_bolts.callbacks.verification.batch_gradient import BatchGradientVerification  # noqa: F401
+from pl_bolts.callbacks.verification.batch_gradient import BatchGradientVerificationCallback  # noqa: F401
