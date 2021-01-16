b6402e71d5853b8282bc6fc1610ee26bdf5367ec
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-01-16 02:10:56 +0100

    stupid mypy

--- pl_bolts/callbacks/__init__.py
+++ pl_bolts/callbacks/__init__.py
@@ -6,7 +6,7 @@ from pl_bolts.callbacks.data_monitor import ModuleDataMonitor, TrainingDataMonit
 from pl_bolts.callbacks.printing import PrintTableMetricsCallback  # noqa: F401
 from pl_bolts.callbacks.ssl_online import SSLOnlineEvaluator  # noqa: F401
 from pl_bolts.callbacks.variational import LatentDimInterpolator  # noqa: F401
-from pl_bolts.callbacks.verification.batch_gradient import BatchGradientVerificationCallback  # noqa: F401
+from pl_bolts.callbacks.verification.batch_gradient import BatchGradientVerificationCallback  # type: ignore
 from pl_bolts.callbacks.vision.confused_logit import ConfusedLogitCallback  # noqa: F401
 from pl_bolts.callbacks.vision.image_generation import TensorboardGenerativeModelImageSampler  # noqa: F401
 
--- pl_bolts/utils/__init__.py
+++ pl_bolts/utils/__init__.py
@@ -1,7 +1,7 @@
 import torch
 from pytorch_lightning.utilities import _module_available
 
-from pl_bolts.callbacks.verification.batch_gradient import BatchGradientVerification
+from pl_bolts.callbacks.verification.batch_gradient import BatchGradientVerification  # type: ignore
 
 _NATIVE_AMP_AVAILABLE: bool = _module_available("torch.cuda.amp") and hasattr(torch.cuda.amp, "autocast")
 
