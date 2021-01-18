07186e9ebeca861f4cef59a7f1ef0afdf0fa537c
Aditya Oke <okeaditya315@gmail.com>
2021-01-18 12:29:37 +0530

    refactor

--- pl_bolts/models/detection/__init__.py
+++ pl_bolts/models/detection/__init__.py
@@ -1,5 +1,5 @@
 try:
-    from pl_bolts.models.detection import components  # noqa
-    from pl_bolts.models.detection.faster_rcnn import FasterRCNN  # noqa
+    from pl_bolts.models.detection import components  # noqa: F401
+    from pl_bolts.models.detection.faster_rcnn import FasterRCNN  # noqa: F401
 except ModuleNotFoundError:  # pragma: no-cover
     pass  # pragma: no-cover
