6843861a532ffad9250c3a98bb6f4d2823bfc905
Jirka Borovec <jirka.borovec@seznam.cz>
2021-01-18 01:03:34 +0100

    num_workers

--- tests/models/test_gans.py
+++ tests/models/test_gans.py
@@ -7,13 +7,15 @@ from pl_bolts.models.gans import GAN
 
 
 @pytest.mark.parametrize(
-    "dm_cls", [pytest.param(MNISTDataModule, id="mnist"),
-               pytest.param(CIFAR10DataModule, id="cifar10")]
+    "dm_cls", [
+        pytest.param(MNISTDataModule, id="mnist"),
+        pytest.param(CIFAR10DataModule, id="cifar10"),
+    ]
 )
 def test_gan(tmpdir, datadir, dm_cls):
     seed_everything()
 
-    dm = dm_cls(data_dir=datadir)
+    dm = dm_cls(data_dir=datadir, num_workers=0)
     model = GAN(*dm.size())
     trainer = pl.Trainer(fast_dev_run=True, default_root_dir=tmpdir)
     trainer.fit(model, dm)
