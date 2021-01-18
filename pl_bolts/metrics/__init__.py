d818522d4c1a5429b817a36fa00a9340d565fab2
Akihiro Nitta <nitta@akihironitta.com>
2021-01-18 14:09:44 +0900

    isort

--- pl_bolts/metrics/__init__.py
+++ pl_bolts/metrics/__init__.py
@@ -1,6 +1,5 @@
 from pl_bolts.metrics.aggregation import accuracy, mean, precision_at_k  # noqa: F401
 
-
 __all__ = [
     "accuracy",
     "mean",
--- pl_bolts/optimizers/__init__.py
+++ pl_bolts/optimizers/__init__.py
@@ -1,7 +1,6 @@
 from pl_bolts.optimizers.lars_scheduling import LARSWrapper  # noqa: F401
 from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR  # noqa: F401
 
-
 __all__ = [
     "LARSWrapper",
     "LinearWarmupCosineAnnealingLR",
