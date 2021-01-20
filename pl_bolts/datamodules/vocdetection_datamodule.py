8b2e1964af51c771af83002e58539d970646d3e1
Akihiro Nitta <nitta@akihironitta.com>
2021-01-20 20:27:35 +0900

    isort

--- pl_bolts/datamodules/vocdetection_datamodule.py
+++ pl_bolts/datamodules/vocdetection_datamodule.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Callable, Tuple, Optional, Union
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 import torch
 from pytorch_lightning import LightningDataModule
