f558a39362190c28c6ed1db933853769d02b8f63
Aditya Oke <okeaditya315@gmail.com>
2021-01-21 01:08:16 +0530

    sppedup test

--- tests/models/test_detection.py
+++ tests/models/test_detection.py
@@ -24,7 +24,9 @@ def test_fasterrcnn_train(tmpdir):
     train_dl = DataLoader(DummyDetectionDataset(), collate_fn=_collate_fn)
     valid_dl = DataLoader(DummyDetectionDataset(), collate_fn=_collate_fn)
 
-    trainer = pl.Trainer(fast_dev_run=True, default_root_dir=tmpdir)
+    trainer = pl.Trainer(
+        fast_dev_run=True, logger=False, checkpoint_callback=False, max_epochs=1, default_root_dir=tmpdir
+    )
     trainer.fit(model, train_dataloader=train_dl, val_dataloaders=valid_dl)
 
 
@@ -33,10 +35,13 @@ def test_fasterrcnn_bbone_train(tmpdir):
     train_dl = DataLoader(DummyDetectionDataset(), collate_fn=_collate_fn)
     valid_dl = DataLoader(DummyDetectionDataset(), collate_fn=_collate_fn)
 
-    trainer = pl.Trainer(fast_dev_run=True, default_root_dir=tmpdir)
+    trainer = pl.Trainer(
+        fast_dev_run=True, logger=False, checkpoint_callback=False, max_epochs=1, default_root_dir=tmpdir
+    )
     trainer.fit(model, train_dl, valid_dl)
 
 
+@torch.no_grad()
 def test_retinanet():
     model = RetinaNet(pretrained=False)
 
@@ -50,7 +55,9 @@ def test_retinanet_train(tmpdir):
     train_dl = DataLoader(DummyDetectionDataset(), collate_fn=_collate_fn)
     valid_dl = DataLoader(DummyDetectionDataset(), collate_fn=_collate_fn)
 
-    trainer = pl.Trainer(fast_dev_run=True, default_root_dir=tmpdir)
+    trainer = pl.Trainer(
+        fast_dev_run=True, logger=False, checkpoint_callback=False, max_epochs=1, default_root_dir=tmpdir
+    )
     trainer.fit(model, train_dataloader=train_dl, val_dataloaders=valid_dl)
 
 
@@ -59,5 +66,7 @@ def test_retinanet_bbone_train(tmpdir):
     train_dl = DataLoader(DummyDetectionDataset(), collate_fn=_collate_fn)
     valid_dl = DataLoader(DummyDetectionDataset(), collate_fn=_collate_fn)
 
-    trainer = pl.Trainer(fast_dev_run=True, default_root_dir=tmpdir)
+    trainer = pl.Trainer(
+        fast_dev_run=True, logger=False, checkpoint_callback=False, max_epochs=1, default_root_dir=tmpdir
+    )
     trainer.fit(model, train_dl, valid_dl)
