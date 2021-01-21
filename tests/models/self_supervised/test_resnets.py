d4bc28745f38cb1528e9a7e974a8a8c2960e7203
Aditya Oke <okeaditya315@gmail.com>
2021-01-22 00:48:17 +0530

    fix failing test

--- tests/models/self_supervised/test_resnets.py
+++ tests/models/self_supervised/test_resnets.py
@@ -25,21 +25,21 @@ def test_cpc_resnet():
 
 @pytest.mark.parametrize(
     "model_class", [
-        resnet18(),
-        resnet34(),
-        resnet50(),
-        resnet101(),
-        resnet152(),
-        resnext50_32x4d(),
-        resnext101_32x8d(),
-        wide_resnet50_2(),
-        wide_resnet101_2(),
+        resnet18,
+        resnet34,
+        resnet50,
+        resnet101,
+        resnet152,
+        resnext50_32x4d,
+        resnext101_32x8d,
+        wide_resnet50_2,
+        wide_resnet101_2,
     ]
 )
 @torch.no_grad()
 def test_torchvision_resnets(model_class):
     x = torch.rand(3, 3, 64, 64)
-    model = model_class()
+    model = model_class(pretrained=False)
     model(x)
 
 
