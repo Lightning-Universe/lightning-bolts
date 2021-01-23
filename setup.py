73fd15b832032e8dcdcf83c1bf8a69a146fbaa3e
Jirka Borovec <jirka.borovec@seznam.cz>
2021-01-23 01:14:16 +0100

    wheel

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     zip_safe=False,
     keywords=['deep learning', 'pytorch', 'AI'],
     python_requires='>=3.6',
-    setup_requires=['pytorch-lightning>=1.1.0'],
+    setup_requires=['wheel'],
     install_requires=_load_requirements(_PATH_ROOT),
     extras_require=_prepare_extras(),
     project_urls={
@@ -76,8 +76,9 @@ setup(
         'Operating System :: OS Independent',
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3'
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.8'
     ],
 )
