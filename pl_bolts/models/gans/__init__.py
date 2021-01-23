635900cc6001a15040c16d2ea11dc0ae7feb83bd
Aniket Maurya <aniket.maurya@gdn-commerce.com>
2021-01-23 19:29:20 +0530

    yapf consistency

--- pl_bolts/models/gans/__init__.py
+++ pl_bolts/models/gans/__init__.py
@@ -1,9 +1,9 @@
-from pl_bolts.models.gans.basic.basic_gan_module import GAN  # noqa: F401
-from pl_bolts.models.gans.dcgan.dcgan_module import DCGAN  # noqa: F401
+from pl_bolts.models.gans.basic.basic_gan_module import GAN
+from pl_bolts.models.gans.dcgan.dcgan_module import DCGAN
 from pl_bolts.models.gans.pix2pix.pix2pix_module import Pix2Pix
 
 __all__ = [
     "GAN",
     "DCGAN",
-    "Pix2Pix"
+    "Pix2Pix",
 ]
