9c505794b89615e5b23690458da2985c00d71ef7
Jirka Borovec <Borda@users.noreply.github.com>
2021-01-24 16:07:47 +0100

    Apply suggestions from code review
    Co-authored-by: Akihiro Nitta <nitta@akihironitta.com>

--- pl_bolts/models/self_supervised/simclr/simclr_module.py
+++ pl_bolts/models/self_supervised/simclr/simclr_module.py
@@ -38,8 +38,9 @@ class SyncFunction(torch.autograd.Function):
         grad_input = grad_output.clone()
         torch.distributed.all_reduce(grad_input, op=torch.distributed.ReduceOp.SUM, async_op=False)
 
-        return grad_input[torch.distributed.get_rank() * ctx.batch_size:(torch.distributed.get_rank() + 1)
-                          * ctx.batch_size]
+        idx_from = torch.distributed.get_rank() * ctx.batch_size
+        idx_to = (torch.distributed.get_rank() + 1) * ctx.batch_size
+        return grad_input[idx_from:idx_to]
 
 
 class Projection(nn.Module):
