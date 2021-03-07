823c0414266e342ccc76d900d34c0d94060b95b4
Kaushik Bokka <kaushikbokka@gmail.com>
2021-03-07 14:41:50 +0530

    add user agent

--- pl_bolts/models/mnist_module.py
+++ pl_bolts/models/mnist_module.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from six.moves import urllib
 
 import torch
 from pytorch_lightning import LightningModule, Trainer
@@ -13,6 +14,11 @@ if _TORCHVISION_AVAILABLE:
     from torchvision.datasets import MNIST
 else:  # pragma: no cover
     warn_missing_pkg('torchvision')
+
+# TorchVision hotfix https://github.com/pytorch/vision/issues/1938
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+urllib.request.install_opener(opener)
 
 
 class LitMNIST(LightningModule):
