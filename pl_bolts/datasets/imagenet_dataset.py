4d194d44f4bd97c2ed568bf07b2ef51ea7ec6903
Kaushik B <45285388+kaushikb11@users.noreply.github.com>
2021-03-09 15:55:59 +0530

    Fix PT 1.8 compatibility issue (#580)
    * fix torch._six PT 1.8 comp issue
    
    * update torch vision
    
    * update torchvision version
    
    * revert back torch versions
    
    * add user agent
    
    * fix expected od loss tensor shape
    
    * fix unused imports
    
    * move
    
    * format
    
    Co-authored-by: Jirka Borovec <jirka.borovec@seznam.cz>

--- pl_bolts/datasets/imagenet_dataset.py
+++ pl_bolts/datasets/imagenet_dataset.py
@@ -3,6 +3,7 @@ import hashlib
 import os
 import sys
 import shutil
+import sys  # noqa F401
 import tarfile
 import tempfile
 import zipfile
@@ -247,6 +248,8 @@ def parse_devkit_archive(root, file=None):
 def extract_archive(from_path, to_path=None, remove_finished=False):
     if to_path is None:
         to_path = os.path.dirname(from_path)
+
+    PY3 = sys.version_info[0] == 3
 
     if _is_tar(from_path):
         with tarfile.open(from_path, 'r') as tar:
