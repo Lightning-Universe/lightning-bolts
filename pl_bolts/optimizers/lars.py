72d92f4e0507bac9e2b460ab34c6ea2ff2a10f7f
ananyahjha93 <ananya@pytorchlightning.ai>
2021-04-07 04:39:59 -0400

    update docs ex

--- pl_bolts/optimizers/lars.py
+++ pl_bolts/optimizers/lars.py
@@ -36,12 +36,14 @@ class LARS(Optimizer):
         The application of momentum in the SGD part is modified according to
         the PyTorch standards. LARS scaling fits into the equation in the
         following fashion.
+
         .. math::
             \begin{aligned}
                 g_{t+1} & = \text{lars_lr} * (\beta * p_{t} + g_{t+1}), \\
                 v_{t+1} & = \mu * v_{t} + g_{t+1}, \\
                 p_{t+1} & = p_{t} - \text{lr} * v_{t+1},
             \end{aligned}
+
         where :math:`p`, :math:`g`, :math:`v`, :math:`\mu` and :math:`\beta` denote the
         parameters, gradient, velocity, momentum, and weight decay respectively.
         The :math:`lars_lr` is defined by Eq. 6 in the paper.
