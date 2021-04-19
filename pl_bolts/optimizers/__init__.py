74f9c543ec8d0214e77e3b8d378051750b173ce0
Akihiro Nitta <nitta@akihironitta.com>
2021-04-19 13:31:59 +0900

    isort

--- pl_bolts/optimizers/__init__.py
+++ pl_bolts/optimizers/__init__.py
@@ -1,6 +1,5 @@
 from pl_bolts.optimizers.lars import LARS
-from pl_bolts.optimizers.lr_scheduler import linear_warmup_decay
-from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
+from pl_bolts.optimizers.lr_scheduler import linear_warmup_decay, LinearWarmupCosineAnnealingLR
 
 __all__ = [
     "LARS",
