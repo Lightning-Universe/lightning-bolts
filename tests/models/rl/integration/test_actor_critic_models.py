8c2145febb0a2a9d315ca7bba3bfa01b6728efc2
Jason Wang <blahblahj.wsy@gmail.com>
2021-04-28 17:02:40 -0700

    added tests

---
+++ tests/models/rl/integration/test_actor_critic_models.py
@@ -0,0 +1,36 @@
+import argparse
+
+import pytorch_lightning as pl
+
+from pl_bolts.models.rl.sac_model import SAC
+
+
+def test_sac():
+    """Smoke test that the SAC model runs"""
+
+    parent_parser = argparse.ArgumentParser(add_help=False)
+    parent_parser = pl.Trainer.add_argparse_args(parent_parser)
+    parent_parser = SAC.add_model_specific_args(parent_parser)
+    args_list = [
+        "--warm_start_size",
+        "100",
+        "--gpus",
+        "0",
+        "--env",
+        "Pendulum-v0",
+        "--batch_size",
+        "10",
+    ]
+    hparams = parent_parser.parse_args(args_list)
+
+    trainer = pl.Trainer(
+        gpus=hparams.gpus,
+        max_steps=100,
+        max_epochs=100,  # Set this as the same as max steps to ensure that it doesn't stop early
+        val_check_interval=1,  # This just needs 'some' value, does not effect training right now
+        fast_dev_run=True
+    )
+    model = SAC(**hparams.__dict__)
+    result = trainer.fit(model)
+
+    assert result == 1
