8beba4bed273df1fea0488338bb8140522f660ef
Akihiro Nitta <nitta@akihironitta.com>
2021-08-10 17:26:13 +0900

    Instantiate transforms only once

--- pl_bolts/datasets/sr_dataset_mixin.py
+++ pl_bolts/datasets/sr_dataset_mixin.py
@@ -26,6 +26,22 @@ class SRDatasetMixin:
     Scales range of high resolution images to [-1, 1] and range or low resolution images to [0, 1].
     """
 
+    def __init__(self) -> None:
+
+        self.hr_transforms = transform_lib.Compose([
+            transform_lib.RandomCrop(self.hr_image_size),
+            transform_lib.ToTensor(),
+            transform_lib.Normalize(mean=(0.5, ) * self.image_channels, std=(0.5, ) * self.image_channels),
+        ])
+
+        # Scale range of LR images to [0, 1]
+        self.lr_transforms = transform_lib.Compose([
+            transform_lib.Normalize(mean=(-1.0, ) * self.image_channels, std=(2.0, ) * self.image_channels),
+            transform_lib.ToPILImage(),
+            transform_lib.Resize(self.lr_image_size, Image.BICUBIC),
+            transform_lib.ToTensor(),
+        ])
+
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor]:
         image = self._get_image(index)
 
@@ -33,21 +49,3 @@ class SRDatasetMixin:
         lr_image = self.lr_transforms(hr_image)
 
         return hr_image, lr_image
-
-    @property
-    def hr_transforms(self):
-        return transform_lib.Compose([
-            transform_lib.RandomCrop(self.hr_image_size),
-            transform_lib.ToTensor(),
-            transform_lib.Normalize(mean=(0.5, ) * self.image_channels, std=(0.5, ) * self.image_channels),
-        ])
-
-    @property
-    def lr_transforms(self):
-        # Scale range of LR images to [0, 1]
-        return transform_lib.Compose([
-            transform_lib.Normalize(mean=(-1.0, ) * self.image_channels, std=(2.0, ) * self.image_channels),
-            transform_lib.ToPILImage(),
-            transform_lib.Resize(self.lr_image_size, Image.BICUBIC),
-            transform_lib.ToTensor(),
-        ])
