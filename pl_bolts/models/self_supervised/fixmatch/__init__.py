c92e547fa8d22fc7adab94d03f7b6b50f52917bf
Zehua Cheng <chengzehua@outlook.com>
2021-09-14 19:36:29 +0800

    Add init for PEP 8.

--- pl_bolts/models/self_supervised/fixmatch/__init__.py
+++ pl_bolts/models/self_supervised/fixmatch/__init__.py
@@ -1,4 +1,14 @@
+from .comatch_module import CoMatch
 from .datasets import SSLDataModule, TransformSSL
 from .fixmatch_module import FixMatch
 from .networks import WideResnet
 from .transforms import RandAugmentMC
+
+__all__ = [
+    "TransformSSL",
+    "SSLDataModule",
+    "FixMatch",
+    "CoMatch",
+    "WideResnet",
+    "RandAugmentMC"
+]
