2a7ba588e117ede2c469ba2818b644735a45a11c
Zehua Cheng <chengzehua@outlook.com>
2021-09-14 19:37:12 +0800

    Add lr scheduler.

---
+++ pl_bolts/models/self_supervised/fixmatch/lr_scheduler.py
@@ -0,0 +1,47 @@
+import numpy as np
+from torch.optim.lr_scheduler import _LRScheduler
+
+
+class WarmupCosineLrScheduler(_LRScheduler):
+    '''
+    reference https://github.com/salesforce/CoMatch/blob/885f1283beec62cba3470f58a0cd978fe180d5cf/utils.py#L87
+    '''
+
+    def __init__(
+            self,
+            optimizer,
+            max_iter,
+            warmup_iter,
+            warmup_ratio=5e-4,
+            warmup='exp',
+            last_epoch=-1,
+    ):
+        self.max_iter = max_iter
+        self.warmup_iter = warmup_iter
+        self.warmup_ratio = warmup_ratio
+        self.warmup = warmup
+        super(WarmupCosineLrScheduler, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        ratio = self.get_lr_ratio()
+        lrs = [ratio * lr for lr in self.base_lrs]
+        return lrs
+
+    def get_lr_ratio(self):
+        if self.last_epoch < self.warmup_iter:
+            ratio = self.get_warmup_ratio()
+        else:
+            real_iter = self.last_epoch - self.warmup_iter
+            real_max_iter = self.max_iter - self.warmup_iter
+            ratio = np.cos((7 * np.pi * real_iter) / (16 * real_max_iter))
+            # ratio = 0.5 * (1. + np.cos(np.pi * real_iter / real_max_iter))
+        return ratio
+
+    def get_warmup_ratio(self):
+        assert self.warmup in ('linear', 'exp')
+        alpha = self.last_epoch / self.warmup_iter
+        if self.warmup == 'linear':
+            ratio = self.warmup_ratio + (1 - self.warmup_ratio) * alpha
+        elif self.warmup == 'exp':
+            ratio = self.warmup_ratio ** (1. - alpha)
+        return ratio
