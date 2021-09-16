37b3e917253c3731f4da5e617fc44705a4c64bf7
Zehua Cheng <chengzehua@outlook.com>
2021-09-17 02:57:32 +0800

    Add CLI into __init__.py

--- pl_bolts/models/self_supervised/fixmatch/__init__.py
+++ pl_bolts/models/self_supervised/fixmatch/__init__.py
@@ -1,7 +1,8 @@
-from .comatch_module import CoMatch
+from .comatch_module import CoMatch, CoMatchCLI
 from .datasets import SSLDataModule, TransformSSL
-from .fixmatch_module import FixMatch
+from .fixmatch_module import FixMatch, FixMatchCLI
 from .networks import WideResnet
 from .transforms import RandAugmentMC
 
-__all__ = ["TransformSSL", "SSLDataModule", "FixMatch", "CoMatch", "WideResnet", "RandAugmentMC"]
+__all__ = ["TransformSSL", "SSLDataModule", "FixMatch", "CoMatch", "FixMatchCLI", "CoMatchCLI", "WideResnet",
+           "RandAugmentMC"]
