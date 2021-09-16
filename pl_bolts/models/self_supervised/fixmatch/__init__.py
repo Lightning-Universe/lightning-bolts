9f69517af57b46c3950843a5a18173c4a2387ca5
limberc <zehua.cheng@cs.ox.ac.uk>
2021-09-17 04:07:26 +0800

    Optimize import in __init__.py

--- pl_bolts/models/self_supervised/fixmatch/__init__.py
+++ pl_bolts/models/self_supervised/fixmatch/__init__.py
@@ -1,8 +1,8 @@
-from .comatch_module import CoMatch, CoMatchCLI
-from .datasets import SSLDataModule, TransformSSL
-from .fixmatch_module import FixMatch, FixMatchCLI
-from .networks import WideResnet, ema_model_update, get_ema_model
-from .transforms import RandAugmentMC
+from pl_bolts.models.self_supervised.fixmatch.comatch_module import CoMatch, CoMatchCLI
+from pl_bolts.models.self_supervised.fixmatch.datasets import SSLDataModule, TransformSSL
+from pl_bolts.models.self_supervised.fixmatch.fixmatch_module import FixMatch, FixMatchCLI
+from pl_bolts.models.self_supervised.fixmatch.networks import WideResnet, ema_model_update, get_ema_model
+from pl_bolts.models.self_supervised.fixmatch.transforms import RandAugmentMC
 
 __all__ = [
     "TransformSSL",
