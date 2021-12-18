3dbf3e880d33f20a2cb2b358bb1ba2028d094c96
Akihiro Nitta <nitta@akihironitta.com>
2021-12-19 01:46:31 +0900

    Revert "Try replace iou with torchvision's"
    This reverts commit a5a01affb8f668f93d73e9afaa19511fbd7b88e6.

--- pl_bolts/metrics/object_detection.py
+++ pl_bolts/metrics/object_detection.py
@@ -1,14 +1,6 @@
 import torch
 from torch import Tensor
 
-from pl_bolts.utils import _TORCHVISION_AVAILABLE
-from pl_bolts.utils.warnings import warn_missing_pkg
-
-if _TORCHVISION_AVAILABLE:
-    from torchvision.ops import box_iou as iou
-else:  # pragma: no cover
-    warn_missing_pkg("torchvision")
-
 
 def _evaluate_iou(preds: torch.Tensor, target: torch.Tensor):
     """Evaluate intersection over union (IOU) for target from dataset and output prediction from model."""
@@ -17,6 +9,38 @@ def _evaluate_iou(preds: torch.Tensor, target: torch.Tensor):
         # no box detected, 0 IOU
         return torch.tensor(0.0, device=preds["boxes"].device)
     return iou(target["boxes"], preds["boxes"]).diag().mean()
+
+
+def iou(preds: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    """Calculates the intersection over union.
+
+    Args:
+        preds: an Nx4 batch of prediction bounding boxes with representation ``[x_min, y_min, x_max, y_max]``
+        target: an Mx4 batch of target bounding boxes with representation ``[x_min, y_min, x_max, y_max]``
+
+    Example:
+
+        >>> import torch
+        >>> from pl_bolts.metrics.object_detection import iou
+        >>> preds = torch.tensor([[100, 100, 200, 200]])
+        >>> target = torch.tensor([[150, 150, 250, 250]])
+        >>> iou(preds, target)
+        tensor([[0.1429]])
+
+    Returns:
+        IoU tensor: an NxM tensor containing the pairwise IoU values for every element in preds and target,
+                    where N is the number of prediction bounding boxes and M is the number of target bounding boxes
+    """
+    x_min = torch.max(preds[:, None, 0], target[:, 0])
+    y_min = torch.max(preds[:, None, 1], target[:, 1])
+    x_max = torch.min(preds[:, None, 2], target[:, 2])
+    y_max = torch.min(preds[:, None, 3], target[:, 3])
+    intersection = (x_max - x_min).clamp(min=0) * (y_max - y_min).clamp(min=0)
+    pred_area = (preds[:, 2] - preds[:, 0]) * (preds[:, 3] - preds[:, 1])
+    target_area = (target[:, 2] - target[:, 0]) * (target[:, 3] - target[:, 1])
+    union = pred_area[:, None] + target_area - intersection
+    iou = torch.true_divide(intersection, union)
+    return iou
 
 
 def giou(preds: Tensor, target: Tensor) -> Tensor:
