ed79219ff9334a9a809ef8bf9b6fe79004bc0ea1
Jirka <jirka.borovec@seznam.cz>
2021-12-19 12:07:45 +0100

    tags

--- setup.py
+++ setup.py
@@ -82,8 +82,9 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
