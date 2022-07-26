10886c3127fad29d6963c086f60457d892f56f3e
Shivam Mehta <shivam.mehta007@gmail.com>
2022-07-26 22:13:44 +0200

    Adding discriminator unit test

--- tests/models/gans/unit/test_basic_components.py
+++ tests/models/gans/unit/test_basic_components.py
@@ -9,10 +9,10 @@ from pl_bolts.models.gans.basic.components import Discriminator, Generator
     "latent_dim, img_shape",
     [
         pytest.param(
-            100, (3, 28, 28), id="100-multichannel"
+            100, (3, 28, 28), id="generator-100-multichannel"
         ),
         pytest.param(
-            100, (1, 28, 28), id="100-singlechannel"
+            100, (1, 28, 28), id="generator-100-singlechannel"
         ),
     ],
 )
@@ -23,3 +23,24 @@ def test_generator(latent_dim, img_shape):
     noise = torch.randn(batch_dim, latent_dim)
     samples = generator(noise)
     assert samples.shape == (batch_dim, *img_shape)
+
+
+@pytest.mark.parametrize(
+    "img_shape",
+    [
+        pytest.param(
+            (3, 28, 28), id="discriminator-multichannel"
+        ),
+        pytest.param(
+            (1, 28, 28), id="discriminator-singlechannel"
+        ),
+    ],
+)
+def test_discriminator(img_shape):
+    batch_dim = 10
+    seed_everything()
+    discriminator = Discriminator(img_shape=img_shape)
+    samples = torch.randn(batch_dim, *img_shape)
+    real_or_fake = discriminator(samples)
+    assert real_or_fake.shape == (batch_dim, 1)
+    assert (torch.clamp(real_or_fake.clone(), 0, 1) == real_or_fake).all()
