aa3425855d5011c661f344f77f59d2c2527b4aa1
Cellan Hall <hallcellan@gmail.com>
2022-08-02 23:25:16 +0100

    utilising apply_func.apply_to_collections

--- pl_bolts/datasets/array_dataset.py
+++ pl_bolts/datasets/array_dataset.py
@@ -1,51 +1,39 @@
-from typing import Tuple
+from typing import List, Tuple, Union
 
+import numpy as np
 import torch
-from pytorch_lightning.utilities import exceptions
+from pytorch_lightning.utilities import apply_func, exceptions
 from torch import Tensor
 from torch.utils.data import Dataset
+
+ARRAYS = Union[torch.Tensor, np.ndarray, List[Union[float, int]]]
 
 
 class ArrayDataset(Dataset):
     """Dataset wrapping tensors, lists, numpy arrays.
 
     Args:
-        data: feature variables.
-        target: target variables.
+        arrays: Arrays that have the same size in the first dimension.
+
+    Attributes:
+        tensors: Input arrays transformed into tensors.
 
     Raises:
         MisconfigurationException: if there is a shape mismatch between arrays in the first dimension.
-
-    Example:
-        >>> from sklearn.datasets import load_diabetes
-        >>> from pl_bolts.datasets import ArrayDataset
-        ...
-        >>> X, y = load_diabetes(return_X_y=True)
-        >>> dataset = ArrayDataset(X, y)
-        >>> len(dataset)
-        442
     """
 
-    def __init__(self, data, target) -> None:
-        self.data = data
-        self.target = target
+    def __init__(self, *arrays: Tuple[ARRAYS]) -> None:
+        self.tensors = apply_func.apply_to_collection(arrays, dtype=(np.ndarray, list), function=torch.tensor)
 
-        if not torch.is_tensor(self.data):
-            self.data = torch.tensor(data)
-
-        if not torch.is_tensor(target):
-            self.target = torch.tensor(target)
-
-        if not self.data.size(0) == self.target.size(0):
+        if not self._equal_size():
             raise exceptions.MisconfigurationException("Shape mismatch between arrays in the first dimension")
 
     def __len__(self) -> int:
-        return self.target.size(0)
+        return self.tensors[0].size(0)
 
-    def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor]:
-        x = self.data[idx].float()
-        y = self.target[idx]
+    def __getitem__(self, idx: int) -> Tuple[Tensor, ...]:
+        return tuple(tensor[idx] for tensor in self.tensors)
 
-        if not ((y.dtype == torch.int32) or (y.dtype == torch.int64)):
-            y = y.float()
-        return x, y
+    def _equal_size(self):
+        """Check the size of the tensors are equal in the first dimension."""
+        return all(tensor.size(0) == self.tensors[0].size(0) for tensor in self.tensors)
