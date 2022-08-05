68d839c36b82b4d4778682944ac0d2441953a056
Arnol Fokam <arnolfokam23@gmail.com>
2022-08-05 02:02:31 +0100

    update GaussianBlur operation to torchvision implementation

--- pl_bolts/models/self_supervised/simclr/transforms.py
+++ pl_bolts/models/self_supervised/simclr/transforms.py
@@ -1,6 +1,4 @@
-import numpy as np
-
-from pl_bolts.utils import _OPENCV_AVAILABLE, _TORCHVISION_AVAILABLE
+from pl_bolts.utils import _TORCHVISION_AVAILABLE
 from pl_bolts.utils.stability import under_review
 from pl_bolts.utils.warnings import warn_missing_pkg
 
@@ -8,11 +6,6 @@ if _TORCHVISION_AVAILABLE:
     from torchvision import transforms
 else:  # pragma: no cover
     warn_missing_pkg("torchvision")
-
-if _OPENCV_AVAILABLE:
-    import cv2
-else:  # pragma: no cover
-    warn_missing_pkg("cv2", pypi_name="opencv-python")
 
 
 @under_review()
@@ -25,7 +18,7 @@ class SimCLRTrainDataTransform:
         RandomHorizontalFlip()
         RandomApply([color_jitter], p=0.8)
         RandomGrayscale(p=0.2)
-        GaussianBlur(kernel_size=int(0.1 * self.input_height))
+        RandomApply([GaussianBlur(kernel_size=int(0.1 * self.input_height))], p=0.5)
         transforms.ToTensor()
 
     Example::
@@ -68,7 +61,7 @@ class SimCLRTrainDataTransform:
             if kernel_size % 2 == 0:
                 kernel_size += 1
 
-            data_transforms.append(GaussianBlur(kernel_size=kernel_size, p=0.5))
+            data_transforms.append(transforms.RandomApply([transforms.GaussianBlur(kernel_size=kernel_size)], p=0.5))
 
         data_transforms = transforms.Compose(data_transforms)
 
@@ -169,30 +162,3 @@ class SimCLRFinetuneTransform:
 
     def __call__(self, sample):
         return self.transform(sample)
-
-
-@under_review()
-class GaussianBlur:
-    # Implements Gaussian blur as described in the SimCLR paper
-    def __init__(self, kernel_size, p=0.5, min=0.1, max=2.0):
-        if not _TORCHVISION_AVAILABLE:  # pragma: no cover
-            raise ModuleNotFoundError("You want to use `GaussianBlur` from `cv2` which is not installed yet.")
-
-        self.min = min
-        self.max = max
-
-        # kernel size is set to be 10% of the image height/width
-        self.kernel_size = kernel_size
-        self.p = p
-
-    def __call__(self, sample):
-        sample = np.array(sample)
-
-        # blur the image with a 50% chance
-        prob = np.random.random_sample()
-
-        if prob < self.p:
-            sigma = (self.max - self.min) * np.random.random_sample() + self.min
-            sample = cv2.GaussianBlur(sample, (self.kernel_size, self.kernel_size), sigma)
-
-        return sample
