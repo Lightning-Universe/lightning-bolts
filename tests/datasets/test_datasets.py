46f224fc91ee3e474376884132869c006108a45e
BaruchG <bgutow@gmail.com>
2022-08-08 17:02:38 -0400

    insert torchvision dependency and write tests for cifar10

--- tests/datasets/test_datasets.py
+++ tests/datasets/test_datasets.py
@@ -1,9 +1,12 @@
 import pytest
 import torch
 from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+
 
 from pl_bolts.datasets import DummyDataset, RandomDataset, RandomDictDataset, RandomDictStringDataset
 from pl_bolts.datasets.sr_mnist_dataset import SRMNIST
+from pl_bolts.datasets.cifar10_dataset import CIFAR10
 
 
 def test_dummy_ds():
@@ -52,3 +55,21 @@ def test_sr_datasets(datadir, scale_factor):
     assert torch.allclose(hr_image.max(), torch.tensor(1.0), atol=atol)
     assert torch.allclose(lr_image.min(), torch.tensor(0.0), atol=atol)
     assert torch.allclose(lr_image.max(), torch.tensor(1.0), atol=atol)
+
+def test_cifar10_datasets(datadir):
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    dl = DataLoader(CIFAR10(root=datadir, download=True, transform=transform))
+    hr_image, lr_image = next(iter(dl))
+    print("==============================", lr_image.size())
+
+    hr_image_size = 32
+    assert hr_image.size() == torch.Size([1, 3, hr_image_size, hr_image_size])
+    assert lr_image.size() == torch.Size([1])
+
+    atol = 0.3
+    assert torch.allclose(hr_image.min(), torch.tensor(-1.0), atol=atol)
+    assert torch.allclose(hr_image.max(), torch.tensor(1.0), atol=atol)
+    assert torch.greater_equal(lr_image.min(), torch.tensor(0))
+    assert torch.less_equal(lr_image.max(), torch.tensor(9))
