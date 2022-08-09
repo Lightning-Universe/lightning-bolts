4502518bac25182861eca7074e58e570bd7884c4
Cellan Hall <hallcellan@gmail.com>
2022-08-09 23:53:41 +0100

    created BoltsDataset

--- pl_bolts/datasets/array_dataset.py
+++ pl_bolts/datasets/array_dataset.py
@@ -1,18 +1,23 @@
-from typing import List, Tuple, Union
+from typing import Callable, List, Optional, Tuple, Union
 
 import numpy as np
 import torch
 from pytorch_lightning.utilities import apply_func, exceptions
-from torch.utils.data import Dataset
 
+from pl_bolts.datasets.base_dataset import BoltsDataset
+
+# TODO: Move this to a more appropriate place.
 ARRAYS = Union[torch.Tensor, np.ndarray, List[Union[float, int]]]
 
 
-class ArrayDataset(Dataset):
+class ArrayDataset(BoltsDataset):
     """Dataset wrapping tensors, lists, numpy arrays.
 
     Args:
-        arrays: Arrays that have the same size in the first dimension.
+        arrays: Sequence of indexables.
+        transforms: A callable that takes input data and target data and returns transformed versions of both.
+        input_transform: A callable that takes input data and returns a transformed version.
+        target_transform: A callable that takes target data and returns a transformed version.
 
     Attributes:
         tensors: Input arrays transformed into tensors.
@@ -21,8 +26,17 @@ class ArrayDataset(Dataset):
         MisconfigurationException: if there is a shape mismatch between arrays in the first dimension.
     """
 
-    def __init__(self, *arrays: Tuple[ARRAYS]) -> None:
-        self.tensors = apply_func.apply_to_collection(arrays, dtype=(np.ndarray, list), function=torch.tensor)
+    def __init__(
+        self,
+        *arrays: Tuple[ARRAYS],
+        transforms: Optional[Callable] = None,
+        input_transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+    ) -> None:
+        super().__init__(
+            *arrays, transforms=transforms, input_transform=input_transform, target_transform=target_transform
+        )
+        self.tensors = apply_func.apply_to_collection(self.arrays, dtype=(np.ndarray, list), function=torch.tensor)
 
         if not self._equal_size():
             raise exceptions.MisconfigurationException("Shape mismatch between arrays in the first dimension")
@@ -31,8 +45,17 @@ class ArrayDataset(Dataset):
         return self.tensors[0].size(0)
 
     def __getitem__(self, idx: int) -> Tuple[torch.Tensor, ...]:
-        return tuple(tensor[idx] for tensor in self.tensors)
+        data, target = self.tensors[idx][0], self.tensors[idx][1]
 
-    def _equal_size(self):
+        if self.transforms is not None:
+            return self.transforms(data, target)
+        elif self.input_transform is not None and self.target_transform is not None:
+            return self.input_transform(data), self.target_transform(target)
+        elif self.input_transform is not None:
+            return self.input_transform(data)
+        else:
+            return tuple(tensor[idx] for tensor in self.tensors)
+
+    def _equal_size(self) -> bool:
         """Check the size of the tensors are equal in the first dimension."""
         return all(tensor.size(0) == self.tensors[0].size(0) for tensor in self.tensors)
--- pl_bolts/datasets/base_dataset.py
+++ pl_bolts/datasets/base_dataset.py
@@ -2,18 +2,23 @@ import logging
 import os
 import urllib.request
 from abc import ABC
-from typing import Sequence, Tuple
+from typing import Callable, List, Optional, Sequence, Tuple, Union
 from urllib.error import HTTPError
 
+import numpy as np
+import torch
+from pytorch_lightning.utilities import exceptions
 from torch import Tensor
 from torch.utils.data import Dataset
 
 from pl_bolts.utils.stability import under_review
 
+# TODO: Move this to a more appropriate place.
+ARRAYS = Union[torch.Tensor, np.ndarray, List[Union[float, int]]]
+
 
 @under_review()
 class LightDataset(ABC, Dataset):
-
     data: Tensor
     targets: Tensor
     normalize: tuple
@@ -58,3 +63,53 @@ class LightDataset(ABC, Dataset):
             urllib.request.urlretrieve(url, fpath)
         except HTTPError as err:
             raise RuntimeError(f"Failed download from {url}") from err
+
+
+class BoltsDataset(Dataset):
+    """Base class for Bolts datasets.
+
+    Args:
+        arrays: Sequence of indexables.
+        transforms: A callable that takes input data and target data and returns transformed versions of both.
+        input_transform: A callable that takes input data and returns a transformed version.
+        target_transform: A callable that takes target data and returns a transformed version.
+
+    Raises:
+        MisconfigurationException: Only transforms or input_transform/target_transform can be passed as argument.
+        MisconfigurationException: input_transform and target_transform can only be applied to arrays length 2.
+        MisconfigurationException: target_transform can only be applied to arrays length 2.
+    """
+
+    def __init__(
+        self,
+        *arrays: Tuple[ARRAYS],
+        transforms: Optional[Callable] = None,
+        input_transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+    ) -> None:
+
+        has_transforms = transforms is not None
+        has_separate_transform = input_transform is not None or target_transform is not None
+        if has_transforms and has_separate_transform:
+            raise exceptions.MisconfigurationException(
+                "Only transforms or input_transform/target_transform can be passed as argument"
+            )
+
+        if input_transform and target_transform and len(arrays) != 2:
+            raise exceptions.MisconfigurationException(
+                "input_transform and target_transform can only be applied to arrays of length 2."
+            )
+
+        if target_transform and len(arrays) != 2:
+            raise exceptions.MisconfigurationException("target_transform can only be applied to arrays of length 2.")
+
+        self.arrays = arrays
+        self.transforms = transforms
+        self.input_transform = input_transform
+        self.target_transform = target_transform
+
+    def __getitem__(self, idx: int):
+        raise NotImplementedError
+
+    def __len__(self) -> int:
+        raise NotImplementedError
