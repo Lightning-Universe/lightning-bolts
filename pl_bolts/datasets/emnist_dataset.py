db6e02f2e544da76e0c04cf09630a3c02b2bb431
Shion <smatsumoto97@gmail.com>
2022-08-18 11:45:42 -0400

    Add threshold as argument. Add docs.

--- pl_bolts/datasets/emnist_dataset.py
+++ pl_bolts/datasets/emnist_dataset.py
@@ -1,4 +1,4 @@
-from typing import Any, Tuple
+from typing import Any, Tuple, Union
 
 from pl_bolts.utils import _PIL_AVAILABLE, _TORCHVISION_AVAILABLE
 from pl_bolts.utils.warnings import warn_missing_pkg
@@ -18,11 +18,34 @@ else:  # pragma: no cover
 class BinaryEMNIST(EMNIST):
     """Binarized EMNIST Dataset.
 
-    EMNIST dataset binarized using a thresholding operation. Threshold is set to 127. Note that the images are binarized
-    prior to the application of any transforms.
+    EMNIST dataset binarized using a thresholding operation. Default threshold value is 127.
+    Note that the images are binarized prior to the application of any transforms.
+
+    Args:
+        root (string): Root directory of dataset where ``EMNIST/raw/train-images-idx3-ubyte``
+            and  ``EMNIST/raw/t10k-images-idx3-ubyte`` exist.
+        split (string): The dataset has 6 different splits: ``byclass``, ``bymerge``,
+            ``balanced``, ``letters``, ``digits`` and ``mnist``. This argument specifies
+            which one to use.
+        threshold (Union[int, float], optional): Threshold value for binarizing image.
+            Pixel value is set to 255 if value is greater than threshold, otherwise 0.
+        train (bool, optional): If True, creates dataset from ``training.pt``,
+            otherwise from ``test.pt``.
+        download (bool, optional): If True, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+
+    Note:
+        Documentation is based on https://pytorch.org/vision/main/generated/torchvision.datasets.EMNIST.html
     """
 
-    threshold = 127.0
+    def __init__(self, root: str, split: str, threshold: Union[int, float] = 127.0, **kwargs) -> None:
+        super().__init__(root, split, **kwargs)
+        self.threshold = threshold
 
     def __getitem__(self, idx: int) -> Tuple[Any, Any]:
         """
--- pl_bolts/datasets/mnist_dataset.py
+++ pl_bolts/datasets/mnist_dataset.py
@@ -1,4 +1,4 @@
-from typing import Any, Tuple
+from typing import Any, Tuple, Union
 
 from pl_bolts.utils import _PIL_AVAILABLE, _TORCHVISION_AVAILABLE
 from pl_bolts.utils.warnings import warn_missing_pkg
@@ -18,11 +18,31 @@ else:  # pragma: no cover
 class BinaryMNIST(MNIST):
     """Binarized MNIST Dataset.
 
-    MNIST dataset binarized using a thresholding operation. Threshold is set to 127. Note that the images are binarized
-    prior to the application of any transforms.
+    MNIST dataset binarized using a thresholding operation. Default threshold value is 127.
+    Note that the images are binarized prior to the application of any transforms.
+
+    Args:
+        root (string): Root directory of dataset where ``MNIST/raw/train-images-idx3-ubyte``
+            and  ``MNIST/raw/t10k-images-idx3-ubyte`` exist.
+        threshold (Union[int, float], optional): Threshold value for binarizing image.
+            Pixel value is set to 255 if value is greater than threshold, otherwise 0.
+        train (bool, optional): If True, creates dataset from ``train-images-idx3-ubyte``,
+            otherwise from ``t10k-images-idx3-ubyte``.
+        download (bool, optional): If True, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+
+    Note:
+        Documentation is based on https://pytorch.org/vision/main/generated/torchvision.datasets.EMNIST.html
     """
 
-    threshold = 127.0
+    def __init__(self, root: str, threshold: Union[int, float] = 127.0, **kwargs) -> None:
+        super().__init__(root, **kwargs)
+        self.threshold = threshold
 
     def __getitem__(self, idx: int) -> Tuple[Any, Any]:
         """
