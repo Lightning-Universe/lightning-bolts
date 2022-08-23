accfd287bd17fec7683bcee9b58246a4e7288c50
Shion <smatsumoto97@gmail.com>
2022-08-23 14:18:12 -0400

    Add test for tau in between 0 and 1.

--- tests/callbacks/test_param_update_callbacks.py
+++ tests/callbacks/test_param_update_callbacks.py
@@ -8,17 +8,14 @@ from pl_bolts.callbacks.byol_updates import BYOLMAWeightUpdate
 
 
 @pytest.mark.parametrize("initial_tau", [-0.1, 0.0, 0.996, 1.0, 1.1])
-def test_byol_ma_weight_single_update_callback(initial_tau):
+def test_byol_ma_weight_single_update_callback(initial_tau, catch_warnings):
     """Check BYOL exponential moving average weight update rule for a single update."""
-    if not 0.0 <= initial_tau <= 1.0:
-        with pytest.raises(ValueError, match="initial tau should be"):
-            cb = BYOLMAWeightUpdate(initial_tau)
-    else:
+    if 0.0 <= initial_tau <= 1.0:
         # Create simple one layer network and their copies
         online_network = nn.Linear(100, 10)
         target_network = deepcopy(online_network)
-        online_network_original = deepcopy(online_network)
-        target_network_original = deepcopy(target_network)
+        online_network_copy = deepcopy(online_network)
+        target_network_copy = deepcopy(target_network)
 
         # Check parameters are equal
         assert torch.equal(next(iter(online_network.parameters()))[0], next(iter(target_network.parameters()))[0])
@@ -33,24 +30,27 @@ def test_byol_ma_weight_single_update_callback(initial_tau):
 
         # Check online network update
         assert not torch.equal(
-            next(iter(online_network.parameters()))[0], next(iter(online_network_original.parameters()))[0]
+            next(iter(online_network.parameters()))[0], next(iter(online_network_copy.parameters()))[0]
         )
 
         # Update target network weights via callback
         cb = BYOLMAWeightUpdate(initial_tau)
         cb.update_weights(online_network, target_network)
 
-        # Check target network update
+        # Check target network update according to value of tau
         if initial_tau == 0.0:
             assert torch.equal(next(iter(target_network.parameters()))[0], next(iter(online_network.parameters()))[0])
         elif initial_tau == 1.0:
             assert torch.equal(
-                next(iter(target_network.parameters()))[0], next(iter(target_network_original.parameters()))[0]
+                next(iter(target_network.parameters()))[0], next(iter(target_network_copy.parameters()))[0]
             )
         else:
-            assert not torch.equal(
-                next(iter(target_network.parameters()))[0], next(iter(target_network_original.parameters()))[0]
+            for online_p, target_p in zip(online_network.parameters(), target_network_copy.parameters()):
+                target_p.data = initial_tau * target_p.data + (1.0 - initial_tau) * online_p.data
+
+            assert torch.equal(
+                next(iter(target_network.parameters()))[0], next(iter(target_network_copy.parameters()))[0]
             )
-            assert not torch.equal(
-                next(iter(target_network.parameters()))[0], next(iter(online_network_original.parameters()))[0]
-            )
+    else:
+        with pytest.raises(ValueError, match="initial tau should be"):
+            cb = BYOLMAWeightUpdate(initial_tau)
