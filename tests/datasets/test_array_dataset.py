46ebb251c6c5ea5c04c224b871ddec56fea74da3
Cellan Hall <hallcellan@gmail.com>
2022-08-25 22:06:57 +0100

    removed auxiliary functions

--- tests/datasets/test_array_dataset.py
+++ tests/datasets/test_array_dataset.py
@@ -7,24 +7,10 @@ from pl_bolts.datasets import ArrayDataset, DataModel
 from pl_bolts.datasets.utils import to_tensor
 
 
-def add_one(integers: np.ndarray) -> np.ndarray:
-    output = []
-    for data in integers:
-        output.append(data + 1)
-    return np.array(output)
-
-
-def add_one_to_tensor(integers: np.ndarray) -> torch.Tensor:
-    integers = add_one(integers)
-    return to_tensor(integers)
-
-
 class TestArrayDataset:
     @pytest.fixture
     def array_dataset(self):
-        features_1 = DataModel(
-            data=[[1, 0, -1, 2], [1, 0, -2, -1], [2, 5, 0, 3], [-7, 1, 2, 2]], transform=add_one_to_tensor
-        )
+        features_1 = DataModel(data=[[1, 0, -1, 2], [1, 0, -2, -1], [2, 5, 0, 3], [-7, 1, 2, 2]], transform=to_tensor)
         target_1 = DataModel(data=[1, 0, 0, 1], transform=to_tensor)
 
         features_2 = DataModel(data=np.array([[2, 1, -5, 1], [1, 0, -2, -1], [2, 5, 0, 3], [-7, 1, 2, 2]]))
@@ -39,19 +25,19 @@ class TestArrayDataset:
         assert len(array_dataset[1]) == 4
         assert len(array_dataset[2]) == 4
         assert len(array_dataset[3]) == 4
-        torch.testing.assert_close(array_dataset[0][0], torch.tensor([2, 1, 0, 3]))
+        torch.testing.assert_close(array_dataset[0][0], torch.tensor([1, 0, -1, 2]))
         torch.testing.assert_close(array_dataset[0][1], torch.tensor(1))
         np.testing.assert_array_equal(array_dataset[0][2], np.array([2, 1, -5, 1]))
         assert array_dataset[0][3] == 1
-        torch.testing.assert_close(array_dataset[1][0], torch.tensor([2, 1, -1, -0]))
+        torch.testing.assert_close(array_dataset[1][0], torch.tensor([1, 0, -2, -1]))
         torch.testing.assert_close(array_dataset[1][1], torch.tensor(0))
         np.testing.assert_array_equal(array_dataset[1][2], np.array([1, 0, -2, -1]))
         assert array_dataset[1][3] == 0
-        torch.testing.assert_close(array_dataset[2][0], torch.tensor([3, 6, 1, 4]))
+        torch.testing.assert_close(array_dataset[2][0], torch.tensor([2, 5, 0, 3]))
         torch.testing.assert_close(array_dataset[2][1], torch.tensor(0))
         np.testing.assert_array_equal(array_dataset[2][2], np.array([2, 5, 0, 3]))
         assert array_dataset[2][3] == 1
-        torch.testing.assert_close(array_dataset[3][0], torch.tensor([-6, 2, 3, 3]))
+        torch.testing.assert_close(array_dataset[3][0], torch.tensor([-7, 1, 2, 2]))
         torch.testing.assert_close(array_dataset[3][1], torch.tensor(1))
         np.testing.assert_array_equal(array_dataset[3][2], np.array([-7, 1, 2, 2]))
         assert array_dataset[3][3] == 1
--- tests/datasets/test_base_dataset.py
+++ tests/datasets/test_base_dataset.py
@@ -1,14 +1,9 @@
 import numpy as np
 import pytest
+import torch
 
 from pl_bolts.datasets.base_dataset import DataModel
-
-
-def add_two(integers: np.ndarray) -> np.ndarray:
-    output = []
-    for data in integers:
-        output.append(data + 2)
-    return np.array(output)
+from pl_bolts.datasets.utils import to_tensor
 
 
 class TestDataModel:
@@ -22,6 +17,6 @@ class TestDataModel:
         np.testing.assert_array_equal(dm.process(data[1]), data[1])
 
     def test_process_transform_is_not_none(self, data):
-        dm = DataModel(data=data, transform=add_two)
-        np.testing.assert_array_equal(dm.process(data[0]), np.array([3, 2, 2, 3]))
-        np.testing.assert_array_equal(dm.process(data[1]), np.array([2, 3, 3, 2]))
+        dm = DataModel(data=data, transform=to_tensor)
+        torch.testing.assert_close(dm.process(data[0]), torch.tensor([1, 0, 0, 1]))
+        torch.testing.assert_close(dm.process(data[1]), torch.tensor([0, 1, 1, 0]))
