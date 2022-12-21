29e9d71744978499ad3cc065fa352e220464f9b2
Cellan Hall <hallcellan@gmail.com>
2022-12-21 12:52:17 +0000

    Refactored tests for `LogisticRegression`
    Created a new test folder structure for regression tests
    
    Moved tests from `test_classic_ml.py` to `test_logistic_regression.py`

--- tests/models/test_classic_ml.py
+++ tests/models/test_classic_ml.py
@@ -1,13 +1,9 @@
-import functools
-import operator
-
 import numpy as np
 from pytorch_lightning import Trainer, seed_everything
 from torch.utils.data import DataLoader
 
-from pl_bolts.datamodules import MNISTDataModule
 from pl_bolts.datamodules.sklearn_datamodule import SklearnDataset
-from pl_bolts.models.regression import LinearRegression, LogisticRegression
+from pl_bolts.models.regression import LinearRegression
 
 
 def test_linear_regression_model(tmpdir):
@@ -37,30 +33,3 @@ def test_linear_regression_model(tmpdir):
     coeffs = model.linear.weight.detach().numpy().flatten()
     np.testing.assert_allclose(coeffs, [1, 2], rtol=1e-3)
     trainer.test(model, loader)
-
-
-def test_logistic_regression_model(tmpdir, datadir):
-    seed_everything(0)
-
-    # create dataset
-    dm = MNISTDataModule(num_workers=0, data_dir=datadir)
-
-    model = LogisticRegression(
-        input_dim=functools.reduce(operator.mul, dm.dims, 1), num_classes=10, learning_rate=0.001
-    )
-    model.prepare_data = dm.prepare_data
-    model.setup = dm.setup
-    model.train_dataloader = dm.train_dataloader
-    model.val_dataloader = dm.val_dataloader
-    model.test_dataloader = dm.test_dataloader
-
-    trainer = Trainer(
-        max_epochs=3,
-        default_root_dir=tmpdir,
-        logger=False,
-        enable_checkpointing=False,
-    )
-    trainer.fit(model)
-    trainer.test(model)
-    # todo: update model and add healthy check
-    # assert trainer.progress_bar_dict['test_acc'] >= 0.9
