51656f5edeeff28a4f8e66fddb8e5e1cbf6bf7ed
Jirka B <j.borovec+github@gmail.com>
2023-05-18 06:46:26 -0400

    import

--- pl_bolts/optimizers/lr_scheduler.py
+++ pl_bolts/optimizers/lr_scheduler.py
@@ -27,16 +27,16 @@ class LinearWarmupCosineAnnealingLR(_LRScheduler):
 
     Example:
         >>> import torch.nn as nn
+        >>> from torch.optim import Adam
+        >>> #
         >>> layer = nn.Linear(10, 1)
         >>> optimizer = Adam(layer.parameters(), lr=0.02)
         >>> scheduler = LinearWarmupCosineAnnealingLR(optimizer, warmup_epochs=10, max_epochs=40)
-        >>> #
         >>> # the default case
         >>> for epoch in range(40):
         ...     # train(...)
         ...     # validate(...)
         ...     scheduler.step()
-        >>> #
         >>> # passing epoch param case
         >>> for epoch in range(40):
         ...     scheduler.step(epoch)
