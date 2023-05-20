6dbec13ee52f8f1e959f7a7f450fc4bbb5d0b432
Jirka B <j.borovec+github@gmail.com>
2023-05-20 14:57:08 -0400

    raise

--- src/pl_bolts/models/rl/common/gym_wrappers.py
+++ src/pl_bolts/models/rl/common/gym_wrappers.py
@@ -3,7 +3,6 @@ https://github.com/Shmuma/ptan/blob/master/ptan/common/wrappers.py."""
 import collections
 
 import numpy as np
-import pytest
 import torch
 
 from pl_bolts.utils import _GYM_AVAILABLE, _OPENCV_AVAILABLE
@@ -133,7 +132,7 @@ class ProcessFrame84(ObservationWrapper):
         elif frame.size == 250 * 160 * 3:
             img = np.reshape(frame, [250, 160, 3]).astype(np.float32)
         else:
-            pytest.fail("Unknown resolution.")
+            raise RuntimeError("Unknown resolution.")
         img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
         resized_screen = cv2.resize(img, (84, 110), interpolation=cv2.INTER_AREA)
         x_t = resized_screen[18:102, :]
