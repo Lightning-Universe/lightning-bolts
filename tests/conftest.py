f55c6cc878a9554faa55469b2ad17e9cd5f94087
Jirka <jirka.borovec@seznam.cz>
2023-06-16 17:33:20 +0200

    imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,6 @@ import torch
 from pl_bolts.utils import _IS_WINDOWS, _TORCHVISION_AVAILABLE, _TORCHVISION_LESS_THAN_0_13
 from pl_bolts.utils.stability import UnderReviewWarning
 from pytorch_lightning.trainer.connectors.signal_connector import SignalConnector
-from pytorch_lightning.utilities.imports import _IS_WINDOWS
 
 # GitHub Actions use this path to cache datasets.
 # Use `datadir` fixture where possible and use `DATASETS_PATH` in
