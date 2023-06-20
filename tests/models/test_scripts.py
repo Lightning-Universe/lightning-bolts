91f79d3be249a109b4bbd2814dfa80b5d065dc18
Jirka <jirka.borovec@seznam.cz>
2023-06-20 20:07:51 +0200

    ...

--- tests/models/test_scripts.py
+++ tests/models/test_scripts.py
@@ -7,16 +7,17 @@ from pl_bolts.utils import _IS_WINDOWS, _JSONARGPARSE_GREATER_THAN_4_16_0
 
 from tests import _MARK_REQUIRE_GPU, DATASETS_PATH
 
-_DEFAULT_ARGS = f" --data_dir {DATASETS_PATH} --max_epochs=1 --max_steps=2 --batch_size=4 --num_workers=0 "
+_DEFAULT_ARGS = f" --data_dir={DATASETS_PATH} --max_epochs=1 --max_steps=2 --batch_size=4"
 
 _DEFAULT_ARGS_RL_PONG = (
     " --env=PongNoFrameskip-v4 --max_steps=10 --fast_dev_run=1 --warm_start_size=10 --n_steps=2 --batch_size=10"
 )
-_DEFAULT_ARGS_CARTPOLE = " --env=CartPole-v0" " --max_steps=10" " --fast_dev_run=1" " --batch_size=10"
+_DEFAULT_ARGS_CARTPOLE = " --env=CartPole-v0 --max_steps=10 --fast_dev_run=1 --batch_size=10"
 _DEFAULT_LIGHTNING_CLI_ARGS = (
     f" fit --data.data_dir={DATASETS_PATH} --data.batch_size=4 --trainer.max_epochs=1 --trainer.max_steps=2"
 )
 _ARG_GPUS = f" --gpus={int(torch.cuda.is_available())}"
+_ARG_WORKERS_0 = " --num_workers=0"
 
 
 @pytest.mark.parametrize(
@@ -30,16 +31,16 @@ _ARG_GPUS = f" --gpus={int(torch.cuda.is_available())}"
         ("models.mnist_module", _DEFAULT_ARGS),
         ("models.autoencoders.basic_ae.basic_ae_module", _DEFAULT_ARGS),
         ("models.autoencoders.basic_vae.basic_vae_module", _DEFAULT_ARGS),
-        ("models.regression.linear_regression", "--max_epochs=1 --max_steps=2"),
-        ("models.regression.logistic_regression", "--max_epochs=1 --max_steps=2"),
+        ("models.regression.linear_regression", " --max_epochs=1 --max_steps=2"),
+        ("models.regression.logistic_regression", " --max_epochs=1 --max_steps=2"),
         pytest.param(
             "models.vision.image_gpt.igpt_module",
-            _DEFAULT_ARGS + f"--gpus={int(torch.cuda.is_available())}",
+            _DEFAULT_ARGS + _ARG_GPUS,
             marks=pytest.mark.skipif(**_MARK_REQUIRE_GPU),
         ),
         pytest.param(
             "models.detection.retinanet.retinanet_module",
-            _DEFAULT_LIGHTNING_CLI_ARGS + f" --trainer.gpus {int(torch.cuda.is_available())}",
+            _DEFAULT_LIGHTNING_CLI_ARGS + f" --trainer.gpus={int(torch.cuda.is_available())}",
             marks=[
                 pytest.mark.skipif(**_MARK_REQUIRE_GPU),
                 pytest.mark.skipif(not _JSONARGPARSE_GREATER_THAN_4_16_0, reason="Failing on CI, need to be fixed"),
@@ -56,7 +57,7 @@ _ARG_GPUS = f" --gpus={int(torch.cuda.is_available())}"
         ("models.rl.sac_model", "--env=Pendulum-v0 --max_steps=10 --fast_dev_run=1 --batch_size=10"),
         pytest.param(  # fixme
             "models.self_supervised.amdim.amdim_module",
-            _DEFAULT_ARGS + f" --gpus={int(torch.cuda.is_available())}",
+            _DEFAULT_ARGS + _ARG_WORKERS_0 + _ARG_GPUS,
             marks=[
                 pytest.mark.skipif(**_MARK_REQUIRE_GPU),
                 pytest.mark.xfail(reason="failing for GPU as some is on CPU other on GPU"),
@@ -64,7 +65,7 @@ _ARG_GPUS = f" --gpus={int(torch.cuda.is_available())}"
         ),
         pytest.param(  # fixme
             "models.self_supervised.cpc.cpc_module",
-            _DEFAULT_ARGS + _ARG_GPUS + " --dataset=cifar10 --hidden_mlp=512 --encoder=resnet18",
+            _DEFAULT_ARGS + _ARG_WORKERS_0 + _ARG_GPUS + " --dataset=cifar10 --hidden_mlp=512 --encoder=resnet18",
             marks=[
                 pytest.mark.skipif(**_MARK_REQUIRE_GPU),
                 pytest.mark.xfail(reason="failing for GPU as some is on CPU other on GPU"),
@@ -72,7 +73,7 @@ _ARG_GPUS = f" --gpus={int(torch.cuda.is_available())}"
         ),
         pytest.param(  # fixme
             "models.self_supervised.cpc.cpc_module",
-            _DEFAULT_ARGS + _ARG_GPUS + " --datase=stl10 --hidden_mlp=512 --encoder=resnet18",
+            _DEFAULT_ARGS + _ARG_WORKERS_0 + _ARG_GPUS + " --datase=stl10 --hidden_mlp=512 --encoder=resnet18",
             marks=[
                 pytest.mark.skipif(**_MARK_REQUIRE_GPU),
                 pytest.mark.xfail(reason="failing for GPU as some is on CPU other on GPU"),
@@ -80,28 +81,28 @@ _ARG_GPUS = f" --gpus={int(torch.cuda.is_available())}"
         ),
         pytest.param(
             "models.self_supervised.moco.moco2_module",
-            _DEFAULT_ARGS + _ARG_GPUS,
+            _DEFAULT_ARGS + _ARG_WORKERS_0 + _ARG_GPUS,
             marks=pytest.mark.skipif(**_MARK_REQUIRE_GPU),
         ),
         pytest.param(
             "models.self_supervised.simclr.simclr_module",
-            _DEFAULT_ARGS + _ARG_GPUS + " --online_ft --fp32",
+            _DEFAULT_ARGS + _ARG_WORKERS_0 + _ARG_GPUS + " --online_ft --fp32",
             marks=pytest.mark.skipif(**_MARK_REQUIRE_GPU),
         ),
         pytest.param(
             "models.self_supervised.byol.byol_module",
-            _DEFAULT_ARGS + _ARG_GPUS,
+            _DEFAULT_ARGS + _ARG_WORKERS_0 + _ARG_GPUS,
             marks=pytest.mark.skipif(**_MARK_REQUIRE_GPU),
         ),
         pytest.param(
             "models.self_supervised.swav.swav_module",
-            _DEFAULT_ARGS + _ARG_GPUS + " --dataset=cifar10 --arch=resnet18 --hidden_mlp=512 --fp32"
+            _DEFAULT_ARGS + _ARG_WORKERS_0 + _ARG_GPUS + " --dataset=cifar10 --arch=resnet18 --hidden_mlp=512 --fp32"
             " --sinkhorn_iterations=1 --nmb_prototypes=2 --queue_length=0",
             marks=pytest.mark.skipif(**_MARK_REQUIRE_GPU),
         ),
         pytest.param(
             "models.self_supervised.simsiam.simsiam_module",
-            _DEFAULT_ARGS + f" --gpus {int(torch.cuda.is_available())}",
+            _DEFAULT_ARGS + _ARG_WORKERS_0 + _ARG_GPUS,
             marks=pytest.mark.skipif(**_MARK_REQUIRE_GPU),
         ),
     ],
