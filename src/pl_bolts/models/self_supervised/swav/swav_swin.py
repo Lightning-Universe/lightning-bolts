8f73b74cb0eeb2a7520a913505159c891b1daaa4
Jirka <jirka.borovec@seznam.cz>
2023-06-30 20:57:11 +0200

    precommit

--- src/pl_bolts/models/self_supervised/swav/swav_swin.py
+++ src/pl_bolts/models/self_supervised/swav/swav_swin.py
@@ -5,7 +5,7 @@ from typing import Any, Callable, List, Optional
 
 import torch
 import torch.fx
-import torch.nn.functional as F
+import torch.nn.functional as F  # noqa: N812
 from torch import Tensor, nn
 
 # from torchvision.ops.misc import MLP, Permute
@@ -88,8 +88,7 @@ class StochasticDepth(nn.Module):
         return stochastic_depth(input, self.p, self.mode, self.training)
 
     def __repr__(self) -> str:
-        s = f"{self.__class__.__name__}(p={self.p}, mode={self.mode})"
-        return s
+        return f"{self.__class__.__name__}(p={self.p}, mode={self.mode})"
 
 
 class MLP(torch.nn.Sequential):
@@ -156,8 +155,8 @@ class Permute(torch.nn.Module):
 
 
 def _patch_merging_pad(x: torch.Tensor) -> torch.Tensor:
-    H, W, _ = x.shape[-3:]
-    x = F.pad(x, (0, 0, 0, W % 2, 0, H % 2))
+    h, w, _ = x.shape[-3:]
+    x = F.pad(x, (0, 0, 0, w % 2, 0, h % 2))
     x0 = x[..., 0::2, 0::2, :]  # ... H/2 W/2 C
     x1 = x[..., 1::2, 0::2, :]  # ... H/2 W/2 C
     x2 = x[..., 0::2, 1::2, :]  # ... H/2 W/2 C
@@ -172,9 +171,9 @@ torch.fx.wrap("_patch_merging_pad")
 def _get_relative_position_bias(
     relative_position_bias_table: torch.Tensor, relative_position_index: torch.Tensor, window_size: List[int]
 ) -> torch.Tensor:
-    N = window_size[0] * window_size[1]
+    n = window_size[0] * window_size[1]
     relative_position_bias = relative_position_bias_table[relative_position_index]  # type: ignore[index]
-    relative_position_bias = relative_position_bias.view(N, N, -1)
+    relative_position_bias = relative_position_bias.view(n, n, -1)
     relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous().unsqueeze(0)
     return relative_position_bias
 
@@ -272,18 +271,18 @@ def shifted_window_attention(
     Returns:
         Tensor[N, H, W, C]: The output tensor after shifted window attention.
     """
-    B, H, W, C = input.shape
+    b, h, w, c = input.shape
     # pad feature maps to multiples of window size
-    pad_r = (window_size[1] - W % window_size[1]) % window_size[1]
-    pad_b = (window_size[0] - H % window_size[0]) % window_size[0]
+    pad_r = (window_size[1] - w % window_size[1]) % window_size[1]
+    pad_b = (window_size[0] - h % window_size[0]) % window_size[0]
     x = F.pad(input, (0, 0, 0, pad_r, 0, pad_b))
-    _, pad_H, pad_W, _ = x.shape
+    _, pad_h, pad_w, _ = x.shape
 
     shift_size = shift_size.copy()
     # If window size is larger than feature size, there is no need to shift window
-    if window_size[0] >= pad_H:
+    if window_size[0] >= pad_h:
         shift_size[0] = 0
-    if window_size[1] >= pad_W:
+    if window_size[1] >= pad_w:
         shift_size[1] = 0
 
     # cyclic shift
@@ -291,9 +290,9 @@ def shifted_window_attention(
         x = torch.roll(x, shifts=(-shift_size[0], -shift_size[1]), dims=(1, 2))
 
     # partition windows
-    num_windows = (pad_H // window_size[0]) * (pad_W // window_size[1])
-    x = x.view(B, pad_H // window_size[0], window_size[0], pad_W // window_size[1], window_size[1], C)
-    x = x.permute(0, 1, 3, 2, 4, 5).reshape(B * num_windows, window_size[0] * window_size[1], C)  # B*nW, Ws*Ws, C
+    num_windows = (pad_h // window_size[0]) * (pad_w // window_size[1])
+    x = x.view(b, pad_h // window_size[0], window_size[0], pad_w // window_size[1], window_size[1], c)
+    x = x.permute(0, 1, 3, 2, 4, 5).reshape(b * num_windows, window_size[0] * window_size[1], c)  # B*nW, Ws*Ws, C
 
     # multi-head attention
     if logit_scale is not None and qkv_bias is not None:
@@ -301,7 +300,7 @@ def shifted_window_attention(
         length = qkv_bias.numel() // 3
         qkv_bias[length : 2 * length].zero_()
     qkv = F.linear(x, qkv_weight, qkv_bias)
-    qkv = qkv.reshape(x.size(0), x.size(1), 3, num_heads, C // num_heads).permute(2, 0, 3, 1, 4)
+    qkv = qkv.reshape(x.size(0), x.size(1), 3, num_heads, c // num_heads).permute(2, 0, 3, 1, 4)
     q, k, v = qkv[0], qkv[1], qkv[2]
     if logit_scale is not None:
         # cosine attention
@@ -309,14 +308,14 @@ def shifted_window_attention(
         logit_scale = torch.clamp(logit_scale, max=math.log(100.0)).exp()
         attn = attn * logit_scale
     else:
-        q = q * (C // num_heads) ** -0.5
+        q = q * (c // num_heads) ** -0.5
         attn = q.matmul(k.transpose(-2, -1))
     # add relative position bias
     attn = attn + relative_position_bias
 
     if sum(shift_size) > 0:
         # generate attention mask
-        attn_mask = x.new_zeros((pad_H, pad_W))
+        attn_mask = x.new_zeros((pad_h, pad_w))
         h_slices = ((0, -window_size[0]), (-window_size[0], -shift_size[0]), (-shift_size[0], None))
         w_slices = ((0, -window_size[1]), (-window_size[1], -shift_size[1]), (-shift_size[1], None))
         count = 0
@@ -324,7 +323,7 @@ def shifted_window_attention(
             for w in w_slices:
                 attn_mask[h[0] : h[1], w[0] : w[1]] = count
                 count += 1
-        attn_mask = attn_mask.view(pad_H // window_size[0], window_size[0], pad_W // window_size[1], window_size[1])
+        attn_mask = attn_mask.view(pad_h // window_size[0], window_size[0], pad_w // window_size[1], window_size[1])
         attn_mask = attn_mask.permute(0, 2, 1, 3).reshape(num_windows, window_size[0] * window_size[1])
         attn_mask = attn_mask.unsqueeze(1) - attn_mask.unsqueeze(2)
         attn_mask = attn_mask.masked_fill(attn_mask != 0, float(-100.0)).masked_fill(attn_mask == 0, float(0.0))
@@ -335,21 +334,20 @@ def shifted_window_attention(
     attn = F.softmax(attn, dim=-1)
     attn = F.dropout(attn, p=attention_dropout)
 
-    x = attn.matmul(v).transpose(1, 2).reshape(x.size(0), x.size(1), C)
+    x = attn.matmul(v).transpose(1, 2).reshape(x.size(0), x.size(1), c)
     x = F.linear(x, proj_weight, proj_bias)
     x = F.dropout(x, p=dropout)
 
     # reverse windows
-    x = x.view(B, pad_H // window_size[0], pad_W // window_size[1], window_size[0], window_size[1], C)
-    x = x.permute(0, 1, 3, 2, 4, 5).reshape(B, pad_H, pad_W, C)
+    x = x.view(b, pad_h // window_size[0], pad_w // window_size[1], window_size[0], window_size[1], c)
+    x = x.permute(0, 1, 3, 2, 4, 5).reshape(b, pad_h, pad_w, c)
 
     # reverse cyclic shift
     if sum(shift_size) > 0:
         x = torch.roll(x, shifts=(shift_size[0], shift_size[1]), dims=(1, 2))
 
     # unpad features
-    x = x[:, :H, :W, :].contiguous()
-    return x
+    return x[:, :h, :w, :].contiguous()
 
 
 torch.fx.wrap("shifted_window_attention")
@@ -823,7 +821,7 @@ def _swin_transformer(
     stochastic_depth_prob: float,
     **kwargs: Any,
 ) -> SwinTransformer:
-    model = SwinTransformer(
+    return SwinTransformer(
         patch_size=patch_size,
         embed_dim=embed_dim,
         depths=depths,
@@ -832,7 +830,6 @@ def _swin_transformer(
         stochastic_depth_prob=stochastic_depth_prob,
         **kwargs,
     )
-    return model
 
 
 def swin_s(**kwargs: Any) -> SwinTransformer:
