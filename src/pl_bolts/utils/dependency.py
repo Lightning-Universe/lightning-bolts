770774d8bc2a5cd86997024e24f60b8198491bc2
Jirka <jirka.borovec@seznam.cz>
2023-07-12 10:14:48 +0200

    typing

--- src/pl_bolts/utils/dependency.py
+++ src/pl_bolts/utils/dependency.py
@@ -1,13 +1,14 @@
 import functools
 import os
+from typing import Callable, Any
 
 from lightning_utilities.core.imports import ModuleAvailableCache, RequirementCache
 
 
-def requires(*module_path_version: str):
+def requires(*module_path_version: str) -> Callable:
     """Wrapper for enforcing certain requirements for a particular class or function."""
 
-    def decorator(func):
+    def decorator(func: Callable) -> Callable:
         reqs = []
         for mod_ver in module_path_version:
             if "." in mod_ver:
@@ -19,7 +20,7 @@ def requires(*module_path_version: str):
         if not available:
 
             @functools.wraps(func)
-            def wrapper(*args, **kwargs):
+            def wrapper(*args: Any, **kwargs: Any) -> Any:
                 msg = os.linesep.join([repr(r) for r in reqs if not bool(r)])
                 raise ModuleNotFoundError(f"Required dependencies not available: \n{msg}")
 
