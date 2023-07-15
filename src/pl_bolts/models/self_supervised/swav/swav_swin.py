b1f55fcb4be2106784602dc674f2c09d0ccbe3b1
Ahmed Mohamed Hamdi Mohamed <amhamdi@aic.gov.eg>
2023-07-15 18:41:52 +0300

    changed the condition in requires from utils/_dependency.py by adding a not in the if condition of reqs list

--- src/pl_bolts/models/self_supervised/swav/swav_swin.py
+++ src/pl_bolts/models/self_supervised/swav/swav_swin.py
@@ -14,26 +14,7 @@ from torch import Tensor, nn
 from typing import Any, Callable, List, Optional, Tuple, Union
 from lightning_utilities.core.imports import ModuleAvailableCache, RequirementCache
 
-# ToDo: replace with utils wrapper after 0.10 is released
-def requires(*module_path_version: str) -> Callable: 
-    """Wrapper for enforcing certain requirements for a particular class or function."""
-    def decorator(func: Callable) -> Callable:
-        reqs = [
-            RequirementCache(mod_ver) if (">" in mod_ver or "=" in mod_ver) else ModuleAvailableCache(mod_ver) 
-            for mod_ver in module_path_version
-        ]
-        available = all(map(bool, reqs))
-        if not available:
-
-            @functools.wraps(func)
-            def wrapper(*args: Any, **kwargs: Any) -> Any:
-                msg = os.linesep.join([repr(r) for r in reqs if not bool(r)])
-                raise ModuleNotFoundError(f"Required dependencies not available: \n{msg}")
-
-            return wrapper
-        return func
-
-    return decorator
+from pl_bolts.utils._dependency import requires
 
 # Support meshgrid indexing for older versions of torch
 def meshgrid(*tensors: Union[Tensor, List[Tensor]], indexing: Optional[str] = None) -> Tuple:
